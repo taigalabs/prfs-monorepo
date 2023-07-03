3d2da4500b53406332788c783cd4cc3653a426fd
Elden Park <eldeniyenden@gmail.com>
2023-07-03 19:50:55 +0900

    c

--- source/prfs_js/src/wasm_wrapper/load_cjs.ts
+++ source/prfs_js/src/wasm_wrapper/load_cjs.ts
@@ -1,22 +1,19 @@
 import * as Comlink from "comlink";
 import path from 'path';
 import { Worker } from 'worker_threads';
-import nodeAdaptor from "comlink/node-adapter";
+import nodeEndpoint from "comlink/dist/umd/node-adapter";
 
 export async function initWasm() {
   console.log("init()");
 
-  console.log(55, nodeAdaptor);
-
   let filename = path.resolve(__dirname, './wasm_worker.js');
+  let worker = new Worker(filename);
 
   // const handlers = await (
-  //   Comlink.wrap(
-  //     new Worker(filename, {
-  //       type: "module"
-  //     })
-  //   ) as any
+  //   Comlink.wrap(nodeEndpoint(worker))
   // ).handlers;
+
+  // console.log(5, handlers);
 
   // if (!handlers) {
   //   console.log("handlers not found");
