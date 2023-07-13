b16e61dc94e126b71aca6fadb2bc724fe470b7af
Elden Park <eldeniyenden@gmail.com>
2023-07-14 00:46:57 +0900

    o

--- source/prfs_web/src/app/signin/page.tsx
+++ source/prfs_web/src/app/signin/page.tsx
@@ -1,5 +1,9 @@
+"use client";
+
 import React from "react";
+
 import styles from "./page.module.scss";
+import { I18nContext } from "@/contexts";
 
 const SignIn: React.FC = () => {
   console.log("Home()");
@@ -8,6 +12,9 @@ const SignIn: React.FC = () => {
   // React.useEffect(() => {
   //   getSigner().then();
   // }, [setAccount]);
+  //
+
+  let i18n = React.useContext(I18nContext);
 
   return (
     <div className={styles.wrapper}>
@@ -16,11 +23,11 @@ const SignIn: React.FC = () => {
           <input type="password"></input>
           <button>Sign in</button>
         </div>
-        <div>
+        <div className={styles.desc}>
           <ol>
-            <li>123</li>
-            <li>123</li>
-            <li>123</li>
+            <li>{i18n.desc_1}</li>
+            <li>{i18n.desc_2}</li>
+            <li>{i18n.desc_3}</li>
           </ol>
         </div>
       </div>
