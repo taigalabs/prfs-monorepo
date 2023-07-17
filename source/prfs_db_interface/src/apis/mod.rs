fa0e8362a294a9bbe3b09523ccddba4a99d609c8
Elden Park <eldeniyenden@gmail.com>
2023-07-17 16:20:09 +0900

    o

--- source/prfs_db_interface/src/apis/mod.rs
+++ source/prfs_db_interface/src/apis/mod.rs
@@ -1,12 +1,2 @@
 mod eth;
 mod prfs_account;
-
-use super::models::EthAccount;
-use crate::{
-    database::Database,
-    models::{EthAccountTreeNode, EthTreeNode, ProofType},
-    DbInterfaceError,
-};
-use rust_decimal::Decimal;
-use std::{collections::BTreeMap, fs::write};
-use tokio_postgres::{Client as PGClient, Row};
