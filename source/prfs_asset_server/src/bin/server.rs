7f2241fc561d15d0528ba499c023752d386bbd6f
Elden Park <eldeniyenden@gmail.com>
2023-07-22 19:53:45 +0900

    o

--- source/prfs_asset_server/src/bin/server.rs
+++ source/prfs_asset_server/src/bin/server.rs
@@ -1,18 +1,24 @@
 use hyper::Server;
 use hyper_staticfile::Static;
 use prfs_asset_server::state::ServerState;
+use prfs_asset_server::utils::copy_dir_all;
+use prfs_asset_server::AssetServerError;
 use prfs_asset_server::{paths::PATHS, router::make_router};
 use prfs_circuits_circom::builder::CircuitBuildJson;
 use routerify::RouterService;
 use std::net::SocketAddr;
-
-pub type AssetServerError = Box<dyn std::error::Error + Sync + Send>;
+use std::path::PathBuf;
 
 #[tokio::main]
 async fn main() -> Result<(), AssetServerError> {
-    load_assets();
+    let _circuit_build_json = match load_assets() {
+        Ok(a) => a,
+        Err(err) => {
+            return Err(format!("Failed to load assets, err: {}", err).into());
+        }
+    };
 
-    let static_serve = Static::new(&PATHS.assets);
+    let static_serve = Static::new(&PATHS.assets_local);
     let server_state = ServerState { static_serve };
     let router = make_router(server_state);
 
@@ -29,26 +35,37 @@ async fn main() -> Result<(), AssetServerError> {
     Ok(())
 }
 
-fn load_assets() {
+fn load_assets() -> Result<CircuitBuildJson, AssetServerError> {
     let circuits_build_path = prfs_circuits_circom::get_build_fs_path();
-    circuits_build_path.try_exists().expect(&format!(
-        "circuits should have been built, path: {:?}",
-        circuits_build_path
-    ));
+    circuits_build_path.try_exists()?;
+
     println!("prfs_circuits build path: {:?}", circuits_build_path);
 
-    let b = std::fs::read(circuits_build_path.join("build.json")).unwrap();
-    let new_circuit_build_json: CircuitBuildJson = serde_json::from_slice(&b).unwrap();
-    println!("c: {:?}", new_circuit_build_json);
+    let b = std::fs::read(circuits_build_path.join("build.json"))?;
+    let new_circuit_build_json: CircuitBuildJson = serde_json::from_slice(&b)?;
 
     if PATHS.assets.exists() {
         let build_json_path = PATHS.assets_local.join("build.json");
-        build_json_path.try_exists().unwrap();
+        build_json_path.try_exists()?;
 
-        let b = std::fs::read(&build_json_path).unwrap();
-        let circuit_build_json: CircuitBuildJson = serde_json::from_slice(&b).unwrap();
-        println!("d: {:?}", new_circuit_build_json);
+        let b = std::fs::read(&build_json_path)?;
+        let old_circuit_build_json: CircuitBuildJson = serde_json::from_slice(&b)?;
+
+        let old_timestamp: usize = old_circuit_build_json.timestamp.parse()?;
+        let new_timestamp: usize = new_circuit_build_json.timestamp.parse()?;
+
+        if new_timestamp > old_timestamp {
+            copy_circuit_build(circuits_build_path);
+        }
     } else {
         println!("circuits don't exist. Copying...");
+
+        copy_circuit_build(circuits_build_path);
     }
+
+    return Ok(new_circuit_build_json);
+}
+
+fn copy_circuit_build(circuit_src: PathBuf) {
+    copy_dir_all(circuit_src, &PATHS.assets).unwrap();
 }
--- source/prfs_asset_server/src/lib.rs
+++ source/prfs_asset_server/src/lib.rs
@@ -2,5 +2,6 @@ pub mod apis;
 pub mod paths;
 pub mod router;
 pub mod state;
+pub mod utils;
 
 pub type AssetServerError = Box<dyn std::error::Error + Sync + Send>;
