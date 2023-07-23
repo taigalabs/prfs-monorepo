242a399fcf04f1d6758540243f524737f4517322
Elden Park <eldeniyenden@gmail.com>
2023-07-23 10:11:34 +0900

    o

--- externals/lib/jest.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('ts-jest').JestConfigWithTsJest} */
-module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
-  transform: {
-    "^.+\\.js?$": "ts-jest"
-  },
-  testTimeout: 600000,
-};
