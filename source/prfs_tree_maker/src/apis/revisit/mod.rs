c3447ba5b15528f8c84eee9cc4c5d82c5236946a
Elden Park <eldeniyenden@gmail.com>
2023-07-26 02:02:15 +0900

    o

--- source/prfs_tree_maker/src/apis/revisit/mod.rs
+++ source/prfs_tree_maker/src/apis/revisit/mod.rs
@@ -6,4 +6,11 @@ pub async fn revisit(_sub_matches: &ArgMatches) {
     let pg_endpoint = &ENVS.postgres_endpoint;
     let pg_pw = &ENVS.postgres_pw;
     let db = Database::connect(pg_endpoint, pg_pw).await.unwrap();
+
+    run(&db).await;
+}
+
+async fn run(db: &Database) {
+    let accs = db.get_eth_accounts("limit 5000").await.unwrap();
+    println!("accs len: {}", accs.len());
 }
