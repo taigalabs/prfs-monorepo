2b300e9510a080126f62540b6c405d6ac4474f5c
Elden Park <eldeniyenden@gmail.com>
2023-07-26 16:53:49 +0900

    o

--- source/prfs_web/src/app/sets/[set]/page.tsx
+++ source/prfs_web/src/app/sets/[set]/page.tsx
@@ -2,22 +2,36 @@
 
 import React from "react";
 import Link from "next/link";
-import { ethers } from "ethers";
-import { useRouter } from "next/navigation";
 import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
 
 import styles from "./Set.module.scss";
 import { stateContext } from "@/contexts/state";
-import Table, { TableData } from "@/components/table/Table";
 import Widget from "@/components/widget/Widget";
 import { i18nContext } from "@/contexts/i18n";
 import DefaultLayout from "@/layouts/default_layout/DefaultLayout";
 import useLocalWallet from "@/hooks/useLocalWallet";
 import Card from "@/components/card/Card";
 import CardRow from "@/components/card_row/CardRow";
-import CircuitTable from "@/components/circuit_table/CircuitTable";
-import SetTable from "@/components/set_table/SetTable";
 import Breadcrumb from "@/components/breadcrumb/Breadcrumb";
+import SetElementTable from "@/components/set_element_table/SetElementTable";
+
+const SetSummary = () => {
+  return (
+    <div className={styles.setSummaryWrapper}>
+      <div className={styles.col}>
+        <div className={styles.cell}>cell1</div>
+      </div>
+      <div className={styles.col}>
+        <div className={styles.cell}>cellb1</div>
+        <div className={styles.cell}>cellb2</div>
+        <div className={styles.cell}>cellb3</div>
+      </div>
+      <div className={styles.col}>
+        <div className={styles.cell}>cell</div>
+      </div>
+    </div>
+  );
+};
 
 const Set: React.FC<SetProps> = ({ params }) => {
   const i18n = React.useContext(i18nContext);
@@ -37,7 +51,8 @@ const Set: React.FC<SetProps> = ({ params }) => {
       <CardRow>
         <Card>
           <Widget label={`${i18n.set} - ${params.set}`}>
-            <div></div>
+            <SetSummary />
+            <SetElementTable />
           </Widget>
         </Card>
       </CardRow>
--- source/prfs_web/src/components/set_element_table/SetElementTable.tsx
+++ source/prfs_web/src/components/set_element_table/SetElementTable.tsx
@@ -3,7 +3,7 @@
 import React from "react";
 import Link from "next/link";
 
-import styles from "./SetTable.module.scss";
+import styles from "./SetElementTable.module.scss";
 import Table, { TableData, TableKeys } from "@/components/table/Table";
 import { i18nContext } from "@/contexts/i18n";
 import prfsBackend from "@/fetch/prfsBackend";
