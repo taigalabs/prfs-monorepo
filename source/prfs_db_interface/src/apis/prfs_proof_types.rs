4606553cb6be0e0d2d331a3c0a621b95bdc0a1fb
Elden Park <eldeniyenden@gmail.com>
2023-07-31 19:52:52 +0900

    o

--- source/prfs_db_interface/src/apis/prfs_proof_types.rs
+++ source/prfs_db_interface/src/apis/prfs_proof_types.rs
@@ -2,8 +2,6 @@ use crate::{
     database::Database,
     database2::Database2,
     models::{PrfsProofType, PrfsSet},
-    utils::{concat_cols, concat_values},
-    DbInterfaceError,
 };
 use chrono::NaiveDate;
 use sqlx::{Execute, Postgres, Row};
@@ -18,24 +16,25 @@ impl Database2 {
     ) {
         println!("2");
 
-        let query = "INSERT INTO prfs_proof_types (proof_type_id, author, label, \"desc\", circuit_id, public_inputs) VALUES ($1, $2, $3, $4, $5, $6) returning proof_type_id";
+        let query = "INSERT INTO prfs_proof_types \
+            (proof_type_id, author, label, \"desc\", circuit_id, public_inputs) \
+            VALUES ($1, $2, $3, $4, $5, $6) returning proof_type_id";
 
         let proof_type = proof_types.get(0).unwrap();
 
-        let mut a: sqlx::query::Query<'_, Postgres, _> = sqlx::query(query)
+        let row = sqlx::query(query)
             .bind(&proof_type.proof_type_id)
             .bind(&proof_type.author)
             .bind(&proof_type.label)
             .bind(&proof_type.desc)
             .bind(&proof_type.circuit_id)
-            .bind(&proof_type.public_inputs);
+            .bind(&proof_type.public_inputs)
+            .fetch_one(&self.pool)
+            .await
+            .unwrap();
 
-        // a.execute(&self.pool).await.unwrap();
+        let proof_type_id: String = row.get("proof_type_id");
 
-        let proof_type_id = a.fetch_one(&self.pool).await.unwrap();
-        // for row in rows {
-        //     let a: String = row.get(0);
-        //     println!("a: {:?}", a);
-        // }
+        println!("proof_type_id: {}", proof_type_id);
     }
 }
