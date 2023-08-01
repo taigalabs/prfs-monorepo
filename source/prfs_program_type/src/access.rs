5d9e17a147a850fee418d8a5c2af3e76019c143a
Elden Park <eldeniyenden@gmail.com>
2023-08-01 16:43:18 +0900

    o

--- source/prfs_program_type/src/access.rs
+++ source/prfs_program_type/src/access.rs
@@ -1,5 +1,9 @@
-use crate::paths::PATHS;
+use crate::{paths::PATHS, ProgramsJson};
 
 pub fn load_system_native_program_types() {
-    &PATHS.data;
+    let programs_json_path = PATHS.data.join("programs");
+    let b = std::fs::read(&programs_json_path).unwrap();
+    let programs_json: ProgramsJson = serde_json::from_slice(&b).unwrap();
+
+    // programs_json.programs
 }
--- source/prfs_program_type/src/json.rs
+++ source/prfs_program_type/src/json.rs
@@ -3,6 +3,11 @@ use serde::{Deserialize, Serialize};
 pub const SPARTAN_CIRCOM_PROGRAM_TYPE: &str = "SPARTAN_CIRCOM_1";
 
 #[derive(Serialize, Deserialize, Clone, Debug)]
+pub struct ProgramsJson {
+    pub programs: Vec<CircuitProgram>,
+}
+
+#[derive(Serialize, Deserialize, Clone, Debug)]
 pub struct CircuitProgram {
     program_id: String,
     program_repository_url: String,
