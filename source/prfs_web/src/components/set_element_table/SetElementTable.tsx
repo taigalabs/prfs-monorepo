0b65420b47696492cb0a6dba9af80b46ab59bbae
Elden Park <eldeniyenden@gmail.com>
2023-08-03 15:29:12 +0900

    o

--- source/prfs_web/src/components/set_element_table/SetElementTable.tsx
+++ source/prfs_web/src/components/set_element_table/SetElementTable.tsx
@@ -7,43 +7,11 @@ import styles from "./SetElementTable.module.scss";
 import Table, { TableBody, TableRow, TableData, TableHeader } from "@/components/table/Table";
 import { i18nContext } from "@/contexts/i18n";
 import * as prfsBackend from "@/fetch/prfsBackend";
+import { PrfsTreeNode } from "@/models";
 
 const SetElementTable: React.FC<SetElementTableProps> = ({ setId }) => {
   const i18n = React.useContext(i18nContext);
-
-  const createHeader = React.useCallback(() => {
-    return (
-      <TableHeader>
-        <TableRow>
-          <th className={styles.id}>{i18n.id}</th>
-          <th className={styles.val}>{i18n.value}</th>
-        </TableRow>
-      </TableHeader>
-    );
-  }, []);
-
-  const createBody = React.useCallback(({ data }) => {
-    // console.log(1, data);
-    let { page, values } = data;
-
-    let rows = [];
-    if (values === undefined || values.length < 1) {
-      return rows;
-    }
-
-    for (let val of values) {
-      let row = (
-        <TableRow key={val.pos_w}>
-          <td className={styles.id}>{val.pos_w}</td>
-          <td className={styles.val}>{val.val}</td>
-        </TableRow>
-      );
-
-      rows.push(row);
-    }
-
-    return <TableBody key={page}>{rows}</TableBody>;
-  }, []);
+  const [data, setData] = React.useState<TableData<PrfsTreeNode>>({ page: 0, values: [] });
 
   const handleChangePage = React.useCallback(
     async (page: number) => {
@@ -65,13 +33,45 @@ const SetElementTable: React.FC<SetElementTableProps> = ({ setId }) => {
     [setId]
   );
 
+  React.useEffect(() => {
+    Promise.resolve(handleChangePage(0)).then(res => {
+      setData(res);
+    });
+  }, [setData, handleChangePage]);
+
+  const rowsElem = React.useMemo(() => {
+    // console.log(1, data);
+    let { page, values } = data;
+
+    let rows = [];
+    if (values === undefined || values.length < 1) {
+      return rows;
+    }
+
+    for (let val of values) {
+      let row = (
+        <TableRow key={val.pos_w}>
+          <td className={styles.id}>{val.pos_w}</td>
+          <td className={styles.val}>{val.val}</td>
+        </TableRow>
+      );
+
+      rows.push(row);
+    }
+
+    return rows;
+  }, [data]);
+
   return (
-    <Table
-      createHeader={createHeader}
-      createBody={createBody}
-      onChangePage={handleChangePage}
-      minWidth={800}
-    />
+    <Table minWidth={800}>
+      <TableHeader>
+        <TableRow>
+          <th className={styles.id}>{i18n.id}</th>
+          <th className={styles.val}>{i18n.value}</th>
+        </TableRow>
+      </TableHeader>
+      <TableBody>{rowsElem}</TableBody>
+    </Table>
   );
 };
 
