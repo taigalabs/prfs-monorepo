109ae02e2c38f2affc9f6d2b82fb157076163247
Elden Park <eldeniyenden@gmail.com>
2023-08-04 16:17:36 +0900

    o

--- source/e2e_test_web/src/test.mjs
+++ source/e2e_test_web/src/test.mjs
@@ -5,19 +5,17 @@ import puppeteer from "puppeteer";
     headless: "new",
   });
   const page = await browser.newPage();
-
   await page.goto("http://localhost:3000");
+
+  // not implemented yet
 
   // Set screen size
   // await page.setViewport({ width: 1080, height: 1024 });
-
-  const searchResultSelector = "body";
-
-  let selector = await page.waitForSelector(searchResultSelector);
-
-  const fullTitle = await selector?.evaluate(el => el.textContent);
-
-  console.log('The title of this blog post is "%s".', fullTitle);
+  //
+  // const searchResultSelector = "body";
+  // let selector = await page.waitForSelector(searchResultSelector);
+  // const fullTitle = await selector?.evaluate(el => el.textContent);
+  // console.log('The title of this blog post is "%s".', fullTitle);
 
   await browser.close();
 })();
