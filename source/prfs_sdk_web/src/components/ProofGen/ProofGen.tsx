141f63b8e2dd8abb7fbcbe58b908ad8f4634cfc5
Elden Park <eldeniyenden@gmail.com>
2023-08-06 22:30:33 +0900

    o

--- source/prfs_sdk_web/src/components/ProofGen/ProofGen.tsx
+++ source/prfs_sdk_web/src/components/ProofGen/ProofGen.tsx
@@ -1,13 +1,19 @@
 import React from "react";
+import { PrfsProofType } from "@taigalabs/prfs-entities/bindings/PrfsProofType";
 
 import styles from "./ProofGen.module.scss";
 
 const ProofGen: React.FC<ProofGenProps> = ({ proofType }) => {
-  return <div className={styles.thisWillBeEncapsulatedInIframe}>Prfs Web SDK</div>;
+  return (
+    <div className={styles.thisWillBeEncapsulatedInIframe}>
+      <div>public input</div>
+      <div>aa</div>
+    </div>
+  );
 };
 
 export default ProofGen;
 
 export interface ProofGenProps {
-  proofType: any;
+  proofType: PrfsProofType;
 }
