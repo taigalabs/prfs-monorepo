56cc25519fe3c5dcdb40fed4b134e41e46d45c09
Elden Park <eldeniyenden@gmail.com>
2023-08-07 03:47:47 +0900

    o

--- source/prfs_sdk_web/jest.config.js
+++ source/prfs_sdk_web/jest.config.js
@@ -1,9 +1,9 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
+  preset: "ts-jest",
+  testEnvironment: "node",
   transform: {
-    "^.+\\.js?$": "ts-jest"
+    "^.+\\.js?$": "ts-jest",
   },
   testTimeout: 600000,
 };
