12ab68c2630056aa731fd478b9dfeba881db0bc8
Elden Park <eldeniyenden@gmail.com>
2023-08-08 11:44:50 +0900

    o

--- source/prfs_sdk_web_ui/src/app/page.tsx
+++ source/prfs_sdk_web_ui/src/app/page.tsx
@@ -2,13 +2,11 @@
 
 import React from "react";
 
-import { stateContext } from "@/contexts/state";
 import styles from "./Home.module.scss";
 // import Widget, { WidgetHeader, WidgetLabel } from "@/components/widget/Widget";
 import { i18nContext } from "@/contexts/i18n";
 // import DefaultLayout from "@/layouts/default_layout/DefaultLayout";
 import useLocalWallet from "@/hooks/useLocalWallet";
-import { ProofGen } from "@taigalabs/prfs-sdk-web";
 // import Teaser from "@/components/teaser/Teaser";
 // import Card from "@/components/card/Card";
 // import CardRow from "@/components/card_row/CardRow";
@@ -18,9 +16,8 @@ import { ProofGen } from "@taigalabs/prfs-sdk-web";
 
 const Home: React.FC = () => {
   const i18n = React.useContext(i18nContext);
-  const { dispatch } = React.useContext(stateContext);
 
-  useLocalWallet(dispatch);
+  // useLocalWallet(dispatch);
   return <div>55</div>;
 
   // return <ProofGen />;
--- source/prfs_sdk_web_ui/src/models/index.ts
+++
@@ -1,4 +0,0 @@
-export * from "./prfs_circuit";
-export * from "./prfs_set";
-export * from "./prfs_tree_node";
-export * from "./prfs_proof_type";
