dbd87ae7678e8b88e38ca3431ab05886a3f61613
Elden Park <eldeniyenden@gmail.com>
2023-08-09 13:12:31 +0900

    o

--- source/prfs_driver_spartan_js/src/wasm_wrapper/load_worker.ts
+++ source/prfs_driver_spartan_js/src/wasm_wrapper/load_worker.ts
@@ -3,7 +3,7 @@ import * as Comlink from "comlink";
 export async function initWasm() {
   console.log("init()");
 
-  let worker = new Worker(new URL("./wasm_worker.js", import.meta.url), {
+  const worker = new Worker(new URL("./wasm_worker.js", import.meta.url), {
     type: "module",
   });
 
