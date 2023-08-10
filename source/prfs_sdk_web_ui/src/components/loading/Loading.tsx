3206d354823c8ef16cc09465b0389b0c53eb68d7
Elden Park <eldeniyenden@gmail.com>
2023-08-10 14:23:15 +0900

    o

---
+++ source/prfs_sdk_web_ui/src/components/loading/Loading.tsx
@@ -0,0 +1,15 @@
+// import React from "react";
+
+import { useContext, type Component } from "solid-js";
+import styles from "./Loading.module.scss";
+import { I18nContext } from "@/contexts/i18n";
+
+const Loading: Component<LoadingProps> = () => {
+  const i18n = useContext(I18nContext);
+
+  return <div class={styles.wrapper}>{i18n.loading}</div>;
+};
+
+export default Loading;
+
+export interface LoadingProps {}
---
+++ source/prfs_sdk_web_ui/src/pages/proof_gen/ProofGen.tsx
@@ -0,0 +1,113 @@
+// import React from "react";
+// import { useRouter, useSearchParams } from "next/navigation";
+import { PrfsProofType } from "@taigalabs/prfs-entities/bindings/PrfsProofType";
+import * as prfsApi from "@taigalabs/prfs-api-js";
+import { MsgType, sendMsgToParent } from "@taigalabs/prfs-sdk-web";
+import { useSearchParams } from "@solidjs/router";
+
+import styles from "./Home.module.scss";
+import { I18nContext } from "@/contexts/i18n";
+import CreateProofForm from "@/components/create_proof_form/CreateProofForm";
+import DefaultLayout from "@/layouts/default_layout/DefaultLayout";
+import Loading from "@/components/loading/Loading";
+import { Component, useContext } from "solid-js";
+
+const PARENT_MSG_HANDLER = {
+  registered: false,
+};
+
+const ProofGen: Component<ProofGenProps> = ({ params }) => {
+  const i18n = useContext(I18nContext);
+
+  const [data, setData] = React.useState();
+  const searchParams = useSearchParams();
+  const [proofType, setProofType] = React.useState<PrfsProofType>();
+
+  useParentMsgHandler(setData);
+
+  React.useEffect(() => {
+    async function fn() {
+      const reply = await sendMsgToParent({
+        type: MsgType.HANDSHAKE,
+        payload: "Hi",
+      });
+
+      console.log("parents reply", reply);
+    }
+
+    fn().then();
+    // window.parent.postMessage(
+    //   {
+    //     type: MsgType.HANDSHAKE,
+    //   },
+    //   "*"
+    // );
+  }, []);
+
+  React.useEffect(() => {
+    async function fn() {
+      let proofTypeId = searchParams.get("proofTypeId");
+      console.log("proofTypeId: %s", proofTypeId);
+
+      if (proofTypeId) {
+        const { payload } = await prfsApi.getPrfsProofTypes({
+          page: 0,
+          proof_type_id: proofTypeId,
+        });
+
+        if (payload.prfs_proof_types.length > 0) {
+          setProofType(payload.prfs_proof_types[0]);
+        } else {
+          console.log("PrfsProofType not found");
+        }
+      }
+    }
+
+    fn().then();
+  }, [searchParams, setProofType]);
+
+  return (
+    <DefaultLayout>
+      {proofType ? <CreateProofForm proofType={proofType} /> : <Loading />}
+    </DefaultLayout>
+  );
+};
+
+export default ProofGen;
+
+export interface ProofGenProps {
+  params: {
+    proofTypeId: string;
+  };
+}
+
+function useParentMsgHandler(setData) {
+  React.useEffect(() => {
+    if (!PARENT_MSG_HANDLER.registered) {
+      console.log("Attaching parent msg handler");
+
+      window.addEventListener("message", (ev: MessageEvent) => {
+        if (ev.ports.length > 0) {
+          console.log("parent says: %o, ports: %o", ev.data, ev.ports);
+
+          const type: MsgType = ev.data.type;
+
+          ev.ports[0].postMessage({ result: `${ev.data} back` });
+        }
+
+        // console.log(44, ports);
+
+        // switch (type) {
+        //   case MsgType.GET_SIGNER_RESPONSE:
+        //     window.postMessage({});
+        //     break;
+
+        //   default:
+        //   // console.error(`Cannot handle this msg type, type: ${type}`);
+        // }
+      });
+
+      PARENT_MSG_HANDLER.registered = true;
+    }
+  }, []);
+}
