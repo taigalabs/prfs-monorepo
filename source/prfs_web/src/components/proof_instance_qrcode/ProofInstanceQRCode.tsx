ae6333aaaee13d84cc572cb09fa786d4b16f1548
Elden Park <eldeniyenden@gmail.com>
2023-08-18 18:57:32 +0900

    o

--- source/prfs_web/src/components/proof_instance_qrcode/ProofInstanceQRCode.tsx
+++ source/prfs_web/src/components/proof_instance_qrcode/ProofInstanceQRCode.tsx
@@ -4,21 +4,32 @@ import QRCode from "qrcode";
 import { useRouter } from "next/navigation";
 import { PrfsProofType } from "@taigalabs/prfs-entities/bindings/PrfsProofType";
 
-import styles from "./ProofInstanceDetailTable.module.scss";
+import styles from "./ProofInstanceQRCode.module.scss";
 import { i18nContext } from "@/contexts/i18n";
 import { PrfsProofInstance } from "@taigalabs/prfs-entities/bindings/PrfsProofInstance";
 import Table, { TableBody, TableHeader, TableRecordData, TableRow } from "../table/Table";
 
 const ProofInstanceQRCode: React.FC<ProofInstanceQRCodeProps> = ({ proofInstance }) => {
   const i18n = React.useContext(i18nContext);
+  const canvasRef = React.useRef<HTMLCanvasElement>(null);
 
   React.useEffect(() => {
-    QRCode.toString("I am a pony!", { type: "terminal" }, function (_, url) {
-      console.log(33, url);
-    });
-  }, []);
+    async function fn() {
+      if (canvasRef.current) {
+        await QRCode.toCanvas(canvasRef.current, "I am a pony!", {
+          errorCorrectionLevel: "H",
+          width: 180,
+        });
+      }
+    }
+    fn().then();
+  }, [canvasRef]);
 
-  return <div></div>;
+  return (
+    <div className={styles.wrapper}>
+      <canvas ref={canvasRef} />
+    </div>
+  );
 };
 
 export default ProofInstanceQRCode;
