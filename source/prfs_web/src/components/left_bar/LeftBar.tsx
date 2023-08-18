ca73747930d3443947711178ff6a43c332fe809e
Elden Park <eldeniyenden@gmail.com>
2023-08-18 23:32:16 +0900

    o

--- source/prfs_web/src/components/left_bar/LeftBar.tsx
+++ source/prfs_web/src/components/left_bar/LeftBar.tsx
@@ -6,6 +6,7 @@ import styles from "./LeftBar.module.scss";
 import { i18nContext } from "@/contexts/i18n";
 import ActiveLink from "@/components/active_link/ActiveLink";
 import ProjectMeta from "../project_meta/ProjectMeta";
+import { paths } from "@/routes/path";
 
 const Leftbar: React.FC<any> = () => {
   const i18n = React.useContext(i18nContext);
@@ -15,7 +16,7 @@ const Leftbar: React.FC<any> = () => {
       <div className={styles.topSection}>
         <ul>
           <li>
-            <ActiveLink href="/#proof_wizard" exact activeClassName={styles.activeLink}>
+            <ActiveLink href={paths.proof__proof_wizard} exact activeClassName={styles.activeLink}>
               <p>{i18n.proof_wizard}</p>
               <p className={styles.menuSublabel}>{i18n.proof_wizard_sublabel}</p>
             </ActiveLink>
@@ -25,7 +26,7 @@ const Leftbar: React.FC<any> = () => {
       <div className={styles.homeSection}>
         <ul>
           <li>
-            <ActiveLink href="/" exact activeClassName={styles.activeLink}>
+            <ActiveLink href={paths.proof} exact activeClassName={styles.activeLink}>
               {i18n.home}
             </ActiveLink>
           </li>
@@ -35,12 +36,12 @@ const Leftbar: React.FC<any> = () => {
         <ul>
           <li className={styles.category}>{i18n.proofs.toUpperCase()}</li>
           <li>
-            <ActiveLink href="/proofs" activeClassName={styles.activeLink}>
+            <ActiveLink href={paths.proof__proof_instances} activeClassName={styles.activeLink}>
               {i18n.proof_instances}
             </ActiveLink>
           </li>
           <li>
-            <ActiveLink href="/proof_types" activeClassName={styles.activeLink}>
+            <ActiveLink href={paths.proof__proof_types} activeClassName={styles.activeLink}>
               {i18n.proof_types}
             </ActiveLink>
           </li>
@@ -50,17 +51,17 @@ const Leftbar: React.FC<any> = () => {
         <ul>
           <li className={styles.category}>{i18n.circuits.toUpperCase()}</li>
           <li>
-            <ActiveLink href="/circuits" activeClassName={styles.activeLink}>
+            <ActiveLink href={paths.proof__circuits} activeClassName={styles.activeLink}>
               {i18n.circuits}
             </ActiveLink>
           </li>
           <li>
-            <ActiveLink href="/drivers" activeClassName={styles.activeLink}>
+            <ActiveLink href={paths.proof__circuit_drivers} activeClassName={styles.activeLink}>
               {i18n.circuit_drivers}
             </ActiveLink>
           </li>
           <li>
-            <ActiveLink href="/circuit_types" activeClassName={styles.activeLink}>
+            <ActiveLink href={paths.proof__circuit_types} activeClassName={styles.activeLink}>
               {i18n.circuit_types}
             </ActiveLink>
           </li>
@@ -72,12 +73,12 @@ const Leftbar: React.FC<any> = () => {
             <span>{i18n.references.toUpperCase()}</span>
           </li>
           <li>
-            <ActiveLink href="/sets" activeClassName={styles.activeLink}>
+            <ActiveLink href={paths.proof__sets} activeClassName={styles.activeLink}>
               {i18n.sets}
             </ActiveLink>
           </li>
           <li>
-            <ActiveLink href="/#dynamic_sets" activeClassName={styles.activeLink}>
+            <ActiveLink href={paths.proof__dynamic_sets} activeClassName={styles.activeLink}>
               {i18n.dynamic_sets}
             </ActiveLink>
           </li>
