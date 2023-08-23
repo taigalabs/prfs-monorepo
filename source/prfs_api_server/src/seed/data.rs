838f9f657163b842642bfbb559361fc4e7a142b4
Elden Park <eldeniyenden@gmail.com>
2023-08-24 07:21:11 +0900

    o

--- source/prfs_api_server/src/seed/data.rs
+++ source/prfs_api_server/src/seed/data.rs
@@ -43,7 +43,10 @@ pub async fn upload(db: &Database2) {
     let circuit_types = load_circuit_types();
     println!("circuit_types: {:#?}", circuit_types);
 
-    for circuit_type in circuit_types.values() {}
+    for circuit_type in circuit_types.values() {
+        db_apis::insert_prfs_circuit_type(&mut tx, circuit_type).await;
+    }
+
     // let circuit_input_types = load_circuit_input_types();
     // println!("circuit_input_types: {:#?}", circuit_input_types);
 
