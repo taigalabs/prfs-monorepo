47dfb69e5ae139a916b565d4cdce12d8f0902fdd
Elden Park <eldeniyenden@gmail.com>
2023-08-25 04:29:32 +0900

    o

--- source/prfs_web/src/components/proof_banner/ProofBanner.tsx
+++ source/prfs_web/src/components/proof_banner/ProofBanner.tsx
@@ -3,25 +3,40 @@ import Link from "next/link";
 import QRCode from "qrcode";
 import { useRouter } from "next/navigation";
 import { PrfsProofInstanceSyn1 } from "@taigalabs/prfs-entities/bindings/PrfsProofInstanceSyn1";
+import { PublicInputMeta } from "@taigalabs/prfs-entities/bindings/PublicInputMeta";
 
 import styles from "./ProofBanner.module.scss";
 import { i18nContext } from "@/contexts/i18n";
 import { paths } from "@/paths";
 import ProofImage from "../proof_image/ProofImage";
 import ProofInstanceQRCode from "../proof_instance_qrcode/ProofInstanceQRCode";
-import { accessObj } from "@/functions/obj_access";
 
 const ProofBanner: React.FC<ProofBannerProps> = ({ proofInstance }) => {
   const i18n = React.useContext(i18nContext);
 
-  console.log(11, proofInstance);
+  const prioritizedValues = React.useMemo(() => {
+    console.log(11, proofInstance);
 
-  // const { prioritized_input_accessors } = proofInstance;
+    const { public_inputs_meta, public_inputs } = proofInstance;
 
-  // for (const accessor of prioritized_input_accessors) {
-  //   const a = accessObj(accessor, proofInstance.public_inputs);
-  //   console.log(22, a);
-  // }
+    let accessors = [];
+    let values = [];
+    for (const meta of public_inputs_meta as PublicInputMeta[]) {
+      if (meta.show_priority === 0) {
+        accessors.push(meta.name);
+      }
+    }
+
+    for (const accessor of accessors) {
+      if (public_inputs[accessor]) {
+        values.push(public_inputs[accessor]);
+      }
+    }
+
+    return values;
+  }, [proofInstance]);
+
+  console.log(11, prioritizedValues);
 
   return (
     <div className={styles.wrapper}>
@@ -30,7 +45,8 @@ const ProofBanner: React.FC<ProofBannerProps> = ({ proofInstance }) => {
       </div>
       <div>
         <div className={styles.expression}>{proofInstance.expression}</div>
-        <div>power</div>
+        <div>{prioritizedValues.join(",")}</div>
+        <div>url</div>
       </div>
       {/* <div className={styles.right}> */}
       {/*   <ProofInstanceQRCode proofInstance={proofInstance} /> */}
--- source/prfs_web/src/functions/obj_access.ts
+++
@@ -1,4 +0,0 @@
-export function accessObj(path: any, obj: any, separator = ".") {
-  var properties = Array.isArray(path) ? path : path.split(separator);
-  return properties.reduce((prev: any, curr: any) => prev?.[curr], obj);
-}
