55441dea1b73f5a1aa2ba6cfdd6fe17ce1b13c33
Elden Park <eldeniyenden@gmail.com>
2023-08-25 02:04:58 +0900

    o

--- source/prfs_web/src/components/circuit_detail_table/CircuitDetailTable.tsx
+++ source/prfs_web/src/components/circuit_detail_table/CircuitDetailTable.tsx
@@ -54,30 +54,24 @@ const columnHelper = createColumnHelper<Person>();
 const columns = [
   columnHelper.accessor("firstName", {
     cell: info => info.getValue(),
-    footer: info => info.column.id,
   }),
   columnHelper.accessor(row => row.lastName, {
     id: "lastName",
     cell: info => <i>{info.getValue()}</i>,
     header: () => <span>Last Name</span>,
-    footer: info => info.column.id,
   }),
   columnHelper.accessor("age", {
     header: () => "Age",
     cell: info => info.renderValue(),
-    footer: info => info.column.id,
   }),
   columnHelper.accessor("visits", {
     header: () => <span>Visits</span>,
-    footer: info => info.column.id,
   }),
   columnHelper.accessor("status", {
     header: "Status",
-    footer: info => info.column.id,
   }),
   columnHelper.accessor("progress", {
     header: "Profile Progress",
-    footer: info => info.column.id,
   }),
 ];
 
