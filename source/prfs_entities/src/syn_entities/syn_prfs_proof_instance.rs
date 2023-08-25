cc2bdfb99bb75d6b2b043dd291d0181f3494f4f6
Elden Park <eldeniyenden@gmail.com>
2023-08-25 12:38:27 +0900

    o

--- source/prfs_entities/src/syn_entities/syn_prfs_proof_instance.rs
+++ source/prfs_entities/src/syn_entities/syn_prfs_proof_instance.rs
@@ -2,22 +2,24 @@ use crate::entities::PublicInputMeta;
 use chrono::{DateTime, Utc};
 use serde::{Deserialize, Serialize};
 use ts_rs::TS;
+use uuid::Uuid;
 
 #[derive(Debug, Serialize, Deserialize, TS)]
 #[ts(export)]
 pub struct PrfsProofInstanceSyn1 {
     #[ts(type = "string")]
-    pub proof_instance_id: uuid::Uuid,
+    pub proof_instance_id: Uuid,
 
     #[ts(type = "string")]
-    pub proof_type_id: uuid::Uuid,
+    pub proof_type_id: Uuid,
 
     pub proof: Vec<u8>,
     pub short_id: String,
     pub expression: String,
     pub img_url: Option<String>,
     pub img_caption: Option<String>,
-    pub circuit_id: String,
+    #[ts(type = "string")]
+    pub circuit_id: Uuid,
     pub circuit_driver_id: String,
     pub proof_desc: String,
     pub proof_label: String,
