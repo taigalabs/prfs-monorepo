a22be9975e3e386578f2b436ff3542cce016613a
Elden Park <eldeniyenden@gmail.com>
2023-09-07 06:00:36 +0900

    o

--- source/webapp_generator/src/components/create_proof_form/CreateProofForm.tsx
+++ source/webapp_generator/src/components/create_proof_form/CreateProofForm.tsx
@@ -4,16 +4,19 @@ import { useRouter } from "next/navigation";
 import styles from "./CreateProofForm.module.scss";
 import { paths } from "@/paths";
 import { i18nContext } from "@/contexts/i18n";
-import DefaultLayout from "@/layouts/default_layout/DefaultLayout";
-import useLocalWallet from "@/hooks/useLocalWallet";
-import Masthead from "@/components/masthead/Masthead";
-import ContentArea from "@/components/content_area/ContentArea";
 
 const CreateProofForm: React.FC = () => {
   const i18n = React.useContext(i18nContext);
-  const router = useRouter();
 
-  return <div className={styles.wrapper}></div>;
+  return (
+    <div className={styles.wrapper}>
+      <div className={styles.proofTypeRow}>
+        <p>{i18n.proof_type}</p>
+        <div>input</div>
+      </div>
+      <div></div>
+    </div>
+  );
 };
 
 export default CreateProofForm;
