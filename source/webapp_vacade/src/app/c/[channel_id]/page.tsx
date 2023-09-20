3a2fd873b1cd5daafddc70328e73da552c731252
Elden Park <eldeniyenden@gmail.com>
2023-09-20 11:10:19 +0900

    o

--- source/webapp_vacade/src/app/c/[channel_id]/page.tsx
+++ source/webapp_vacade/src/app/c/[channel_id]/page.tsx
@@ -13,6 +13,7 @@ import TimelineFeeds from "@/components/timeline_feeds/TimelineFeeds";
 import { paths } from "@/paths";
 import Feeds from "@/components/feeds/Feeds";
 import Feeds2 from "@/components/feeds2/Feeds2";
+import Feeds3 from "@/components/feeds3/Feeds3";
 
 const ChannelPage: React.FC<ChannelPageProps> = ({ params }) => {
   const i18n = React.useContext(i18nContext);
@@ -30,7 +31,7 @@ const ChannelPage: React.FC<ChannelPageProps> = ({ params }) => {
       </ContentLeft>
       <ContentMain>
         <div className={styles.container}>
-          {isPostPage ? <div>333333333333333</div> : <Feeds2 />}
+          {isPostPage ? <div>333333333333333</div> : <Feeds3 />}
         </div>
       </ContentMain>
     </DefaultLayout>
