49ce4dfe169fae218bb79de7f4d432604a98ba33
Elden Park <eldeniyenden@gmail.com>
2023-09-26 19:51:00 +0900

    o

--- source/prfs_react_components/src/wallet_dialog/WalletModal.tsx
+++ source/prfs_react_components/src/wallet_dialog/WalletModal.tsx
@@ -1,14 +1,45 @@
 import React from "react";
-import { useAccount, useConnect, useDisconnect, useEnsName } from "wagmi";
+import { Connector, useAccount, useConnect, useDisconnect, useEnsName } from "wagmi";
 import cn from "classnames";
 
 import styles from "./WalletModal.module.scss";
+
+const ConnectedInfo: React.FC<ConnectedInfoProps> = ({
+  ensName,
+  address,
+  connector,
+  disconnect,
+}) => {
+  return (
+    <div>
+      <div>{ensName ? `${ensName} (${address})` : address}</div>
+      <div>Connected to {connector.name}</div>
+      <button onClick={() => disconnect()}>Disconnect</button>
+    </div>
+  );
+};
 
 const WalletModal = () => {
   const { address, connector, isConnected } = useAccount();
   const { data: ensName } = useEnsName({ address });
   const { connect, connectors, error, isLoading, pendingConnector } = useConnect();
   const { disconnect } = useDisconnect();
+
+  const connectorsElem = React.useMemo(() => {
+    return (
+      <ul className={styles.connectList}>
+        {connectors.map(connector => (
+          <li key={connector.id}>
+            <button disabled={!connector.ready} onClick={() => connect({ connector })}>
+              {connector.name}
+              {!connector.ready && " (unsupported)"}
+              {isLoading && connector.id === pendingConnector?.id && " (connecting)"}
+            </button>
+          </li>
+        ))}
+      </ul>
+    );
+  }, [connectors]);
 
   if (isConnected && connector) {
     return (
@@ -21,19 +52,18 @@ const WalletModal = () => {
   }
 
   return (
-    <div>
-      {connectors.map(connector => (
-        <button
-          disabled={!connector.ready}
-          key={connector.id}
-          onClick={() => connect({ connector })}
-        >
-          {connector.name}
-          {!connector.ready && " (unsupported)"}
-          {isLoading && connector.id === pendingConnector?.id && " (connecting)"}
-        </button>
-      ))}
-
+    <div className={styles.wrapper}>
+      {isConnected && connector ? (
+        <ConnectedInfo
+          ensName={ensName}
+          address={address}
+          connector={connector}
+          disconnect={disconnect}
+        />
+      ) : (
+        connectorsElem
+      )}
+      <div></div>
       {error && <div>{error.message}</div>}
     </div>
   );
@@ -42,3 +72,10 @@ const WalletModal = () => {
 export default WalletModal;
 
 export interface WalletModalProps {}
+
+interface ConnectedInfoProps {
+  ensName: string | null | undefined;
+  address: `0x${string}` | undefined;
+  connector: Connector<any, any>;
+  disconnect: () => void;
+}
