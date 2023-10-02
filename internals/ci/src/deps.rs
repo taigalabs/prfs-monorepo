6cbf10f1db559cf60578ad22ba90dac28841a599
Elden Park <eldeniyenden@gmail.com>
2023-10-03 03:25:01 +0900

    o

--- internals/ci/src/deps.rs
+++ internals/ci/src/deps.rs
@@ -4,7 +4,7 @@ pub const BASH: &str = "bash";
 pub const DOCKER: &str = "docker";
 pub const PRETTIERD: &str = "prettierd";
 pub const JS_ENGINE: &str = "pnpm";
-pub const NODE_VERSION: &str = "v18.17.0";
+pub const NODE_VERSION: &str = "v18";
 pub const WASM_PACK_VERSION: &str = "wasm-pack 0.12.1";
 pub const RUST_NIGHTLY_TOOLCHAIN: &str = "nightly-2023-10-02-x86_64-unknown-linux-gnu";
 
@@ -32,11 +32,10 @@ pub fn check_nodejs() {
         .expect(&format!("{} command failed to start", cmd));
 
     let node_version = String::from_utf8(output.stdout).unwrap();
-    if NODE_VERSION != node_version.trim() {
+    if !node_version.starts_with(NODE_VERSION) {
         panic!(
             "node wrong version, expected: {}, has: {}",
-            NODE_VERSION,
-            node_version.trim(),
+            NODE_VERSION, node_version,
         );
     }
 }
