64b3a7d28d7a21ce0b83eb7c1a1586683e439f12
Elden Park <eldeniyenden@gmail.com>
2023-10-04 20:35:23 +0900

    o

--- source/webapp_proof/src/app/p/[short_id]/page.tsx
+++ source/webapp_proof/src/app/p/[short_id]/page.tsx
@@ -27,29 +27,6 @@ const ShortIdPage: React.FC<ShortIdPageProps> = ({ params }) => {
     redirect(`${paths.proofs}/${proofInstanceId}`);
   }
 
-  // React.useEffect(() => {
-  //   async function fn() {
-  //     if (params.short_id) {
-  //       try {
-  //         const { payload } = await prfsApi2("get_prfs_proof_instance_by_short_id", {
-  //           short_id: params.short_id,
-  //         });
-
-  //         if (payload.prfs_proof_instance) {
-  //           let proofInstanceId = payload.prfs_proof_instance.proof_instance_id;
-  //           router.push(`${paths.proofs}/${proofInstanceId}`);
-  //         } else {
-  //           throw new Error("Response does not contain proof instance");
-  //         }
-  //       } catch (err) {
-  //         console.error(err);
-  //       }
-  //     }
-  //   }
-
-  //   fn().then();
-  // }, [router]);
-
   return <div>{i18n.redirecting}</div>;
 };
 
