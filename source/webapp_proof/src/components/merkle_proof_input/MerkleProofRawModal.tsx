12d6a950c1ebc8d879c2fe75339d3f6e31252af3
Elden Park <eldeniyenden@gmail.com>
2023-10-05 17:39:57 +0900

    o

--- source/webapp_proof/src/components/merkle_proof_input/MerkleProofRawModal.tsx
+++ source/webapp_proof/src/components/merkle_proof_input/MerkleProofRawModal.tsx
@@ -4,9 +4,9 @@ import { CircuitInput } from "@taigalabs/prfs-entities/bindings/CircuitInput";
 import { makePathIndices, makeSiblingPath } from "@taigalabs/prfs-crypto-js";
 import { prfsApi2 } from "@taigalabs/prfs-api-js";
 import { PrfsSet } from "@taigalabs/prfs-entities/bindings/PrfsSet";
-import { SpartanMerkleProof } from "@taigalabs/prfs-driver-spartan-js";
 import Button from "@taigalabs/prfs-react-components/src/button/Button";
 import TextButton from "@taigalabs/prfs-react-components/src/text_button/TextButton";
+import { SpartanMerkleProof } from "@taigalabs/prfs-driver-interface";
 
 import styles from "./MerkleProofRawModal.module.scss";
 import { i18nContext } from "@/contexts/i18n";
