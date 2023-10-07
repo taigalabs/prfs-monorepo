094a7dc91903a338e2bc33a4e223cee0144215ac
Elden Park <eldeniyenden@gmail.com>
2023-10-07 23:17:05 +0900

    o

--- source/webapp_proof/src/app/page.tsx
+++ source/webapp_proof/src/app/page.tsx
@@ -2,15 +2,17 @@
 
 import React from "react";
 import { useRouter } from "next/navigation";
+import Link from "next/link";
+import Image from "next/image";
 
 import styles from "./HomePage.module.scss";
 import { i18nContext } from "@/contexts/i18n";
 import DefaultLayout, { DefaultBody } from "@/layouts/default_layout/DefaultLayout";
 import CreateProofForm from "@/components/create_proof_form/CreateProofForm";
 import Masthead from "@/components/masthead/Masthead";
-import Link from "next/link";
 import { paths } from "@/paths";
 import Logo from "@taigalabs/prfs-react-components/src/logo/Logo";
+import Icon from "./prfs_logo2.svg";
 
 const HomePage: React.FC = () => {
   const i18n = React.useContext(i18nContext);
@@ -20,8 +22,10 @@ const HomePage: React.FC = () => {
       <Masthead variant="mini" />
       <DefaultBody>
         <div className={styles.container}>
-          <div>
-            <Logo variant="simple" appName={i18n.proof} beta />
+          <div className={styles._inner}>
+            <div className={styles.logoContainer}>
+              <Icon />
+            </div>
             <CreateProofForm />
           </div>
         </div>
