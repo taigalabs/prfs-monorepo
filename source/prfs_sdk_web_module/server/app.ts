587ae546b7914955cf709b68bc1959c1c5b31836
Elden Park <eldeniyenden@gmail.com>
2023-10-09 23:27:54 +0900

    o

---
+++ source/prfs_sdk_web_module/server/app.ts
@@ -0,0 +1,38 @@
+import express from "express";
+import cors from "cors";
+import path from "path";
+
+const PORT = 3010;
+const distPath = path.resolve(__dirname, "../dist");
+console.log("distPath", distPath);
+
+export function createApp() {
+  const app = express();
+
+  app.use(cors());
+
+  app.use((_, res, next) => {
+    res.set("Cross-Origin-Embedder-Policy", "require-corp");
+    res.set("Cross-Origin-Opener-Policy", "same-origin");
+    res.set("Cross-Origin-Resource-Policy", "cross-origin");
+    next();
+  });
+
+  app.use("/proof_gen", express.static(distPath));
+
+  app.get("/", (_, res) => {
+    res.send({
+      status: "Ok",
+    });
+  });
+
+  app.post("/api", (_, res) => {
+    res.send({
+      status: "Ok",
+    });
+  });
+
+  app.listen(PORT, () => console.log(`Server listening on port: ${PORT}`));
+
+  return app;
+}
--- source/prfs_sdk_web_module/server/dev.server.ts
+++ source/prfs_sdk_web_module/server/dev.server.ts
@@ -1,38 +1,20 @@
-import express from "express";
-import cors from "cors";
-import middleware from "webpack-dev-middleware";
 import webpack from "webpack";
 import path from "path";
 
 import webpackConfig from "../webpack.config";
-
-const distPath = path.resolve(__dirname, "../dist");
-const PORT = 3010;
+import { createApp } from "./app";
 
 const compiler = webpack(webpackConfig);
 
-const app = express();
-app.use(cors());
+console.log("Start webpack compilation, watching");
+compiler.watch({}, (err, stats) => {
+  if (err) {
+    console.error(err);
+  }
 
-app.use((_, res, next) => {
-  res.set("Cross-Origin-Embedder-Policy", "require-corp");
-  res.set("Cross-Origin-Opener-Policy", "same-origin");
-  res.set("Cross-Origin-Resource-Policy", "cross-origin");
-  next();
+  if (stats) {
+    console.log(stats.toJson("normal"));
+  }
 });
 
-// app.use("/proof_gen", ;
-
-app.get("/", (_, res) => {
-  res.send({
-    status: "Ok",
-  });
-});
-
-app.post("/api", (_, res) => {
-  res.send({
-    status: "Ok",
-  });
-});
-
-app.listen(PORT, () => console.log(`Server listening on port: ${PORT}`));
+createApp();
