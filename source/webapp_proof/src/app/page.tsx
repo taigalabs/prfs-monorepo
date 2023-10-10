41dbc0c552aa1efbeaca703b74a33ff1154c976e
Elden Park <eldeniyenden@gmail.com>
2023-10-11 01:34:59 +0900

    o

--- source/webapp_proof/src/app/page.tsx
+++ source/webapp_proof/src/app/page.tsx
@@ -1,11 +1,11 @@
 import React from "react";
+import ImageLogo from "@taigalabs/prfs-react-components/src/image_logo/ImageLogo";
 
 import styles from "./page.module.scss";
 import DefaultLayout, { DefaultBody, DefaultFooter } from "@/layouts/default_layout/DefaultLayout";
 import CreateProofForm from "@/components/create_proof_form/CreateProofForm";
 import Masthead from "@/components/masthead/Masthead";
 import HomeFooter from "@/components/home_footer/HomeFooter";
-import ImageLogo from "@/components/image_logo/ImageLogo";
 
 const HomePage = () => {
   return (
@@ -15,7 +15,7 @@ const HomePage = () => {
         <div className={styles.container}>
           <div className={styles._inner}>
             <div className={styles.logoContainer}>
-              <ImageLogo />
+              <ImageLogo width={180} />
             </div>
             <CreateProofForm />
           </div>
