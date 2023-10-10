2fe71d6177044783a8f075ebe0949566525cdcdc
Elden Park <eldeniyenden@gmail.com>
2023-10-11 02:35:09 +0900

    o

--- source/docs_website/components/navigation/Navigation.tsx
+++ source/docs_website/components/navigation/Navigation.tsx
@@ -3,45 +3,36 @@ import { Navbar } from "nextra-theme-docs";
 import { Item, PageItem, MenuItem } from "nextra/normalize-pages";
 
 function Navigation(props: NavigationProps) {
+  console.log(1, props);
   // const site = useTurboSite();
 
-  /*
-    Inject a dynamic docs link when NOT on root
-    1. Points to /repo/docs when on /repo
-    2. Points to /pack/docs when on /pack
-  */
-  // const leadingItem = props.items[0];
-  // if (leadingItem?.id !== "contextual-docs" && site) {
-  //   props.items.unshift({
-  //     title: "Docs",
-  //     type: "page",
-  //     route: `/${site}/docs`,
-  //     id: "contextual-docs",
-  //     key: "contextual-docs",
-  //   });
-  // }
-
-  // const lastItem = props.items[props.items.length - 1];
-  // if (lastItem?.id !== "contextual-enterprise") {
-  //   props.items.push({
-  //     title: "Enterprise",
-  //     newWindow: true,
-  //     // https://github.com/shuding/nextra/issues/1028
-  //     route: "enterprise",
-  //     href: `https://vercel.com/${
-  //       site === "repo" ? "solutions/turborepo" : "contact/sales"
-  //     }?utm_source=turbo.build&utm_medium=referral&utm_campaign=header-enterpriseLink`,
-  //     id: "contextual-enterprise",
-  //     key: "contextual-enterprise",
-  //   });
-  // }
-
-  console.log(1, props);
-
-  // remove the top level repo and pack links
-  const headerItems = props.items.filter(item => {
-    return item.name !== "repo" && item.name !== "pack";
-  });
+  const headerItems: (PageItem | MenuItem)[] = [
+    {
+      kind: "MdxPage",
+      name: "docs",
+      route: "/",
+      title: "Documentation",
+      type: "page",
+    },
+    {
+      kind: "MdxPage",
+      href: "https://www.prfs.xyz",
+      name: "prfs",
+      newWindow: true,
+      route: "#",
+      title: "Prfs↗",
+      type: "page",
+    },
+    {
+      kind: "MdxPage",
+      href: "https://github.com/taigalabs/prfs-monorepo",
+      name: "code",
+      newWindow: true,
+      route: "#",
+      title: "Code↗",
+      type: "page",
+    },
+  ];
 
   // items last to override the default
   return <Navbar {...props} items={headerItems} />;
