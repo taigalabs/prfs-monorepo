996bd815be11c501e052746b185c63273671de48
Elden Park <eldeniyenden@gmail.com>
2023-10-10 14:44:14 +0900

    o

--- source/webapp_proof/src/components/image_logo/ImageLogo.tsx
+++ source/webapp_proof/src/components/image_logo/ImageLogo.tsx
@@ -4,7 +4,7 @@ import React from "react";
 import { useRouter } from "next/navigation";
 
 import styles from "./HomePage.module.scss";
-import Icon from "./prfs_logo2_chivo.svg";
+import Icon from "./prfs_logo_chivo_shadow.svg";
 
 const ImageLogo: React.FC = () => {
   return <Icon />;
