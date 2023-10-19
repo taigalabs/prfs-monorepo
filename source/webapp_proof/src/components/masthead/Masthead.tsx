6944ac861d46397973db3072fa0808f467413602
Elden Park <eldeniyenden@gmail.com>
2023-10-19 14:01:17 +0900

    o

--- source/webapp_proof/src/components/masthead/Masthead.tsx
+++ source/webapp_proof/src/components/masthead/Masthead.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import React from "react";
-import Logo from "@taigalabs/prfs-react-components/src/logo/Logo";
+import cn from "classnames";
 import Link from "next/link";
 import ActiveLink from "@taigalabs/prfs-react-components/src/active_link/ActiveLink";
 import PrfsAppsPopover from "@taigalabs/prfs-react-components/src/prfs_apps_popover/PrfsAppsPopover";
@@ -9,18 +9,28 @@ import PrfsAppsPopover from "@taigalabs/prfs-react-components/src/prfs_apps_popo
 import styles from "./Masthead.module.scss";
 import { i18nContext } from "@/contexts/i18n";
 import { paths } from "@/paths";
-
-const searchParamKeys = ["tutorial"];
+import { useSearchParams } from "next/navigation";
 
 const Masthead: React.FC<MastheadProps> = () => {
   const i18n = React.useContext(i18nContext);
+
+  const searchParams = useSearchParams();
+
+  const isTutorial = React.useMemo(() => {
+    if (searchParams.get("tutorial") !== null) {
+      return true;
+    }
+    return false;
+  }, [searchParams]);
 
   return (
     <div className={styles.wrapper}>
       <div className={styles.inner}>
         <ul className={styles.rightGroup}>
           <li>
-            <Link href={`${paths.__}?tutorial`}>{i18n.tutorial}</Link>
+            <Link href={`${paths.__}?tutorial`}>
+              <p className={cn({ [styles.isTutorial]: isTutorial })}>{i18n.tutorial}</p>
+            </Link>
           </li>
           <li>
             <Link href={process.env.NEXT_PUBLIC_DOCS_WEBSITE_ENDPOINT}>{i18n.docs}</Link>
