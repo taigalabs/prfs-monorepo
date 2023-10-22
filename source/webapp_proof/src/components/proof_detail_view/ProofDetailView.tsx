de2981f74a630e5290f7af12eef95c830024f39b
Elden Park <eldeniyenden@gmail.com>
2023-10-23 02:16:41 +0900

    o

--- source/webapp_proof/src/components/proof_detail_view/ProofDetailView.tsx
+++ source/webapp_proof/src/components/proof_detail_view/ProofDetailView.tsx
@@ -3,17 +3,23 @@ import Link from "next/link";
 import { PrfsProofInstanceSyn1 } from "@taigalabs/prfs-entities/bindings/PrfsProofInstanceSyn1";
 import { AiOutlineCopy } from "@react-icons/all-files/ai/AiOutlineCopy";
 import JSONBig from "json-bigint";
+import { PrfsSDK } from "@taigalabs/prfs-sdk-web";
 
 import styles from "./ProofDetailView.module.scss";
 import { i18nContext } from "@/contexts/i18n";
 import { paths } from "@/paths";
 import VerifyProofForm from "@/components/verify_proof_form/VerifyProofForm";
 import { ProveReceipt, ProveResult } from "@taigalabs/prfs-driver-interface";
+import ProofGenElement from "@taigalabs/prfs-sdk-web/src/proof_gen_element/proof_gen_element";
+
+const prfsSDK = new PrfsSDK("prfs-proof");
 
 const JSONbigNative = JSONBig({ useNativeBigInt: true, alwaysParseAsBig: true });
 
 const ProofDetailView: React.FC<ProofDetailViewProps> = ({ proofInstance }) => {
   const i18n = React.useContext(i18nContext);
+  const didTryInitialize = React.useRef(false);
+  const [proofGenElement, setProofGenElement] = React.useState<ProofGenElement | null>(null);
 
   const proveResult = React.useMemo(() => {
     return {
@@ -22,7 +28,37 @@ const ProofDetailView: React.FC<ProofDetailViewProps> = ({ proofInstance }) => {
     } as ProveResult;
   }, [proofInstance]);
 
-  const proofGenElement = React.useMemo(() => {}, []);
+  React.useEffect(() => {
+    async function fn() {
+      if (didTryInitialize.current) {
+        return;
+      }
+      didTryInitialize.current = true;
+
+      // const { circuit_driver_id, driver_properties } = proofInstance.circuit_driver_id;
+
+      // try {
+      //   const elem = await prfsSDK.create("proof-gen", {
+      //     proofTypeId: proofType.proof_type_id,
+      //     circuit_driver_id,
+      //     driver_properties,
+      //     sdkEndpoint: process.env.NEXT_PUBLIC_PRFS_SDK_WEB_ENDPOINT,
+      //     proofGenEventListener: proofGenEventListener,
+      //   });
+
+      //   elem.subscribe(msg => {
+      //     setSystemMsg(msg.data);
+      //   });
+
+      //   setProofGenElement(elem);
+      //   return elem;
+      // } catch (err) {
+      //   setSystemMsg(`Driver init failed, id: ${circuit_driver_id}, err: ${err}`);
+      // }
+    }
+
+    fn().then();
+  }, [proofInstance, setProofGenElement]);
 
   return (
     <div className={styles.wrapper}>
