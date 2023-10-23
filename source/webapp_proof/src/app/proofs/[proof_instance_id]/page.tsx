2388411abf453afb99d51955dd95cdfd531826f4
Elden Park <eldeniyenden@gmail.com>
2023-10-23 21:40:28 +0900

    o

--- source/webapp_proof/src/app/proofs/[proof_instance_id]/page.tsx
+++ source/webapp_proof/src/app/proofs/[proof_instance_id]/page.tsx
@@ -10,6 +10,10 @@ import { PrfsProofInstanceSyn1 } from "@taigalabs/prfs-entities/bindings/PrfsPro
 import ProofBanner from "@taigalabs/prfs-react-components/src/proof_banner/ProofBanner";
 import SocialSharePopover from "@taigalabs/prfs-react-components/src/social_share_popover/SocialSharePopover";
 import { HiOutlineDesktopComputer } from "@react-icons/all-files/hi/HiOutlineDesktopComputer";
+import Link from "next/link";
+import { useMutation } from "wagmi";
+import { GetPrfsProofInstanceByInstanceIdRequest } from "@taigalabs/prfs-entities/bindings/GetPrfsProofInstanceByInstanceIdRequest";
+import ImageLogo from "@taigalabs/prfs-react-components/src/image_logo/ImageLogo";
 
 import styles from "./page.module.scss";
 import { i18nContext } from "@/contexts/i18n";
@@ -18,11 +22,7 @@ import DefaultLayout, { DefaultBody } from "@/layouts/default_layout/DefaultLayo
 import { TopPlaceholder } from "@/components/content_area/ContentArea";
 import { envs } from "@/envs";
 import ProofDetailView from "@/components/proof_detail_view/ProofDetailView";
-import Link from "next/link";
 import Masthead from "@/components/masthead/Masthead";
-import { useMutation } from "wagmi";
-import { GetPrfsProofInstanceByInstanceIdRequest } from "@taigalabs/prfs-entities/bindings/GetPrfsProofInstanceByInstanceIdRequest";
-import ImageLogo from "@taigalabs/prfs-react-components/src/image_logo/ImageLogo";
 import Tutorial from "@/components/tutorial/Tutorial";
 import TutorialStepper from "@/components/tutorial/TutorialStepper";
 
@@ -58,7 +58,7 @@ const ProofInstancePage: React.FC<ProofInstancePageProps> = ({ params }) => {
     fn().then();
   }, [setProofInstance, getPrfsProofInstanceByInstanceIdRequest, setConsoleUrl]);
 
-  const headerLabel = `${i18n.proof_instance} ${params.proof_instance_id}`;
+  // const headerLabel = `${i18n.proof_instance} ${params.proof_instance_id}`;
 
   return (
     <DefaultLayout>
@@ -68,54 +68,7 @@ const ProofInstancePage: React.FC<ProofInstancePageProps> = ({ params }) => {
         <div className={styles.container}>
           <Tutorial />
           {proofInstance ? (
-            <div className={styles.inner}>
-              <div className={styles.header}>
-                <div className={styles.logoContainer}>
-                  <a href={paths.__}>
-                    <ImageLogo width={45} />
-                  </a>
-                </div>
-                <div className={styles.titleRow}>
-                  <p className={styles.headerLabel}>{headerLabel}</p>
-                </div>
-                <div className={styles.buttonRow}>
-                  <ul>
-                    <li>
-                      <Link href={consoleUrl}>
-                        <Button variant="transparent_aqua_blue_1">
-                          <HiOutlineDesktopComputer />
-                          <span>{i18n.console.toUpperCase()}</span>
-                        </Button>
-                      </Link>
-                    </li>
-                  </ul>
-                  <ul>
-                    <li>
-                      <Button variant="transparent_aqua_blue_1">
-                        <AiOutlineCopy />
-                        <span>{i18n.copy_url.toUpperCase()}</span>
-                      </Button>
-                    </li>
-                    <li>
-                      <SocialSharePopover />
-                    </li>
-                  </ul>
-                </div>
-              </div>
-              <div className={styles.content}>
-                <div className={styles.bannerContainer}>
-                  <TutorialStepper steps={[5]}>
-                    <ProofBanner
-                      proofInstance={proofInstance}
-                      webappConsoleEndpoint={envs.NEXT_PUBLIC_WEBAPP_CONSOLE_ENDPOINT}
-                    />
-                  </TutorialStepper>
-                </div>
-                <div className={styles.proofDetailContainer}>
-                  <ProofDetailView proofInstance={proofInstance} />
-                </div>
-              </div>
-            </div>
+            <ProofDetailView proofInstance={proofInstance} />
           ) : (
             <div className={styles.loading}>Loading...</div>
           )}
