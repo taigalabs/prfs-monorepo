26d4c15ba7f52db6f936d29c2fe7ca3826fb95aa
Elden Park <eldeniyenden@gmail.com>
2023-10-25 18:16:14 -0400

    o

--- source/prfs_asset_server/src/bin/upload_s3_asset.rs
+++ source/prfs_asset_server/src/bin/upload_s3_asset.rs
@@ -1,8 +1,17 @@
+use std::path::Path;
+
 use aws_config::meta::region::RegionProviderChain;
-use aws_sdk_s3::Client;
+use aws_sdk_s3::{
+    error::SdkError,
+    operation::put_object::{PutObjectError, PutObjectOutput},
+    primitives::ByteStream,
+    Client,
+};
 use colored::Colorize;
 use dotenvy::dotenv;
-use prfs_asset_server::envs::ENVS;
+use prfs_asset_server::{envs::ENVS, local};
+
+const PRFS_BUCKET: &str = "prfs-asset-1";
 
 #[tokio::main]
 async fn main() -> Result<(), aws_sdk_s3::Error> {
@@ -15,19 +24,43 @@ async fn main() -> Result<(), aws_sdk_s3::Error> {
 
     dotenv().unwrap();
 
+    local::setup_local_assets();
+
     let region_provider = RegionProviderChain::default_provider().or_else("us-east-1");
     let config = aws_config::from_env().region(region_provider).load().await;
     let client = Client::new(&config);
 
-    let resp = client.list_buckets().send().await?;
-    let buckets = resp.buckets().unwrap_or_default();
-    let num_buckets = buckets.len();
-
-    for bucket in buckets {
-        println!("{}", bucket.name().unwrap_or_default());
-    }
-
-    println!("Found {} buckets.", num_buckets);
+    load_buckets(&client).await?;
 
     Ok(())
+}
+
+async fn load_buckets(client: &Client) -> Result<(), aws_sdk_s3::Error> {
+    let resp = client.list_buckets().send().await?;
+    let buckets = resp.buckets().unwrap_or_default();
+
+    for bucket in buckets {
+        println!(
+            "Found a S3 bucket, name: {}",
+            bucket.name().unwrap_or_default()
+        );
+    }
+
+    Ok(())
+}
+
+async fn upload_object(
+    client: &Client,
+    bucket_name: &str,
+    file_name: &str,
+    key: &str,
+) -> Result<PutObjectOutput, SdkError<PutObjectError>> {
+    let body = ByteStream::from_path(Path::new(file_name)).await;
+    client
+        .put_object()
+        .bucket(bucket_name)
+        .key(key)
+        .body(body.unwrap())
+        .send()
+        .await
 }
