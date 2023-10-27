0749762cccce19c2071eef1514936fdc1c6a203b
Elden Park <eldeniyenden@gmail.com>
2023-10-27 11:00:11 -0400

    o

--- source/webapp_proof/src/components/create_proof_form/LogoContainer.tsx
+++ source/webapp_proof/src/components/create_proof_form/LogoContainer.tsx
@@ -5,9 +5,21 @@ import cn from "classnames";
 import styles from "./LogoContainer.module.scss";
 
 const LogoContainer: React.FC<LogoContainerProps> = ({ proofTypeChosen }) => {
+  const [logoWidth, setLogoWidth] = React.useState(140);
+
+  // React.useEffect(() => {
+  //   const { clientWidth } = document.body;
+
+  //   if (clientWidth <= 480) {
+  //     // setLogoWidth(110);
+  //   } else {
+  //     // setLogoWidth(140);
+  //   }
+  // }, [setLogoWidth]);
+
   return (
     <div className={cn({ [styles.wrapper]: true, [styles.proofTypeChosen]: proofTypeChosen })}>
-      <ImageLogo width={proofTypeChosen ? 55 : 140} />
+      <ImageLogo width={proofTypeChosen ? 55 : logoWidth} />
     </div>
   );
 };
