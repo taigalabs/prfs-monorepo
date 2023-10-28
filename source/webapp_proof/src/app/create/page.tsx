568c18bc1cd49eb864dee108ecae6c228a38da4b
Elden Park <eldeniyenden@gmail.com>
2023-10-28 12:35:27 -0400

    o

---
+++ source/webapp_proof/src/app/create/page.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+import styles from "./page.module.scss";
+import DefaultLayout, { DefaultBody, DefaultFooter } from "@/layouts/default_layout/DefaultLayout";
+import CreateProofForm from "@/components/create_proof_form/CreateProofForm";
+import Masthead from "@/components/masthead/Masthead";
+import GlobalFooter from "@/components/global_footer/GlobalFooter";
+import Tutorial from "@/components/tutorial/Tutorial";
+
+const CreatePage = () => {
+  return (
+    <DefaultLayout>
+      <Masthead />
+      <DefaultBody>
+        <div className={styles.container}>
+          <Tutorial />
+          <CreateProofForm />
+        </div>
+      </DefaultBody>
+      <DefaultFooter>
+        <GlobalFooter />
+      </DefaultFooter>
+    </DefaultLayout>
+  );
+};
+
+export default CreatePage;
