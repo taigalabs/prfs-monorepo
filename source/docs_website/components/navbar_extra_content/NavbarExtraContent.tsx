0e2fcd9cd5a9b45521acba6b1c86ff8c7d325214
Elden Park <eldeniyenden@gmail.com>
2023-11-01 01:52:26 +0900

    o

--- source/docs_website/components/navbar_extra_content/NavbarExtraContent.tsx
+++ source/docs_website/components/navbar_extra_content/NavbarExtraContent.tsx
@@ -1,18 +1,24 @@
 import React from "react";
+import { BiLinkExternal } from "@react-icons/all-files/bi/BiLinkExternal";
+import cn from "classnames";
 
 import styles from "./NavbarExtraContent.module.scss";
 
 const NavbarExtraContent: React.FC<NavbarExtraContentProps> = () => {
   return (
-    <div className={styles.wrapper}>
-      <div className="nx-text-sm contrast-more:nx-text-gray-700 contrast-more:dark:nx-text-gray-100 nx-relative -nx-ml-2 nx-hidden nx-whitespace-nowrap nx-p-2 md:nx-inline-block nx-text-gray-600 hover:nx-text-gray-800 dark:nx-text-gray-400 dark:hover:nx-text-gray-200">
-        <a href={process.env.NEXT_PUBLIC_WEBAPP_PROOF_ENDPOINT}>
-          <span>Prfs</span>
-        </a>
-        <a href={process.env.NEXT_PUBLIC_CODE_REPOSITORY_URL}>
-          <span>Code</span>
-        </a>
-      </div>
+    <div
+      className={cn(
+        "nx-text-sm contrast-more:nx-text-gray-700 contrast-more:dark:nx-text-gray-100 nx-relative -nx-ml-2 nx-hidden nx-whitespace-nowrap nx-p-2 md:nx-inline-block nx-text-gray-600 hover:nx-text-gray-800 dark:nx-text-gray-400 dark:hover:nx-text-gray-200",
+        styles.wrapper,
+      )}
+    >
+      <a className={styles.link} href={process.env.NEXT_PUBLIC_WEBAPP_PROOF_ENDPOINT}>
+        <span>Prfs</span>
+        <BiLinkExternal />
+      </a>
+      <a className={styles.link} href={process.env.NEXT_PUBLIC_CODE_REPOSITORY_URL}>
+        <span>Code</span>
+      </a>
     </div>
   );
 };
