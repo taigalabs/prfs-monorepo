f95241d445bde0ef6c37c73ad764664693fc64e5
Elden Park <eldeniyenden@gmail.com>
2023-11-02 19:04:25 +0900

    o

--- source/webapp_proof/src/components/create_proof_module/ProofTypeMeta.tsx
+++ source/webapp_proof/src/components/create_proof_module/ProofTypeMeta.tsx
@@ -7,6 +7,7 @@ import * as marked from "marked";
 import styles from "./ProofTypeMeta.module.scss";
 import { i18nContext } from "@/contexts/i18n";
 import TutorialStepper from "@/components/tutorial/TutorialStepper";
+import CaptionedImg from "@taigalabs/prfs-react-components/src/captioned_img/CaptionedImg";
 
 const ProofTypeMeta: React.FC<ProofTypeMetaProps> = ({ proofType }) => {
   const i18n = React.useContext(i18nContext);
@@ -20,11 +21,13 @@ const ProofTypeMeta: React.FC<ProofTypeMetaProps> = ({ proofType }) => {
 
   return (
     <div className={styles.wrapper}>
-      <div className={styles.section}>
-        <div className={styles.left}></div>
+      <div className={styles.headerSection}>
+        <div className={styles.left}>
+          <CaptionedImg img_url={proofType.img_url} size={28} />
+        </div>
         <div>
-          <p className={styles.title}>{i18n.author}</p>
-          <p>{proofType.author}</p>
+          <p>{proofType.proof_type_id}</p>
+          <p>{`${process.env.NEXT_PUBLIC_WEBAPP_CONSOLE_ENDPOINT}/proof_types/${proofType.proof_type_id}`}</p>
         </div>
       </div>
       <div className={styles.section}>
