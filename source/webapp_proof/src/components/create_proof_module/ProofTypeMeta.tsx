7c329116c1906999f4aadf8c4849f16c5bb4e07e
Elden Park <eldeniyenden@gmail.com>
2023-11-02 15:13:43 +0900

    o

--- source/webapp_proof/src/components/create_proof_module/ProofTypeMeta.tsx
+++ source/webapp_proof/src/components/create_proof_module/ProofTypeMeta.tsx
@@ -1,10 +1,5 @@
 import React from "react";
 import { PrfsProofType } from "@taigalabs/prfs-entities/bindings/PrfsProofType";
-import { CircuitInput } from "@taigalabs/prfs-entities/bindings/CircuitInput";
-import { ProveReceipt } from "@taigalabs/prfs-driver-interface";
-import Button from "@taigalabs/prfs-react-components/src/button/Button";
-import Spinner from "@taigalabs/prfs-react-components/src/spinner/Spinner";
-import ProofGenElement from "@taigalabs/prfs-sdk-web/src/proof_gen_element/proof_gen_element";
 import cn from "classnames";
 
 import styles from "./ProofTypeMeta.module.scss";
@@ -14,7 +9,7 @@ import TutorialStepper from "@/components/tutorial/TutorialStepper";
 const ProofTypeMeta: React.FC<ProofTypeMetaProps> = ({ proofType }) => {
   return (
     <div className={styles.wrapper}>
-      <p className={styles.proofTypeLabel}>About {proofType.label}</p>
+      <p className={styles.title}>About {proofType.label}</p>
     </div>
   );
 };
