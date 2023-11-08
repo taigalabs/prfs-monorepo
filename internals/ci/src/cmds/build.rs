cd429a782297358acdadea1d5d6d8c26b4b35f46
Elden Park <eldeniyenden>
2023-11-08 17:41:32 +0300

    o

--- internals/ci/src/cmds/build.rs
+++ internals/ci/src/cmds/build.rs
@@ -1,3 +1,7 @@
+use clap::ArgMatches;
+use colored::Colorize;
+use serde::{Deserialize, Serialize};
+
 use crate::{
     build_cmd::tasks::{
         build_js_dependencies::BuildJsDependenciesTask,
@@ -8,17 +12,20 @@ use crate::{
     },
     build_handle::BuildHandle,
     paths::PATHS,
-    CiError,
 };
-use clap::ArgMatches;
-use colored::Colorize;
-use serde::{Deserialize, Serialize};
 
 #[derive(Debug, Serialize, Deserialize, PartialEq)]
-#[serde(tag = "type", content = "args")]
-enum BuidlTask {
+struct CiTOML {
+    build_tasks: Vec<BuildTaskLabel>,
+}
+
+#[derive(Debug, Serialize, Deserialize, PartialEq)]
+enum BuildTaskLabel {
     BuildPrfsEntitiesTSBindingTask,
     BuildJsDependenciesTask,
+    CompileCircuitsTask,
+    BuildPrfsDriverSpartanWasmTask,
+    BuildPrfsDriverSpartanJsTask,
 }
 
 pub fn run(sub_matches: &ArgMatches, timestamp: &String) {
@@ -27,16 +34,47 @@ pub fn run(sub_matches: &ArgMatches, timestamp: &String) {
     };
 
     if let Ok(str) = std::fs::read_to_string(PATHS.internals_ci.join("ci.toml")) {
-        println!("str: {}", str);
-        // toml::from_str()
+        println!(
+            "{} ci.toml at: {:?}, parsing",
+            "Found".green(),
+            PATHS.internals_ci.join("ci.toml")
+        );
+        //
+        if let Ok(ci_toml) = toml::from_str::<CiTOML>(&str) {
+            println!("ci.toml: {:?}", ci_toml);
+
+            let mut build_tasks: Vec<Box<dyn BuildTask>> = vec![];
+            for task in ci_toml.build_tasks {
+                match task {
+                    BuildTaskLabel::BuildPrfsEntitiesTSBindingTask => {
+                        build_tasks.push(Box::new(BuildPrfsEntitiesTSBindingTask));
+                    }
+                    BuildTaskLabel::BuildJsDependenciesTask => {
+                        build_tasks.push(Box::new(BuildJsDependenciesTask));
+                    }
+                    BuildTaskLabel::CompileCircuitsTask => {
+                        build_tasks.push(Box::new(CompileCircuitsTask));
+                    }
+                    BuildTaskLabel::BuildPrfsDriverSpartanWasmTask => {
+                        build_tasks.push(Box::new(BuildPrfsDriverSpartanWasmTask));
+                    }
+                    BuildTaskLabel::BuildPrfsDriverSpartanJsTask => {
+                        build_tasks.push(Box::new(BuildPrfsDriverSpartanJsTask))
+                    }
+                }
+            }
+
+            run_tasks(sub_matches, build_tasks, build_handle).expect("Ci failed");
+            return;
+        }
     }
 
     let build_tasks: Vec<Box<dyn BuildTask>> = vec![
-        // Box::new(BuildPrfsEntitiesTSBindingTask),
-        // Box::new(BuildJsDependenciesTask),
-        // Box::new(CompileCircuitsTask),
-        // Box::new(BuildPrfsDriverSpartanWasmTask),
-        // Box::new(BuildPrfsDriverSpartanJsTask),
+        Box::new(BuildPrfsEntitiesTSBindingTask),
+        Box::new(BuildJsDependenciesTask),
+        Box::new(CompileCircuitsTask),
+        Box::new(BuildPrfsDriverSpartanWasmTask),
+        Box::new(BuildPrfsDriverSpartanJsTask),
     ];
 
     run_tasks(sub_matches, build_tasks, build_handle).expect("Ci failed");
