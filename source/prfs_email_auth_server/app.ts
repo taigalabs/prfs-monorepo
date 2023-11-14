a2efa3681670681fb822557c706d268e5704b024
Elden Park <eldeniyenden@gmail.com>
2023-11-14 11:52:17 +0300

    o

--- source/prfs_email_auth_server/app.ts
+++ source/prfs_email_auth_server/app.ts
@@ -1,7 +1,9 @@
 import { google } from "googleapis";
+import fs, { access } from "fs";
 import path from "path";
 
-const KEY_PATH = path.resolve(__dirname, "prfs-auth-key.json");
+const KEY_PATH = path.resolve(__dirname, "prfs_auth_key.json");
+const ACCESS_TOKEN_PATH = path.resolve(__dirname, "prfs_auth_access_token");
 
 (async () => {
   console.log("KEY_PATH: %s", KEY_PATH);
@@ -14,6 +16,16 @@ const KEY_PATH = path.resolve(__dirname, "prfs-auth-key.json");
   });
 
   await authClient.authorize();
+
+  const accessToken = await authClient.getAccessToken();
+  console.log("access token: %s", accessToken);
+
+  if (accessToken.token) {
+    fs.writeFileSync(ACCESS_TOKEN_PATH, accessToken.token);
+    console.log("Successfully created an access token file, path: %s", ACCESS_TOKEN_PATH);
+  } else {
+    throw new Error("token does not exist");
+  }
 
   const gmail = google.gmail({
     version: "v1",
