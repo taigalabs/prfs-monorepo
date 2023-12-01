304ae0f47c077f1b5e00e09e08d5ad2d07128fd9
Elden Park <eldeniyenden@gmail.com>
2023-12-02 03:41:13 +0900

    o

--- source/webapp_proof/src/components/create_id/CreateID.tsx
+++ source/webapp_proof/src/components/create_id/CreateID.tsx
@@ -17,13 +17,10 @@ import SignInModule, {
 import Button from "@taigalabs/prfs-react-components/src/button/Button";
 import Link from "next/link";
 import { useRouter, useSearchParams } from "next/navigation";
+import Fade from "@taigalabs/prfs-react-components/src/fade/Fade";
 
 import { paths } from "@/paths";
 import { IdForm, idFormEmpty, validateIdForm } from "@/functions/validate_id";
-
-function switchCreateIdModule() {
-  return null;
-}
 
 const CreateID: React.FC = () => {
   const i18n = React.useContext(i18nContext);
@@ -32,6 +29,7 @@ const CreateID: React.FC = () => {
 
   const [formData, setFormData] = React.useState<IdForm>(idFormEmpty);
   const [formErrors, setFormErrors] = React.useState<IdForm>(idFormEmpty);
+  const [step, setStep] = React.useState("1");
 
   React.useEffect(() => {
     const step = searchParams.get("step");
@@ -39,10 +37,10 @@ const CreateID: React.FC = () => {
     if (step === null) {
       const search = `?${searchParams.toString()}&step=1`;
       router.push(search);
+    } else {
+      setStep(step);
     }
-    // return step;
-  }, [router, searchParams]);
-  // console.log(33, step);
+  }, [router, searchParams, setStep]);
 
   const handleChangeValue = React.useCallback(
     (ev: React.ChangeEvent<HTMLInputElement>) => {
@@ -77,90 +75,101 @@ const CreateID: React.FC = () => {
     return url;
   }, []);
 
+  const content = React.useMemo(() => {
+    switch (step) {
+      case "1": {
+        return (
+          <Fade>
+            <SignInModuleHeader>
+              <SignInModuleTitle>{i18n.create_zauth_identity}</SignInModuleTitle>
+            </SignInModuleHeader>
+            <SignInModuleInputArea>
+              <div className={styles.inputGroup}>
+                <SignInInputItem
+                  name="email"
+                  placeholder={i18n.email}
+                  error={formErrors.email}
+                  handleChangeValue={handleChangeValue}
+                />
+                <SignInInputItem
+                  name="email_confirm"
+                  placeholder={i18n.confirm}
+                  error={formErrors.email_confirm}
+                  handleChangeValue={handleChangeValue}
+                />
+              </div>
+              <SignInInputGuide>
+                <Link href={emailGuideURL} target="_blank">
+                  {i18n.why_we_ask_for_email}
+                </Link>
+              </SignInInputGuide>
+              <div className={styles.inputGroup}>
+                <SignInInputItem
+                  name="password_1"
+                  placeholder={i18n.password_1}
+                  error={formErrors.password_1}
+                  handleChangeValue={handleChangeValue}
+                  type="password"
+                />
+                <SignInInputItem
+                  name="password_1_confirm"
+                  placeholder={i18n.confirm}
+                  error={formErrors.password_1_confirm}
+                  handleChangeValue={handleChangeValue}
+                  type="password"
+                />
+              </div>
+              <div className={styles.inputGroup}>
+                <SignInInputItem
+                  name="password_2"
+                  placeholder={i18n.password_2}
+                  error={formErrors.password_2}
+                  handleChangeValue={handleChangeValue}
+                  type="password"
+                />
+                <SignInInputItem
+                  name="password_2_confirm"
+                  placeholder={i18n.confirm}
+                  error={formErrors.password_2_confirm}
+                  handleChangeValue={handleChangeValue}
+                  type="password"
+                />
+              </div>
+              <SignInInputGuide>
+                <Link href={emailGuideURL} target="_blank">
+                  {i18n.why_we_ask_for_two_passwords}
+                </Link>
+              </SignInInputGuide>
+            </SignInModuleInputArea>
+            <SignInModuleBtnRow>
+              <Link href={paths.id}>
+                <Button variant="transparent_blue_2" noTransition>
+                  {i18n.already_have_id}
+                </Button>
+              </Link>
+              <Button
+                variant="blue_2"
+                className={styles.nextBtn}
+                noTransition
+                handleClick={handleClickNext}
+                noShadow
+              >
+                {i18n.next}
+              </Button>
+            </SignInModuleBtnRow>
+          </Fade>
+        );
+      }
+      default:
+        <div>Invalid step</div>;
+    }
+  }, [step, handleClickNext, handleChangeValue, formErrors]);
+
   return (
     <div className={styles.wrapper}>
       <SignInModule>
         <SignInModuleLogoArea />
-        <div>
-          {switchCreateIdModule()}
-          <SignInModuleHeader>
-            <SignInModuleTitle>{i18n.create_zauth_identity}</SignInModuleTitle>
-          </SignInModuleHeader>
-          <SignInModuleInputArea>
-            <div className={styles.inputGroup}>
-              <SignInInputItem
-                name="email"
-                placeholder={i18n.email}
-                error={formErrors.email}
-                handleChangeValue={handleChangeValue}
-              />
-              <SignInInputItem
-                name="email_confirm"
-                placeholder={i18n.confirm}
-                error={formErrors.email_confirm}
-                handleChangeValue={handleChangeValue}
-              />
-            </div>
-            <SignInInputGuide>
-              <Link href={emailGuideURL} target="_blank">
-                {i18n.why_we_ask_for_email}
-              </Link>
-            </SignInInputGuide>
-            <div className={styles.inputGroup}>
-              <SignInInputItem
-                name="password_1"
-                placeholder={i18n.password_1}
-                error={formErrors.password_1}
-                handleChangeValue={handleChangeValue}
-                type="password"
-              />
-              <SignInInputItem
-                name="password_1_confirm"
-                placeholder={i18n.confirm}
-                error={formErrors.password_1_confirm}
-                handleChangeValue={handleChangeValue}
-                type="password"
-              />
-            </div>
-            <div className={styles.inputGroup}>
-              <SignInInputItem
-                name="password_2"
-                placeholder={i18n.password_2}
-                error={formErrors.password_2}
-                handleChangeValue={handleChangeValue}
-                type="password"
-              />
-              <SignInInputItem
-                name="password_2_confirm"
-                placeholder={i18n.confirm}
-                error={formErrors.password_2_confirm}
-                handleChangeValue={handleChangeValue}
-                type="password"
-              />
-            </div>
-            <SignInInputGuide>
-              <Link href={emailGuideURL} target="_blank">
-                {i18n.why_we_ask_for_two_passwords}
-              </Link>
-            </SignInInputGuide>
-          </SignInModuleInputArea>
-          <SignInModuleBtnRow>
-            <Link href={paths.id}>
-              <Button variant="transparent_blue_2" noTransition>
-                {i18n.already_have_id}
-              </Button>
-            </Link>
-            <Button
-              variant="blue_2"
-              className={styles.nextBtn}
-              noTransition
-              handleClick={handleClickNext}
-              noShadow
-            >
-              {i18n.next}
-            </Button>
-          </SignInModuleBtnRow>
-        </div>
+        <div>{content}</div>
       </SignInModule>
     </div>
   );
