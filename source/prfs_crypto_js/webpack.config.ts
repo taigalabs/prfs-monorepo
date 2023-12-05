a6fd2cd9a262fc844fec4eee1ff481bbf55970bc
Elden Park <eldeniyenden@gmail.com>
2023-12-05 15:34:23 +0900

    o

--- source/prfs_crypto_js/webpack.config.ts
+++ source/prfs_crypto_js/webpack.config.ts
@@ -78,12 +78,6 @@ const config = {
     new WasmPackPlugin({
       crateDirectory: path.resolve(__dirname, "."),
     }),
-    // Have this example work in Edge which doesn't ship `TextEncoder` or
-    // `TextDecoder` at this time.
-    new webpack.ProvidePlugin({
-      TextDecoder: ["text-encoding", "TextDecoder"],
-      TextEncoder: ["text-encoding", "TextEncoder"],
-    }),
   ],
   mode: "development",
   experiments: {
--- source/prfs_crypto_wasm/src/lib.rs
+++
@@ -1,7 +0,0 @@
-mod api;
-pub mod wasm;
-
-#[cfg(test)]
-mod test;
-
-pub type PrfsDriverSpartanWasmError = Box<dyn std::error::Error + Sync + Send>;
