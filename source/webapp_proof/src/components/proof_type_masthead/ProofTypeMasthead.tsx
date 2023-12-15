62f6ce9c86f07186ade060501c19d05ceac4edcf
Elden Park <eldeniyenden@gmail.com>
2023-12-15 16:20:53 +0900

    o

--- source/webapp_proof/src/components/proof_type_masthead/ProofTypeMasthead.tsx
+++ source/webapp_proof/src/components/proof_type_masthead/ProofTypeMasthead.tsx
@@ -3,7 +3,6 @@
 import React from "react";
 import cn from "classnames";
 import Link from "next/link";
-import PrfsAppsPopover from "@taigalabs/prfs-react-components/src/prfs_apps_popover/PrfsAppsPopover";
 import { BsBook } from "@react-icons/all-files/bs/BsBook";
 import { PrfsProofType } from "@taigalabs/prfs-entities/bindings/PrfsProofType";
 import SearchProofDialog from "@taigalabs/prfs-react-components/src/search_proof_dialog/SearchProofDialog";
@@ -14,15 +13,8 @@ import styles from "./ProofTypeMasthead.module.scss";
 import { i18nContext } from "@/i18n/context";
 import { paths } from "@/paths";
 import { useIsTutorial } from "@/hooks/tutorial";
-import PrfsIdSignInBtn from "../prfs_id_sign_in_btn/PrfsIdSignInBtn";
-import {
-  PrfsAppsPopoverLi,
-  PrfsAppsPopoverUl,
-} from "@taigalabs/prfs-react-components/src/prfs_apps_popover/Modal";
-import { TbCertificate } from "@taigalabs/prfs-react-components/src/tabler_icons/TbCertificate";
-import { TbMathPi } from "@taigalabs/prfs-react-components/src/tabler_icons/TbMathPi";
-import { GrMonitor } from "@react-icons/all-files/gr/GrMonitor";
-import { useUrls } from "@/hooks/useUrls";
+import PrfsIdSignInBtn from "@/components/prfs_id_sign_in_btn/PrfsIdSignInBtn";
+import PrfsAppsPopoverDefault from "@/components/prfs_apps_popover_default/PrfsAppsPopoverDefault";
 
 const ProofTypeMasthead: React.FC<ProofTypeMastheadProps> = ({
   isActivated,
@@ -32,7 +24,6 @@ const ProofTypeMasthead: React.FC<ProofTypeMastheadProps> = ({
 }) => {
   const i18n = React.useContext(i18nContext);
   const isTutorial = useIsTutorial();
-  const { tutorialUrl, accVerrificationUrl } = useUrls();
 
   return (
     <div className={cn({ [styles.wrapper]: true, [styles.isTutorial]: isTutorial })}>
@@ -61,44 +52,7 @@ const ProofTypeMasthead: React.FC<ProofTypeMastheadProps> = ({
               </li>
             </Tooltip>
             <li className={cn(styles.menu, styles.appPopover)}>
-              <PrfsAppsPopover
-                className={styles.popover}
-                isOpenClassName={styles.popoverIsOpen}
-                tooltip={i18n.apps}
-              >
-                <PrfsAppsPopoverUl>
-                  <PrfsAppsPopoverLi>
-                    <a href={process.env.NEXT_PUBLIC_WEBAPP_PROOF_ENDPOINT}>
-                      <span>{i18n.documentation}</span>
-                    </a>
-                  </PrfsAppsPopoverLi>
-                  <li>
-                    <a href={tutorialUrl}>
-                      <span>{i18n.start_tutorial}</span>
-                    </a>
-                  </li>
-                </PrfsAppsPopoverUl>
-                <PrfsAppsPopoverUl>
-                  <PrfsAppsPopoverLi>
-                    <a href={accVerrificationUrl}>
-                      <TbCertificate />
-                      <span>{i18n.account_verification}</span>
-                    </a>
-                  </PrfsAppsPopoverLi>
-                  <PrfsAppsPopoverLi>
-                    <a href={process.env.NEXT_PUBLIC_WEBAPP_PROOF_ENDPOINT}>
-                      <TbMathPi />
-                      <span>{i18n.proof}</span>
-                    </a>
-                  </PrfsAppsPopoverLi>
-                  <PrfsAppsPopoverLi>
-                    <a href={process.env.NEXT_PUBLIC_WEBAPP_CONSOLE_ENDPOINT}>
-                      <GrMonitor />
-                      <span>{i18n.console}</span>
-                    </a>
-                  </PrfsAppsPopoverLi>
-                </PrfsAppsPopoverUl>
-              </PrfsAppsPopover>
+              <PrfsAppsPopoverDefault />
             </li>
             <li className={(styles.menu, styles.signInBtn)}>
               <PrfsIdSignInBtn />
