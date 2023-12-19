f44b4920fe2f6e3681a624ea7a08c57a69a073fb
Elden Park <eldeniyenden@gmail.com>
2023-12-19 12:58:30 +0900

    o

--- source/hyper_utils/src/macros.rs
+++ source/hyper_utils/src/macros.rs
@@ -2,21 +2,45 @@
 macro_rules! generate_api_error_codes {
     (
         $struct: ident,
+        $object_literal: ident,
         $(
             $(#[$docs:meta])*
-            ($code:expr, $konst:ident, $phrase:expr);
+            ($code:expr, $name:ident, $phrase:expr);
         )+
     ) => {
-        pub struct $struct;
-
-        impl $struct {
+        #[allow(non_snake_case)]
+        #[derive(Serialize, Deserialize)]
+        pub struct $struct {
         $(
-            $(#[$docs])*
-            pub const $konst: ApiHandleErrorCode = ApiHandleErrorCode {
-                code: $code,
-                phrase: $phrase,
-            };
+            pub $name: ApiHandleErrorCode,
         )+
         }
+
+        lazy_static::lazy_static! {
+            pub static ref $object_literal: $struct = {
+                // let error_codes_str = include_str!("../../data_api/error_codes.json");
+
+                let ret: $struct = $struct {
+                    $(
+                        $name: ApiHandleErrorCode {
+                            code: $code,
+                            phrase: String::from($phrase),
+                        },
+                    )+
+                };
+
+                ret
+            };
+        }
+
+        // impl $struct {
+        // $(
+        //     $(#[$docs])*
+        //     pub const $name: ApiHandleErrorCode = ApiHandleErrorCode {
+        //         code: $code,
+        //         phrase: String::from($phrase),
+        //     };
+        // )+
+        // }
     }
 }
--- source/prfs_api_server/src/error_codes/mod.rs
+++ source/prfs_api_server/src/error_codes/mod.rs
@@ -3,29 +3,48 @@ use hyper_utils::ApiHandleErrorCode;
 use native_json::json;
 use serde::{Deserialize, Serialize};
 
-// generate_api_error_codes! {
-//     PrfsApiHandleErrorCode,
-//     (2000000, SUCCESS, "Success");
-//     (4000000, UNKNOWN_ERROR, "Unknown error");
-//     (4000001, CANNOT_FIND_USER, "Can't find a user");
-//     (4000002, USER_EXISTS, "User already exists");
-//     (4000003, NO_POLICY_ATTACHED, "User has no policy attached");
+generate_api_error_codes! {
+    PrfsApiErrorCodes,
+    API_ERROR_CODES,
+    (2000000, SUCCESS, "Success");
+    (4000000, UNKNOWN_ERROR, "Unknown error");
+    (4000001, CANNOT_FIND_USER, "Can't find a user");
+    (4000002, USER_ALREADY_EXISTS, "User already exists");
+    (4000003, NO_POLICY_ATTACHED, "User has no policy attached");
+}
+
+// #[allow(non_snake_case)]
+// #[derive(Serialize, Deserialize)]
+// pub struct PrfsApiErrorCodes {
+//     pub UNKNOWN_ERROR: ApiHandleErrorCode,
+//     pub CANNOT_FIND_USER: ApiHandleErrorCode,
+//     pub USER_ALREADY_EXISTS: ApiHandleErrorCode,
+//     pub NO_POLICY_ATTACHED: ApiHandleErrorCode,
 // }
 
-#[allow(non_snake_case)]
-#[derive(Serialize, Deserialize)]
-pub struct PrfsApiErrorCodes {
-    pub UNKNOWN_ERROR: ApiHandleErrorCode,
-    pub CANNOT_FIND_USER: ApiHandleErrorCode,
-    pub USER_ALREADY_EXISTS: ApiHandleErrorCode,
-    pub NO_POLICY_ATTACHED: ApiHandleErrorCode,
-}
+// lazy_static::lazy_static! {
+//     pub static ref API_ERROR_CODES: PrfsApiErrorCodes = {
+//         // let error_codes_str = include_str!("../../data_api/error_codes.json");
 
-lazy_static::lazy_static! {
-    pub static ref API_ERROR_CODES: PrfsApiErrorCodes = {
-        let error_codes_str = include_str!("../../data_api/error_codes.json");
-        let ret: PrfsApiErrorCodes = serde_json::from_str(error_codes_str).unwrap();
+//         let ret: PrfsApiErrorCodes = PrfsApiErrorCodes {
+//             UNKNOWN_ERROR: ApiHandleErrorCode {
+//                 code: 4000000,
+//                 phrase: String::from("Unknown error")
+//             },
+//             CANNOT_FIND_USER: ApiHandleErrorCode {
+//                 code: 4000001,
+//                 phrase: String::from("Can't find a user"),
+//             },
+//             USER_ALREADY_EXISTS: ApiHandleErrorCode {
+//                 code: 4000002,
+//                 phrase: String::from("User already exists"),
+//             },
+//             NO_POLICY_ATTACHED: ApiHandleErrorCode {
+//                 code: 4000003,
+//                 phrase: String::from("User has no policy attached"),
+//             },
+//         };
 
-        ret
-    };
-}
+//         ret
+//     };
+// }
