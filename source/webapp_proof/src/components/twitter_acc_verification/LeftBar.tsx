15081b9c77d7a9ebf472649d5c0003e613e2f2ef
Elden Park <eldeniyenden@gmail.com>
2023-12-19 21:23:59 +0900

    o

--- source/webapp_proof/src/components/twitter_acc_verification/LeftBar.tsx
+++ source/webapp_proof/src/components/twitter_acc_verification/LeftBar.tsx
@@ -3,6 +3,7 @@ import cn from "classnames";
 import Button from "@taigalabs/prfs-react-components/src/button/Button";
 import Link from "next/link";
 import { HiPlus } from "@react-icons/all-files/hi/HiPlus";
+import { usePathname } from "next/navigation";
 
 import styles from "./LeftBar.module.scss";
 import { i18nContext } from "@/i18n/context";
@@ -10,6 +11,15 @@ import { paths } from "@/paths";
 
 const LeftBar: React.FC<LeftBarProps> = () => {
   const i18n = React.useContext(i18nContext);
+  const pathname = usePathname();
+  const name = React.useMemo(() => {
+    const segments = pathname.split("/");
+
+    if (segments.length > 2) {
+      return segments[2];
+    }
+    return null;
+  }, [pathname]);
 
   return (
     <div className={styles.wrapper}>
@@ -28,14 +38,21 @@ const LeftBar: React.FC<LeftBarProps> = () => {
       </ul>
       <ul className={styles.menuList}>
         <li className={cn(styles.menu, styles.twitterMenu)}>
-          <Link href={paths.account_verification__twitter}>
-            <img
-              src="https://d1w1533jipmvi2.cloudfront.net/x-logo-black.png"
-              alt="Twitter"
-              crossOrigin=""
-            />
-            <span>{i18n.x_twitter}</span>
-          </Link>
+          <Button
+            variant="light_blue_1"
+            handleClick={() => {}}
+            className={cn(styles.button, { [styles.isActive]: name === "twitter" })}
+            noShadow
+          >
+            <Link href={paths.account_verification__twitter}>
+              <img
+                src="https://d1w1533jipmvi2.cloudfront.net/x-logo-black.png"
+                alt="Twitter"
+                crossOrigin=""
+              />
+              <span>{i18n.x_twitter}</span>
+            </Link>
+          </Button>
         </li>
       </ul>
     </div>
