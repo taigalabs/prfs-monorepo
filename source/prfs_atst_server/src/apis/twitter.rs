ad156dfc021b85016535ab3f99f1dd6f557cc387
Elden Park <eldeniyenden@gmail.com>
2023-12-22 22:31:18 +0900

    o

--- source/prfs_atst_server/src/apis/twitter.rs
+++ source/prfs_atst_server/src/apis/twitter.rs
@@ -12,8 +12,10 @@ use prfs_entities::apis_entities::{
 use prfs_entities::atst_api_entities::{ScrapeTwitterRequest, ScrapeTwitterResponse};
 use prfs_entities::entities::PrfsIdentity;
 use std::sync::Arc;
+use tokio::net::TcpStream;
 
 use crate::error_codes::API_ERROR_CODE;
+use crate::AtstServerError;
 
 pub async fn scrape_tweet(req: Request<Incoming>, state: Arc<ServerState>) -> ApiHandlerResult {
     let req: ScrapeTwitterRequest = parse_req(req).await;
@@ -33,4 +35,44 @@ pub async fn scrape_tweet(req: Request<Incoming>, state: Arc<ServerState>) -> Ap
     let resp = ApiResponse::new_success(ScrapeTwitterResponse { is_valid: false });
 
     return Ok(resp.into_hyper_response());
+}
+
+async fn fetch_url(url: hyper::Uri) -> Result<(), AtstServerError> {
+    let host = url.host().expect("uri has no host");
+    let port = url.port_u16().unwrap_or(80);
+    let addr = format!("{}:{}", host, port);
+    let stream = TcpStream::connect(addr).await?;
+    let io = TokioIo::new(stream);
+
+    let (mut sender, conn) = hyper::client::conn::http1::handshake(io).await?;
+    tokio::task::spawn(async move {
+        if let Err(err) = conn.await {
+            println!("Connection failed: {:?}", err);
+        }
+    });
+
+    let authority = url.authority().unwrap().clone();
+
+    let req = Request::builder()
+        .uri(url)
+        .header(hyper::header::HOST, authority.as_str())
+        .body(Empty::<Bytes>::new())?;
+
+    let mut res = sender.send_request(req).await?;
+
+    println!("Response: {}", res.status());
+    println!("Headers: {:#?}\n", res.headers());
+
+    // Stream the body, writing each chunk to stdout as we get it
+    // (instead of buffering and printing at the end).
+    while let Some(next) = res.frame().await {
+        let frame = next?;
+        if let Some(chunk) = frame.data_ref() {
+            io::stdout().write_all(&chunk).await?;
+        }
+    }
+
+    println!("\n\nDone!");
+
+    Ok(())
 }
