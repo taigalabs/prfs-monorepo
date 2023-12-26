24c35c1d87ac7717f73975748f3e221970185c19
Elden Park <eldeniyenden@gmail.com>
2023-12-26 19:50:43 +0900

    o

--- source/webapp_shy/src/app/page.tsx
+++ source/webapp_shy/src/app/page.tsx
@@ -1,47 +1,28 @@
 import React, { Suspense } from "react";
-import { redirect, useRouter } from "next/navigation";
-import { useDispatch } from "react-redux";
 
 import styles from "./HomePage.module.scss";
-import { i18nContext } from "@/contexts/i18n";
-import { paths } from "@/paths";
-import useLocalWallet from "@/hooks/useLocalWallet";
-import { useAppSelector } from "@/state/hooks";
 import DefaultLayout from "@/components/layouts/default_layout/DefaultLayout";
 import { ContentLeft, ContentMain } from "@/components/content_area/ContentArea";
 import LeftBar from "@/components/left_bar/LeftBar";
-// import TimelineFeeds from "@/components/timeline_feeds/TimelineFeeds";
 import TimelineFeeds2 from "@/components/timeline_feeds2/TimelineFeeds2";
+import Home from "@/components/home/Home";
 
 const HomePage: React.FC = () => {
-  // const i18n = React.useContext(i18nContext);
-  // const router = useRouter();
-  // const dispatch = useDispatch();
-  // const localPrfsAccount = useAppSelector(state => state.user.localPrfsAccount);
-  // useLocalWallet(dispatch);
-  // React.useEffect(() => {
-  //   if (localPrfsAccount) {
-  //     router.push(`${paths.c}/crypto`);
-  //   } else if (localPrfsAccount === null) {
-  //     router.push(`${paths.sign_in}`);
-  //   }
-  // }, [router, localPrfsAccount]);
-  // redirect(`${paths.c}/crypto`);
-  // return <div>Redirecting...</div>;
   return (
     <DefaultLayout>
-      <ContentLeft>
-        <Suspense>
-          <LeftBar />
-        </Suspense>
-      </ContentLeft>
-      <ContentMain>
-        <div className={styles.container}>
-          <Suspense>
-            <TimelineFeeds2 channelId="default" />
-          </Suspense>
-        </div>
-      </ContentMain>
+      <Home />
+      {/* <ContentLeft> */}
+      {/*   <Suspense> */}
+      {/*     <LeftBar /> */}
+      {/*   </Suspense> */}
+      {/* </ContentLeft> */}
+      {/* <ContentMain> */}
+      {/*   <div className={styles.container}> */}
+      {/*     <Suspense> */}
+      {/*       <TimelineFeeds2 channelId="default" /> */}
+      {/*     </Suspense> */}
+      {/*   </div> */}
+      {/* </ContentMain> */}
     </DefaultLayout>
   );
 };
