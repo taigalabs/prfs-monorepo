01e54856e654f4ed0699f3786590f4a3820fa495
Elden Park <eldeniyenden@gmail.com>
2023-12-27 17:46:11 +0900

    o

--- source/prfs_db_interface/src/shy/shy_posts.rs
+++ source/prfs_db_interface/src/shy/shy_posts.rs
@@ -43,11 +43,12 @@ LIMIT $2
 }
 
 pub async fn insert_shy_post(tx: &mut Transaction<'_, Postgres>, shy_post: &ShyPost) -> uuid::Uuid {
-    let query = "INSERT INTO shy_posts \
-            (post_id, content, channel_id)
-            VALUES ($1, $2, $3) returning post_id";
-
-    // let proof_instance = social_post.get(0).unwrap();
+    let query = r#"
+INSERT INTO shy_posts
+(post_id, content, channel_id)
+VALUES ($1, $2, $3)
+RETURNING post_id
+"#;
 
     let row = sqlx::query(query)
         .bind(&shy_post.post_id)
