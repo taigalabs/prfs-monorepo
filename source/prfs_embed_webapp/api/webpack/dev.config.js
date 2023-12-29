49f454e4b5fefa59d3247989979a2c6975693cee
Elden Park <eldeniyenden@gmail.com>
2023-12-29 22:20:52 +0900

    o

--- source/prfs_embed_webapp/api/webpack/dev.config.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const webpack_config_1 = __importDefault(require("./webpack.config"));
-const devConfig = Object.assign(Object.assign({}, webpack_config_1.default), { mode: "development" });
-exports.default = devConfig;
