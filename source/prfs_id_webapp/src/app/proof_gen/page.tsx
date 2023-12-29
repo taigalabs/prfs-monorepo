ce26fbcd3beb601ffac552f61182684d23828d6f
Elden Park <eldeniyenden@gmail.com>
2023-12-29 16:12:58 +0900

    o

--- source/prfs_id_webapp/src/app/proof_gen/page.tsx
+++ source/prfs_id_webapp/src/app/proof_gen/page.tsx
@@ -5,15 +5,9 @@ import DefaultLayout, {
   DefaultBody,
   DefaultFooter,
 } from "@/components/layouts/default_layout/DefaultLayout";
-import GlobalFooter from "@/components/global_footer/GlobalFooter";
-import SearchProofTypeForm from "@/components/search_proof_type_form/SearchProofTypeForm";
-import SearchProofTypeFormFallback from "@/components/search_proof_type_form/SearchProofTypeFormFallback";
-import TutorialPlaceholder from "@/components/tutorial/TutorialPlaceholder";
-import HomeMasthead from "@/components/home_masthead/HomeMasthead";
-import HomeMastheadFallback from "@/components/home_masthead/HomeMastheadFallback";
 
-const HomePage = () => {
+const ProofGenPage = () => {
   return <div>Wrong URL</div>;
 };
 
-export default HomePage;
+export default ProofGenPage;
