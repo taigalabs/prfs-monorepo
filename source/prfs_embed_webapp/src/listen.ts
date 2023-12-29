510ec4c61482fe4f2590ce1027c3697e93d68d28
Elden Park <eldeniyenden@gmail.com>
2023-12-29 20:52:59 +0900

    o

--- source/prfs_embed_webapp/src/listen.ts
+++ source/prfs_embed_webapp/src/listen.ts
@@ -15,7 +15,7 @@ import {
 } from "@taigalabs/prfs-sdk-web";
 
 // import { initDriver, interpolateSystemAssetEndpoint } from "./circuitDriver";
-import { createProof, verifyProof } from "./functions/proof";
+// import { createProof, verifyProof } from "./functions/proof";
 import { envs } from "./envs";
 
 const ASSET_ACCESS_ENDPOINT = envs.NEXT_PUBLIC_PRFS_ASSET_ACCESS_ENDPOINT;
@@ -25,40 +25,9 @@ const state: ProofGenModuleState = {
 };
 
 async function eventListener(ev: MessageEvent) {
-  const { driver } = state;
-
   if (ev.ports.length > 0) {
     const type: MsgType = ev.data.type;
     // console.log("Msg, type: %s", type);
-
-    switch (type) {
-      case "CREATE_PROOF": {
-        const payload = ev.data.payload as CreateProofPayload;
-        console.log("create proof", payload, driver);
-
-        if (!driver) {
-          return;
-        }
-
-        sendMsgToParent(
-          new Msg("CREATE_PROOF_EVENT", {
-            type: "info",
-            payload: `Start proving... hardware concurrency: ${window.navigator.hardwareConcurrency}`,
-          }),
-        );
-
-        try {
-          const proveReceipt = await createProof(driver, payload, ev => {
-            sendMsgToParent(new Msg("CREATE_PROOF_EVENT", ev.payload as LogEventPayload));
-          });
-          ev.ports[0].postMessage(new Msg("CREATE_PROOF_RESPONSE", proveReceipt));
-        } catch (err) {
-          ev.ports[0].postMessage(new Msg("CREATE_PROOF_RESPONSE", undefined, err));
-        }
-
-        break;
-      }
-    }
   }
 }
 
