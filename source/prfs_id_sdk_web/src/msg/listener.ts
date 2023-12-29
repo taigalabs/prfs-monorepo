97b2e32e2a85e11cc16b955cf4141d75369c9015
Elden Park <eldeniyenden@gmail.com>
2023-12-30 05:50:05 +0900

    o

--- source/prfs_id_sdk_web/src/msg/listener.ts
+++ source/prfs_id_sdk_web/src/msg/listener.ts
@@ -33,7 +33,32 @@ export function setupChildMsgHandler() {
   });
 }
 
-export function setupParentMsgHandler() {}
+export function setupParentMsgHandler() {
+  function listener(ev: MessageEvent) {
+    if (ev.ports.length > 0) {
+      console.log("child msg", ev.data);
+      const data = ev.data as PrfsIdMsg<any>;
+
+      if (data.type) {
+        switch (data.type) {
+          case "HANDSHAKE": {
+            ev.ports[0].postMessage(newPrfsIdMsg("HANDSHAKE_ACK", {}));
+            // resolve(listenerRef);
+            break;
+          }
+          default:
+            console.error(`invalid msg type, ${data.type}`);
+          // reject();
+        }
+      }
+    }
+  }
+
+  if (listenerRef.current === null) {
+    listenerRef.current = listener;
+    window.addEventListener("message", listener);
+  }
+}
 
 export interface ListenerRef {
   current: ((ev: MessageEvent) => void) | null;
