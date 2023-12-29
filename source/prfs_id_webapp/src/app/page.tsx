8b864578b02e65bcd5468704ce76bb9258d8779c
Elden Park <eldeniyenden@gmail.com>
2023-12-29 16:08:44 +0900

    o

--- source/prfs_id_webapp/src/app/page.tsx
+++ source/prfs_id_webapp/src/app/page.tsx
@@ -1,16 +1,6 @@
 import React, { Suspense } from "react";
 
 import styles from "./page.module.scss";
-import DefaultLayout, {
-  DefaultBody,
-  DefaultFooter,
-} from "@/components/layouts/default_layout/DefaultLayout";
-import GlobalFooter from "@/components/global_footer/GlobalFooter";
-import SearchProofTypeForm from "@/components/search_proof_type_form/SearchProofTypeForm";
-import SearchProofTypeFormFallback from "@/components/search_proof_type_form/SearchProofTypeFormFallback";
-import TutorialPlaceholder from "@/components/tutorial/TutorialPlaceholder";
-import HomeMasthead from "@/components/home_masthead/HomeMasthead";
-import HomeMastheadFallback from "@/components/home_masthead/HomeMastheadFallback";
 import { envs } from "@/envs";
 
 const HomePage = () => {
