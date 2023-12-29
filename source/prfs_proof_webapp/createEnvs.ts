b90f156225d51bb3d2d85b0f082084269c1c343a
Elden Park <eldeniyenden@gmail.com>
2023-12-29 23:02:26 +0900

    o

--- source/prfs_proof_webapp/createEnvs.ts
+++ source/prfs_proof_webapp/createEnvs.ts
@@ -3,6 +3,13 @@ import fs from "fs";
 import path from "path";
 import chalk from "chalk";
 import child_process from "child_process";
+import {
+  getGitCommitHash,
+  getPrfsDevEndpoints,
+  getPrfsProdEndpoints,
+  getTimestamp,
+  writeEnvsToDotEnv,
+} from "@taigalabs/prfs-env-js";
 
 import { Envs } from "./src/envs";
 
@@ -10,12 +17,6 @@ const DOT_ENV_PATH = path.resolve(".env");
 
 async function run() {
   console.log("%s createEnvs.ts prfs web launch", chalk.green("Launching"));
-
-  const ts = await getGitTimestamp();
-  createEnvs(ts);
-}
-
-function createEnvs(ts: string) {
   const { values } = parseArgs({
     options: {
       production: {
@@ -24,60 +25,43 @@ function createEnvs(ts: string) {
     },
   });
 
-  console.log("cli args: %j", values);
-
-  const { production } = values;
-
-  const env_dev: Envs = {
-    NEXT_PUBLIC_UPDATE_TIMESTAMP: ts,
-    NEXT_PUBLIC_CODE_REPOSITORY_URL: "https://github.com/taigalabs/prfs-monorepo",
-    NEXT_PUBLIC_TAIGALABS_ENDPOINT: "http://localhost:3060",
-    NEXT_PUBLIC_WEBAPP_CONSOLE_ENDPOINT: "http://localhost:3020",
-    NEXT_PUBLIC_WEBAPP_PROOF_ENDPOINT: "http://localhost:3000",
-    NEXT_PUBLIC_WEBAPP_POLL_ENDPOINT: "http://localhost:3021",
-    NEXT_PUBLIC_PRFS_ID_WEBAPP_ENDPOINT: "http://localhost:3011",
-    NEXT_PUBLIC_PRFS_API_SERVER_ENDPOINT: "http://localhost:4000",
-    NEXT_PUBLIC_PRFS_ASSET_SERVER_ENDPOINT: "http://localhost:4010",
-    NEXT_PUBLIC_PRFS_SDK_WEB_ENDPOINT: "http://localhost:3010",
-    NEXT_PUBLIC_DOCS_WEBSITE_ENDPOINT: "http://localhost:3061",
-  };
-
-  const env_prod: Envs = {
-    NEXT_PUBLIC_UPDATE_TIMESTAMP: ts,
-    NEXT_PUBLIC_CODE_REPOSITORY_URL: "https://github.com/taigalabs/prfs-monorepo",
-    NEXT_PUBLIC_TAIGALABS_ENDPOINT: "https://www.taigalabs.xyz",
-    NEXT_PUBLIC_WEBAPP_CONSOLE_ENDPOINT: "https://console.prfs.xyz",
-    NEXT_PUBLIC_WEBAPP_PROOF_ENDPOINT: "https://www.prfs.xyz",
-    NEXT_PUBLIC_WEBAPP_POLL_ENDPOINT: "https://poll.prfs.xyz",
-    NEXT_PUBLIC_PRFS_ID_WEBAPP_ENDPOINT: "http://id.prfs.xyz",
-    NEXT_PUBLIC_PRFS_API_SERVER_ENDPOINT: "https://api.prfs.xyz",
-    NEXT_PUBLIC_PRFS_ASSET_SERVER_ENDPOINT: "https://asset.prfs.xyz",
-    NEXT_PUBLIC_PRFS_SDK_WEB_ENDPOINT: "https://sdk.prfs.xyz",
-    NEXT_PUBLIC_DOCS_WEBSITE_ENDPOINT: "http://docs.prfs.xyz",
-  };
-
-  const envs = production ? env_prod : env_dev;
-  console.log("Writing envs to %s, envs: %o", DOT_ENV_PATH, envs);
-
-  writeEnvsToDotEnv(envs);
+  console.log("[create-envs] cli args: %j", values);
+  createEnvs(values);
 }
 
-function writeEnvsToDotEnv(envs: Envs) {
-  let ws = fs.createWriteStream(DOT_ENV_PATH);
-
-  for (const [key, val] of Object.entries(envs)) {
-    ws.write(`${key}=${val}\n`);
+function createEnvs(values: CliArgs) {
+  console.log("cli args: %j", values);
+  const { production } = values;
+  let ep;
+  if (production) {
+    ep = getPrfsDevEndpoints();
+  } else {
+    ep = getPrfsProdEndpoints();
   }
 
-  ws.close();
-}
+  const gitCommitHash = getGitCommitHash();
+  const timestamp = getTimestamp();
+  const envs: Envs = {
+    NEXT_PUBLIC_LAUNCH_TIMESTAMP: timestamp,
+    NEXT_PUBLIC_GIT_COMMIT_HASH: gitCommitHash,
+    NEXT_PUBLIC_CODE_REPOSITORY_URL: ep.prfs_code_repository,
+    NEXT_PUBLIC_TAIGALABS_ENDPOINT: ep.taigalabs_website,
+    NEXT_PUBLIC_WEBAPP_CONSOLE_ENDPOINT: ep.prfs_console_webapp,
+    NEXT_PUBLIC_WEBAPP_PROOF_ENDPOINT: ep.prfs_proof_webapp,
+    NEXT_PUBLIC_PRFS_ID_WEBAPP_ENDPOINT: ep.prfs_id_webapp,
+    NEXT_PUBLIC_WEBAPP_POLL_ENDPOINT: ep.prfs_poll_webapp,
+    NEXT_PUBLIC_PRFS_API_SERVER_ENDPOINT: ep.prfs_api_server,
+    NEXT_PUBLIC_PRFS_ASSET_SERVER_ENDPOINT: ep.prfs_asset_server,
+    NEXT_PUBLIC_PRFS_SDK_WEB_ENDPOINT: ep.prfs_sdk_web,
+    NEXT_PUBLIC_DOCS_WEBSITE_ENDPOINT: ep.prfs_docs_website,
+  };
 
-async function getGitTimestamp() {
-  const output = child_process.execSync(
-    `TZ=UTC0 git show --quiet --date=iso-strict --format="%cd"`,
-  );
-
-  return output.toString();
+  console.log("Writing envs to %s, envs: %o", DOT_ENV_PATH, envs);
+  writeEnvsToDotEnv(envs as any, DOT_ENV_PATH);
 }
 
 run().then();
+
+interface CliArgs {
+  production: boolean | undefined;
+}
