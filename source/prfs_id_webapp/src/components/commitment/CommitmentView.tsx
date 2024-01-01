8083690fee71bb3d8ae432f25e33b53eac7a62e3
Elden Park <eldeniyenden@gmail.com>
2024-01-01 21:14:42 +0900

    o

--- source/prfs_id_webapp/src/components/commitment/CommitmentView.tsx
+++ source/prfs_id_webapp/src/components/commitment/CommitmentView.tsx
@@ -37,7 +37,6 @@ enum Status {
 }
 
 const CommitmentView: React.FC<CommitmentViewProps> = ({
-  // handleClickPrev,
   query,
   credential,
   // prfsEmbed,
@@ -46,42 +45,11 @@ const CommitmentView: React.FC<CommitmentViewProps> = ({
   const searchParams = useSearchParams();
   const [status, setStatus] = React.useState(Status.Loading);
   const [errorMsg, setErrorMsg] = React.useState("");
-  const { mutateAsync: prfsIdentitySignInRequest } = useMutation({
-    mutationFn: (req: PrfsIdentitySignInRequest) => {
-      return idApi("sign_in_prfs_identity", req);
-    },
-  });
-  const [commitmentReceipt, setCommitmentReceipt] = React.useState<Record<string, string> | null>(
-    null,
-  );
   const [elem, setElem] = React.useState<React.ReactNode>(null);
 
   React.useEffect(() => {
     async function fn() {
       try {
-        // let elems = [];
-        let receipt: Record<string, string> = {};
-        // for (const cm of commitmentArgs.cms) {
-        //   const { name, preImage, type } = cm;
-
-        //   if (type === CommitmentType.SIG_POSEIDON_1) {
-        //     const sig = await prfsSign(credential.secret_key, preImage);
-        //     const sigBytes = sig.toCompactRawBytes();
-        //     const hashed = await poseidon_2(sigBytes);
-        //     const hashedHex = hexlify(hashed);
-        //     receipt[name] = hashedHex;
-        //     elems.push(
-        //       <CommitmentItem
-        //         key={name}
-        //         name={name}
-        //         val={preImage}
-        //         type={type}
-        //         hashedHex={hashedHex}
-        //       />,
-        //     );
-        //   }
-        // }
-
         console.log("query", query);
         const { name, preImage, type } = query;
         if (type === CommitmentType.SIG_POSEIDON_1) {
@@ -89,7 +57,8 @@ const CommitmentView: React.FC<CommitmentViewProps> = ({
           const sigBytes = sig.toCompactRawBytes();
           const hashed = await poseidon_2(sigBytes);
           const hashedHex = hexlify(hashed);
-          receipt[name] = hashedHex;
+          // receipt[name] = hashedHex;
+          console.log("elem");
           setElem(
             <CommitmentItem
               key={name}
@@ -100,109 +69,16 @@ const CommitmentView: React.FC<CommitmentViewProps> = ({
             />,
           );
         }
-
-        setCommitmentReceipt(receipt);
-        setElem(elem);
       } catch (err) {
         console.error(err);
       }
     }
     fn().then();
-  }, [searchParams, setCommitmentReceipt, setElem, query]);
-
-  React.useEffect(() => {
-    if (commitmentReceipt) {
-      setStatus(Status.Standby);
-    }
-  }, [setStatus, commitmentReceipt]);
+  }, [searchParams, setElem, query]);
 
   console.log("elem", elem);
 
-  // const handleClickSubmit = React.useCallback(async () => {
-  //   if (commitmentArgs && credential && prfsEmbed) {
-  //     const { payload: _signInRequestPayload, error } = await prfsIdentitySignInRequest({
-  //       identity_id: credential.id,
-  //     });
-
-  //     if (error) {
-  //       setErrorMsg(error);
-  //       return;
-  //     }
-
-  //     if (!commitmentReceipt) {
-  //       setErrorMsg("no commitment receipt");
-  //       return;
-  //     }
-
-  //     const payload: CommitmentSuccessPayload = {
-  //       receipt: commitmentReceipt,
-  //     };
-  //     const encrypted = JSON.stringify(
-  //       encrypt(commitmentArgs.publicKey, Buffer.from(JSON.stringify(payload))),
-  //     );
-  //     console.log("receipt: %o, encrypted", commitmentReceipt, encrypted);
-
-  //     try {
-  //       await sendMsgToChild(
-  //         newPrfsIdMsg("COMMITMENT_SUCCESS", {
-  //           appId: commitmentArgs.appId,
-  //           key: commitmentArgs.publicKey,
-  //           value: encrypted,
-  //         }),
-  //         prfsEmbed,
-  //       );
-  //     } catch (err: any) {
-  //       await sendMsgToChild(newPrfsIdErrorMsg("ERROR", err.toString()), prfsEmbed);
-  //       console.error(err);
-  //     }
-  //     window.close();
-  //   }
-  // }, [searchParams, commitmentArgs, credential, setErrorMsg, commitmentReceipt]);
-
-  // return (
-  //   <>
-  //     {status === Status.Loading && (
-  //       <div className={styles.overlay}>
-  //         <Spinner color="#1b62c0" />
-  //       </div>
-  //     )}
-  //     <DefaultTopLabel>{i18n.sign_in_with_prfs_id}</DefaultTopLabel>
-  //     <DefaultInnerPadding>
-  //       <DefaultModuleHeader noTopPadding>
-  //         <DefaultModuleTitle>
-  //           <span className={styles.blueText}>{commitmentArgs.appId}</span> wants you to submit
-  //           commitment (s)
-  //         </DefaultModuleTitle>
-  //       </DefaultModuleHeader>
-  //       <div className={styles.prfsId}>
-  //         <p>{credential.id}</p>
-  //       </div>
-  //       <CommitmentItemList>{commitmentElems}</CommitmentItemList>
-  //       <div className={styles.dataWarning}>
-  //         <p className={styles.title}>Make sure you trust {commitmentArgs.appId} app</p>
-  //         <p className={styles.desc}>{i18n.app_data_sharing_guide}</p>
-  //       </div>
-  //       <DefaultModuleBtnRow className={styles.btnRow}>
-  //         <Button variant="transparent_blue_2" noTransition handleClick={handleClickPrev}>
-  //           {i18n.go_back}
-  //         </Button>
-  //         <Button
-  //           type="button"
-  //           variant="blue_2"
-  //           className={styles.signInBtn}
-  //           noTransition
-  //           handleClick={handleClickSubmit}
-  //           noShadow
-  //         >
-  //           {i18n.submit}
-  //         </Button>
-  //       </DefaultModuleBtnRow>
-  //       <DefaultErrorMsg>{errorMsg}</DefaultErrorMsg>
-  //     </DefaultInnerPadding>
-  //   </>
-  // );
-
-  return elem ? elem : <div></div>;
+  return <>{elem}</>;
 };
 
 export default CommitmentView;
