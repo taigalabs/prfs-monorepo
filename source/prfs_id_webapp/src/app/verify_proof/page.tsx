cbca2e5259bb84cf26e571cebf8972835246de09
Elden Park <eldeniyenden@gmail.com>
2024-01-05 08:30:27 +0900

    o

--- source/prfs_id_webapp/src/app/verify_proof/page.tsx
+++ source/prfs_id_webapp/src/app/verify_proof/page.tsx
@@ -2,14 +2,14 @@ import React, { Suspense } from "react";
 
 import styles from "./page.module.scss";
 import { DefaultLayout, DefaultBody } from "@/components/layouts/default_layout/DefaultLayout";
-import ProofGen from "@/components/proof_gen/ProofGen";
+import VerifyProof from "@/components/verify_proof/VerifyProof";
 
 const VerifyProofPage = () => {
   return (
     <DefaultLayout>
       <DefaultBody>
         <Suspense>
-          <ProofGen />
+          <VerifyProof />
         </Suspense>
       </DefaultBody>
     </DefaultLayout>
---
+++ source/prfs_id_webapp/src/components/verify_proof/VerifyProof.tsx
@@ -0,0 +1,142 @@
+"use client";
+
+import React from "react";
+import Link from "next/link";
+import { useSearchParams } from "next/navigation";
+import Spinner from "@taigalabs/prfs-react-lib/src/spinner/Spinner";
+import { PrfsIdCredential, parseProofGenSearchParams } from "@taigalabs/prfs-id-sdk-web";
+import { usePrfsEmbed } from "@taigalabs/prfs-id-sdk-react";
+
+import styles from "./ProofGen.module.scss";
+import { i18nContext } from "@/i18n/context";
+import {
+  DefaultModule,
+  DefaultForm,
+  DefaultModuleFooter,
+  DefaultTopLabel,
+} from "@/components/default_module/DefaultModule";
+import { envs } from "@/envs";
+import PrfsIdErrorDialog from "@/components/error_dialog/PrfsIdErrorDialog";
+import SignIn from "@/components/sign_in/SignIn";
+// import ProofGenForm from "./ProofGenForm";
+import { useAppDispatch } from "@/state/hooks";
+import { goToStep } from "@/state/tutorialReducer";
+import GlobalFooter from "@/components/global_footer/GlobalFooter";
+import TutorialDefault from "@/components/tutorial_default/TutorialDefault";
+import TutorialPlaceholder from "../tutorial_default/TutorialPlaceholder";
+
+enum ProofGenStep {
+  PrfsIdCredential,
+  Form,
+}
+
+export enum Status {
+  Loading,
+  Standby,
+}
+
+const VerifyProof: React.FC = () => {
+  const i18n = React.useContext(i18nContext);
+  const [status, setStatus] = React.useState(Status.Loading);
+  const [errorMsg, setErrorMsg] = React.useState<string | null>(null);
+  const searchParams = useSearchParams();
+  const dispatch = useAppDispatch();
+  const [step, setStep] = React.useState(ProofGenStep.PrfsIdCredential);
+  const [credential, setCredential] = React.useState<PrfsIdCredential | null>(null);
+  const proofGenArgs = React.useMemo(() => {
+    try {
+      const args = parseProofGenSearchParams(searchParams as URLSearchParams);
+      return args;
+    } catch (err: any) {
+      return null;
+    }
+  }, [searchParams]);
+  const { prfsEmbed, isReady: isPrfsReady } = usePrfsEmbed();
+
+  React.useEffect(() => {
+    if (proofGenArgs) {
+      const { public_key, app_id, tutorial } = proofGenArgs;
+
+      if (!public_key) {
+        setErrorMsg("Invalid URL. 'public_key' is missing. Closing the window");
+      } else if (!app_id) {
+        setErrorMsg("Invalid URL. 'app_id' is missing. Closing the window");
+      } else {
+        if (isPrfsReady) {
+          setStatus(Status.Standby);
+        }
+      }
+
+      if (tutorial) {
+        dispatch(goToStep(tutorial.step));
+      }
+    }
+  }, [searchParams, setStatus, setErrorMsg, setStep, proofGenArgs, isPrfsReady, dispatch]);
+
+  const handleCloseErrorDialog = React.useCallback(() => {
+    window.close();
+  }, []);
+
+  const handleClickPrev = React.useCallback(() => {
+    setStep(ProofGenStep.PrfsIdCredential);
+  }, [setStep]);
+
+  const handleSucceedSignIn = React.useCallback(
+    (credential: PrfsIdCredential) => {
+      if (credential) {
+        setCredential(credential);
+        setStep(ProofGenStep.Form);
+      }
+    },
+    [setCredential, setStep],
+  );
+
+  // const content = React.useMemo(() => {
+  //   if (!proofGenArgs) {
+  //     return null;
+  //   }
+
+  //   switch (step) {
+  //     case ProofGenStep.PrfsIdCredential: {
+  //       return <SignIn appId={proofGenArgs.app_id} handleSucceedSignIn={handleSucceedSignIn} />;
+  //     }
+  //     case ProofGenStep.Form: {
+  //       return credential ? (
+  //         <ProofGenForm
+  //           credential={credential}
+  //           proofGenArgs={proofGenArgs}
+  //           handleClickPrev={handleClickPrev}
+  //           prfsEmbed={prfsEmbed}
+  //         />
+  //       ) : (
+  //         <div>Invalid access. Credential does not exist</div>
+  //       );
+  //     }
+  //     default:
+  //       <div>Invalid step</div>;
+  //   }
+  // }, [step, proofGenArgs]);
+
+  return (
+    <DefaultModule>
+      <DefaultForm>
+        {errorMsg && <PrfsIdErrorDialog errorMsg={errorMsg} handleClose={handleCloseErrorDialog} />}
+        <DefaultTopLabel>{i18n.create_data_with_prfs_id}</DefaultTopLabel>
+        {/* {status === Status.Loading ? ( */}
+        {/*   <div className={styles.overlay}> */}
+        {/*     <Spinner color="#1b62c0" /> */}
+        {/*   </div> */}
+        {/* ) : ( */}
+        {/*   content */}
+        {/* )} */}
+        <TutorialDefault tutorial={proofGenArgs?.tutorial} />
+      </DefaultForm>
+      <DefaultModuleFooter>
+        <GlobalFooter />
+      </DefaultModuleFooter>
+      <TutorialPlaceholder tutorial={proofGenArgs?.tutorial} />
+    </DefaultModule>
+  );
+};
+
+export default VerifyProof;
