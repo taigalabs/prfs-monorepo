1f1e63012e5da264735b572a08917dcef06be747
Elden Park <eldeniyenden@gmail.com>
2024-01-11 20:12:59 +0900

    o

---
+++ source/shy_docs_website/createEnvs.ts
@@ -0,0 +1,76 @@
+import { parseArgs } from "node:util";
+import fs from "fs";
+import path from "path";
+import chalk from "chalk";
+
+import { Envs } from "./envs";
+
+const DOT_ENV_PATH = path.resolve(".env");
+
+function run() {
+  console.log("%s createEnvs.ts prfs web launch", chalk.green("Launching"));
+
+  createEnvs();
+}
+
+function createEnvs() {
+  const { values } = parseArgs({
+    options: {
+      production: {
+        type: "boolean",
+      },
+      teaser: {
+        type: "boolean",
+      },
+    },
+  });
+
+  console.log("cli args: %j", values);
+
+  const { production, teaser } = values;
+
+  const env_dev: Envs = {
+    NEXT_PUBLIC_IS_TEASER: teaser ? "yes" : "no",
+    NEXT_PUBLIC_CODE_REPOSITORY_URL: "https://github.com/taigalabs/prfs-monorepo",
+    NEXT_PUBLIC_TAIGALABS_ENDPOINT: "http://localhost:3060",
+    NEXT_PUBLIC_WEBAPP_CONSOLE_ENDPOINT: "http://localhost:3020",
+    NEXT_PUBLIC_WEBAPP_PROOF_ENDPOINT: "http://localhost:3000",
+    NEXT_PUBLIC_WEBAPP_POLL_ENDPOINT: "http://localhost:3021",
+    NEXT_PUBLIC_PRFS_API_SERVER_ENDPOINT: "http://localhost:4000",
+    NEXT_PUBLIC_PRFS_ASSET_SERVER_ENDPOINT: "http://localhost:4010",
+    NEXT_PUBLIC_PRFS_SDK_WEB_ENDPOINT: "http://localhost:3010",
+    NEXT_PUBLIC_ZAUTH_VERSION: "0.1.0",
+    NEXT_PUBLIC_DOCS_WEBSITE_ENDPOINT: "http://localhost:3061",
+  };
+
+  const env_prod: Envs = {
+    NEXT_PUBLIC_IS_TEASER: teaser ? "yes" : "no",
+    NEXT_PUBLIC_CODE_REPOSITORY_URL: "https://github.com/taigalabs/prfs-monorepo",
+    NEXT_PUBLIC_TAIGALABS_ENDPOINT: "https://www.taigalabs.xyz",
+    NEXT_PUBLIC_WEBAPP_CONSOLE_ENDPOINT: "https://console.prfs.xyz",
+    NEXT_PUBLIC_WEBAPP_PROOF_ENDPOINT: "https://www.prfs.xyz",
+    NEXT_PUBLIC_WEBAPP_POLL_ENDPOINT: "https://poll.prfs.xyz",
+    NEXT_PUBLIC_PRFS_API_SERVER_ENDPOINT: "https://api.prfs.xyz",
+    NEXT_PUBLIC_PRFS_ASSET_SERVER_ENDPOINT: "https://asset.prfs.xyz",
+    NEXT_PUBLIC_PRFS_SDK_WEB_ENDPOINT: "https://sdk.prfs.xyz",
+    NEXT_PUBLIC_ZAUTH_VERSION: "0.1.0",
+    NEXT_PUBLIC_DOCS_WEBSITE_ENDPOINT: "http://docs.prfs.xyz",
+  };
+
+  const envs = production ? env_prod : env_dev;
+  console.log("Writing envs to %s, envs: %o", DOT_ENV_PATH, envs);
+
+  writeEnvsToDotEnv(envs);
+}
+
+function writeEnvsToDotEnv(envs: Envs) {
+  let ws = fs.createWriteStream(DOT_ENV_PATH);
+
+  for (const [key, val] of Object.entries(envs)) {
+    ws.write(`${key}=${val}\n`);
+  }
+
+  ws.close();
+}
+
+run();
---
+++ source/shy_docs_website/next.config.js
@@ -0,0 +1,70 @@
+const dotenv = require("dotenv");
+
+(() => {
+  const envObj = {};
+  dotenv.config({ processEnv: envObj });
+  console.log("Parsing env variables...");
+  console.log(envObj);
+})();
+
+const withNextra = require("nextra")({
+  theme: "nextra-theme-docs",
+  themeConfig: "./theme.config.tsx",
+});
+
+const nextConfig = (phase, { defaultConfig }) => {
+  /** @type {import('next').NextConfig} */
+  const cfg = {
+    webpack: (config, { isServer, dev }) => {
+      config.resolve.fallback = { fs: false };
+
+      const fileLoaderRule = config.module.rules.find(rule => rule.test?.test?.(".svg"));
+
+      config.module.rules.push(
+        // Reapply the existing rule, but only for svg imports ending in ?url
+        {
+          ...fileLoaderRule,
+          test: /\.svg$/i,
+          resourceQuery: /url/, // *.svg?url
+        },
+        // Convert all other *.svg imports to React components
+        {
+          test: /\.svg$/i,
+          issuer: /\.[jt]sx?$/,
+          resourceQuery: { not: /url/ }, // exclude if *.svg?url
+          use: ["@svgr/webpack"],
+        },
+      );
+
+      // Modify the file loader rule to ignore *.svg, since we have it handled now.
+      fileLoaderRule.exclude = /\.svg$/i;
+
+      return config;
+    },
+    images: {
+      remotePatterns: [
+        {
+          protocol: "https",
+          hostname: "**.amazonaws.com",
+          port: "",
+          pathname: "/**",
+        },
+      ],
+    },
+
+    eslint: {
+      ignoreDuringBuilds: true,
+    },
+  };
+
+  return cfg;
+};
+
+const cfg = withNextra(nextConfig);
+
+// withNextra() removes some properties, thus adding after
+cfg.reactStrictMode = true;
+cfg.swcMinify = true;
+cfg.transpilePackages = ["@taigalabs/prfs-api-js", "@taigalabs/prfs-react-lib"];
+
+module.exports = cfg;
