c0bffdd3f303ae57393d70a39d1ca0b7c67c4804
Elden Park <eldeniyenden@gmail.com>
2024-01-12 18:44:41 +0900

    o

--- source/prfs_tls_relayer/src/destinations/infura.rs
+++ source/prfs_tls_relayer/src/destinations/infura.rs
@@ -1,12 +1,33 @@
-use headless_chrome::{Browser, LaunchOptions};
-use prfs_entities::atst_api_entities::TwitterAccValidation;
-use regex::Regex;
-use thiserror::Error;
+use bytes::Bytes;
+use http_body_util::BodyExt;
+
+use http_body_util::Empty;
+use hyper_tls::HttpsConnector;
+use hyper_util::{client::legacy::Client, rt::TokioExecutor};
+use tokio::io::{self, AsyncWriteExt as _};
+use tracing::instrument::WithSubscriber;
 
 use crate::TLSRelayError;
 
 pub async fn fetch() -> Result<(), TLSRelayError> {
     println!("fetch()");
+
+    let https = HttpsConnector::new();
+
+    let client = Client::builder(TokioExecutor::new()).build::<_, Empty<Bytes>>(https);
+
+    let mut res = client.get("https://hyper.rs".parse()?).await?;
+
+    println!("Status: {}", res.status());
+    println!("Headers:\n{:#?}", res.headers());
+
+    while let Some(frame) = res.body_mut().frame().await {
+        let frame = frame?;
+
+        if let Some(d) = frame.data_ref() {
+            io::stdout().write_all(d).await?;
+        }
+    }
 
     Ok(())
 }
