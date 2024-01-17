9406e703163363ea9f83af1c6c2ace80cdc507cf
Elden Park <eldeniyenden@gmail.com>
2024-01-17 10:47:12 +0900

    o

--- source/kfans_api_server/src/lib.rs
+++
@@ -1,7 +0,0 @@
-mod apis;
-pub mod envs;
-mod error_codes;
-pub mod paths;
-pub mod server;
-
-pub type ShyServerError = Box<dyn std::error::Error + Send + Sync>;
--- source/kfans_webapp/src/paths.ts
+++
@@ -1,7 +0,0 @@
-export const paths = {
-  __: "/",
-  generate: "/",
-  account: "/account",
-  account__sign_in: "/account/sign_in",
-  ch: "/ch",
-};
