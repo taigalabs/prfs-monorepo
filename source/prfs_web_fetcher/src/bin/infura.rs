826d7e51bbe6e920019b49a629ebb2b4fa1c63e1
Elden Park <eldeniyenden@gmail.com>
2024-01-18 15:56:08 +0900

    o

--- source/prfs_web_fetcher/src/bin/infura.rs
+++ source/prfs_web_fetcher/src/bin/infura.rs
@@ -2,6 +2,7 @@ use prfs_web_fetcher::destinations::infura;
 
 #[tokio::main]
 pub async fn main() {
-    println!("123123");
-    infura::fetch_asset().await.unwrap();
+    infura::fetch_asset("0xD6E4aA932147A3FE5311dA1b67D9e73da06F9cEf")
+        .await
+        .unwrap();
 }
--- source/prfs_web_fetcher/src/destinations/infura.rs
+++ source/prfs_web_fetcher/src/destinations/infura.rs
@@ -7,57 +7,72 @@ use hyper::header::CONTENT_TYPE;
 use hyper::Method;
 use hyper::Request;
 use hyper::Uri;
-// use hyper_014::Uri;
 use hyper_tls::HttpsConnector;
 use hyper_util::rt::TokioIo;
 use hyper_util::{client::legacy::Client, rt::TokioExecutor};
 use hyper_utils::io::full;
+use rust_decimal::Decimal;
+use serde::Deserialize;
+use serde::Serialize;
 use serde_json::json;
 use tokio::io::{self, AsyncWriteExt as _};
 use tokio::net::{TcpListener, TcpStream};
 
 use crate::WebFetcherError;
 
-pub async fn fetch_asset(wallet_addr: &String) -> Result<(), WebFetcherError> {
-    println!("fetch()");
+#[derive(Serialize, Deserialize, Debug)]
+pub struct InfuraResponse<D> {
+    pub jsonrpc: String,
+    pub id: u32,
+    pub result: D,
+}
 
+#[derive(Serialize, Deserialize, Debug)]
+pub struct CryptoAsset {
+    wallet_addr: String,
+    amount: String,
+    unit: String,
+}
+
+pub async fn fetch_asset<S: AsRef<str> + Serialize>(wallet_addr: S) -> Result<(), WebFetcherError> {
     let https = HttpsConnector::new();
-
     let client = Client::builder(TokioExecutor::new()).build::<_, Full<Bytes>>(https);
-
-    // println!("Headers:\n{:#?}", res.headers());
     let url = Uri::from_static("https://mainnet.infura.io/v3/b92e8750d18a4bfa9d748d03807db92d");
 
-    // let authority = url.authority().unwrap().clone();
-    //
-    let data = json!({
+    let get_block_height = json!({
         "jsonrpc": "2.0",
         "id": 1,
         "method": "eth_blockNumber",
         "params": [],
     });
-
     let req = Request::builder()
-        .uri(url)
+        .uri(&url)
         .method(Method::POST)
         .header(CONTENT_TYPE, "application/json")
-        .body(Full::from(data.to_string()))?;
+        .body(Full::from(get_block_height.to_string()))?;
+    let res = client.request(req).await.unwrap();
+    let data = res.collect().await.unwrap().aggregate();
+    let block_height: InfuraResponse<String> = serde_json::from_reader(data.reader()).unwrap();
 
-    let mut res = client.request(req).await.unwrap();
-    println!("Status: {}", res.status());
-    // let whole_body = res.collect().await.unwrap().aggregate();
-    // let data: String = serde_json::from_reader(whole_body.reader()).unwrap();
-    // // println!("d: {}", data);
-
-    // // res.body_mut().with_current_subscriber
-
-    while let Some(frame) = res.body_mut().frame().await {
-        let frame = frame?;
-
-        if let Some(d) = frame.data_ref() {
-            io::stdout().write_all(d).await?;
-        }
-    }
+    let get_balance = json!({
+        "jsonrpc": "2.0",
+        "id": 1,
+        "method": "eth_getBalance",
+        "params": [wallet_addr, "latest"],
+    });
+    println!("get_balance: {}", get_balance);
+    let req = Request::builder()
+        .uri(&url)
+        .method(Method::POST)
+        .header(CONTENT_TYPE, "application/json")
+        .body(Full::from(get_balance.to_string()))?;
+    let res = client.request(req).await.unwrap();
+    let data = res.collect().await.unwrap().aggregate();
+    let balance: InfuraResponse<String> = serde_json::from_reader(data.reader()).unwrap();
+    println!("balaa: {:?}", balance);
+    let balance = balance.result.trim_start_matches("0x");
+    let bal = Decimal::from_str_radix(balance, 16).unwrap();
+    println!("bal: {}", bal);
 
     Ok(())
 }
