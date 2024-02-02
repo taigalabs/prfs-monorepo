19a7da4f000816b6e6a26f514fb079864c2af1c3
Elden Park <eldeniyenden@gmail.com>
2024-02-02 22:04:56 +0900

    o

--- source/prfs_tree_maker/src/apis2/tree.rs
+++ source/prfs_tree_maker/src/apis2/tree.rs
@@ -22,7 +22,12 @@ pub fn create_leaves(set_elements: &Vec<PrfsSetElement>) -> Result<Vec<[u8; 32]>
         for (idx, d) in data.iter().enumerate() {
             match d.r#type {
                 PrfsSetElementDataType::WalletCm => {
-                    let u = U256::from_be_hex(&d.val);
+                    let val = if d.val.starts_with("0x") {
+                        &d.val[2..]
+                    } else {
+                        &d.val
+                    };
+                    let u = U256::from_be_hex(&val);
                     let bytes = u.to_be_bytes();
                     args[idx] = bytes;
                 }
