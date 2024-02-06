3d0af378112dcd8f58d929d6a4b4e11dd20e0a4b
Elden Park <eldeniyenden@gmail.com>
2024-02-06 21:31:59 +0900

    o

--- source/prfs_embed_webapp/webpack/webpack.config.ts
+++ source/prfs_embed_webapp/webpack/webpack.config.ts
@@ -36,12 +36,11 @@ const config: webpack.Configuration = {
   plugins: [
     new NodePolyfillPlugin(),
     new webpack.DefinePlugin({
-      "process.env.NEXT_PUBLIC_PRFS_SDK_VERSION": str(process.env.NEXT_PUBLIC_PRFS_SDK_VERSION),
       "process.env.NEXT_PUBLIC_PRFS_API_SERVER_ENDPOINT": str(
         process.env.NEXT_PUBLIC_PRFS_API_SERVER_ENDPOINT,
       ),
-      "process.env.NEXT_PUBLIC_PRFS_ASSET_ACCESS_ENDPOINT": str(
-        process.env.NEXT_PUBLIC_PRFS_ASSET_ACCESS_ENDPOINT,
+      "process.env.NEXT_PUBLIC_PRFS_ID_SESSION_SOCKET_ENDPOINT": str(
+        process.env.NEXT_PUBLIC_PRFS_ID_SESSION_SOCKET_ENDPOINT,
       ),
     }),
   ],
