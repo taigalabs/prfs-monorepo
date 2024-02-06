795bf2d679bb8889989260c768a413aa396b7bf4
Elden Park <eldeniyenden@gmail.com>
2024-02-06 20:02:00 +0900

    o

--- source/prfs_api_server/src/server/server.rs
+++ source/prfs_api_server/src/server/server.rs
@@ -3,6 +3,7 @@ use hyper::service::service_fn;
 use hyper::{Request, Response};
 use hyper_util::rt::TokioIo;
 use prfs_common_server_state::ServerState;
+use prfs_db_interface::events::start_listening_to_prfs_id_session_events;
 use std::net::SocketAddr;
 use std::sync::Arc;
 use tokio::net::TcpListener;
@@ -20,9 +21,16 @@ pub async fn make_server(server_state: Arc<ServerState>) -> Result<(), ApiServer
     let mut http = http1::Builder::new();
     http.keep_alive(true);
 
+    let server_state_clone = server_state.clone();
+    tokio::task::spawn(async move {
+        let pool = &server_state_clone.db2.pool;
+        start_listening_to_prfs_id_session_events(&pool)
+            .await
+            .unwrap();
+    });
+
     loop {
         let (stream, _) = listener.accept().await?;
-
         let io = TokioIo::new(stream);
         let server_state = server_state.clone();
 
--- source/prfs_db_interface/src/events/mod.rs
+++ source/prfs_db_interface/src/events/mod.rs
@@ -4,12 +4,24 @@ use crate::DbInterfaceError;
 
 pub const PRFS_ID_SESSION_CHAN: &str = "prfs_id_session_chan";
 
-pub async fn listen_to_prfs_id_session_events(
+pub async fn start_listening_to_prfs_id_session_events(
     pool: &Pool<Postgres>,
 ) -> Result<PgListener, DbInterfaceError> {
-    let mut listener = PgListener::connect_with(&pool).await.unwrap();
+    println!("Start_listening to prfs id session events");
 
+    let mut listener = PgListener::connect_with(&pool).await.unwrap();
     listener.listen(PRFS_ID_SESSION_CHAN).await.unwrap();
+
+    let mut counter = 0usize;
+    loop {
+        let notification = listener.recv().await?;
+        println!("[from recv]: {notification:?}");
+
+        counter += 1;
+        if counter >= 3 {
+            break;
+        }
+    }
 
     Ok(listener)
 }
