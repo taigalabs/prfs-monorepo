3930fdaf0e00de144bc820327cec6b39b480fae4
Elden Park <eldeniyenden@gmail.com>
2024-02-09 22:54:27 +0900

    o

--- source/prfs_proof_webapp/src/paths.ts
+++ source/prfs_proof_webapp/src/paths.ts
@@ -1,10 +1,6 @@
 export const paths = {
   __: "/",
   create: "/create",
-  // console__proof_types: "/console/proof_types",
-  // console__sets: "/console/sets",
-  // console__sets__create: "/console/sets/create",
-  // console__sets__crypto_holders: "/console/sets/crypto_holders",
   attestations: "/attestations",
   attestations__create__twitter: "/attestations/create/twitter",
   attestations__create__crypto_asset_size: "/attestations/create/crypto_asset_size",
