de33a61b1ab0e87557502c717907464c80ffbd05
Elden Park <eldeniyenden@gmail.com>
2024-02-09 23:01:40 +0900

    o

--- source/prfs_proof_webapp/src/components/product_updates/ProductUpdates.tsx
+++ source/prfs_proof_webapp/src/components/product_updates/ProductUpdates.tsx
@@ -10,6 +10,7 @@ import { paths } from "@/paths";
 import { i18nContext } from "@/i18n/context";
 import { MastheadPlaceholder } from "@/components/masthead/Masthead";
 import DocumentView from "@/components/document_view/DocumentView";
+import { envs } from "@/envs";
 
 const ProductUpdates = () => {
   const i18n = React.useContext(i18nContext);
@@ -19,6 +20,16 @@ const ProductUpdates = () => {
       <DocMasthead title={i18n.updates} titleHref={paths.updates} />
       <MastheadPlaceholder />
       <DocumentView>
+        <div className={styles.versionMeta}>
+          <div className={styles.row}>
+            <p>{i18n.build_time}</p>
+            <p>{envs.NEXT_PUBLIC_LAUNCH_TIMESTAMP}</p>
+          </div>
+          <div className={styles.row}>
+            <p>{i18n.commit_hash}</p>
+            <p>{envs.NEXT_PUBLIC_GIT_COMMIT_HASH}</p>
+          </div>
+        </div>
         <Markdown>
           <UpdatesMD />
         </Markdown>
