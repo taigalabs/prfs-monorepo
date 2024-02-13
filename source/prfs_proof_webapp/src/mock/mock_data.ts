9c3918f2e925ebfb067988fa43b38825e24b3fba
Elden Park <eldeniyenden@gmail.com>
2024-02-13 20:48:22 +0900

    o

--- source/prfs_proof_webapp/src/mock/mock_data.ts
+++ source/prfs_proof_webapp/src/mock/mock_data.ts
@@ -1,1 +1,11 @@
-export const MASTER_ACCOUNT_ID = "0x0185fda40467453790648c536ee77f9905490944";
+const masterAccountIds = [
+  "0x0185fda40467453790648c536ee77f9905490944",
+  "0xda6870ea13b66dd273c084e0fba28a68de597986",
+];
+
+export function isMasterAccountId(id?: string) {
+  if (id) {
+    return masterAccountIds.includes(id);
+  }
+  return false;
+}
