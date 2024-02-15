48a90c7f787a0bbaa69c521b0c997d1515c6aca4
Elden Park <eldeniyenden@gmail.com>
2024-02-15 15:14:29 +0900

    o

--- source/prfs_asset_server/src/server/router.rs
+++ source/prfs_asset_server/src/server/router.rs
@@ -1,5 +1,5 @@
 use axum::{
-    extract::State,
+    extract::{MatchedPath, Request, State},
     handler::HandlerWithoutStateExt,
     http::{HeaderValue, Method, StatusCode},
     routing::get,
@@ -9,17 +9,17 @@ use prfs_circuits_circom::CircuitBuildListJson;
 use serde_json::{json, Value};
 use std::fs;
 use tower_http::{cors::CorsLayer, services::ServeDir, trace::TraceLayer};
+use tracing::{info_span, Span};
 use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
 
 use super::ServerState;
-use crate::paths::PATHS;
 
 pub fn route() -> Router {
     let state = ServerState::init();
     let serve_dir = ServeDir::new(&state.circuits_build_path);
 
     tracing_subscriber::fmt()
-        .with_max_level(tracing::Level::DEBUG)
+        .with_max_level(tracing::Level::INFO)
         .init();
 
     Router::new()
@@ -27,7 +27,27 @@ pub fn route() -> Router {
         .nest_service("/assets", serve_dir)
         .with_state(state)
         .fallback_service(handle_404.into_service())
-        .layer(TraceLayer::new_for_http())
+        .layer(
+            TraceLayer::new_for_http()
+                .make_span_with(|request: &Request<_>| {
+                    // Log the matched route's path (with placeholders not filled in).
+                    // Use request.uri() or OriginalUri if you want the real path.
+                    let matched_path = request
+                        .extensions()
+                        .get::<MatchedPath>()
+                        .map(MatchedPath::as_str);
+
+                    info_span!(
+                        "http_request",
+                        method = ?request.method(),
+                        matched_path,
+                        some_other_field = tracing::field::Empty,
+                    )
+                })
+                .on_request(|_request: &Request<_>, span: &Span| {
+                    span.record("power", 1);
+                }),
+        )
         .layer(
             CorsLayer::new()
                 .allow_origin("*".parse::<HeaderValue>().unwrap())
--- source/prfs_asset_server/src/server/state.rs
+++ source/prfs_asset_server/src/server/state.rs
@@ -1,3 +1,4 @@
+use prfs_circuits_circom::CircuitBuildListJson;
 use prfs_entities::sqlx::types::chrono::Utc;
 use serde::{Deserialize, Serialize};
 use std::path::PathBuf;
@@ -6,6 +7,7 @@ use std::path::PathBuf;
 pub struct ServerState {
     pub launched_at: String,
     pub circuits_build_path: PathBuf,
+    pub circuit_built_at_launch: String,
 }
 
 impl ServerState {
@@ -18,9 +20,14 @@ impl ServerState {
             circuits_build_path
         );
 
+        let list_json_path = circuits_build_path.join("list.json");
+        let b = std::fs::read(list_json_path).unwrap();
+        let list_json = serde_json::from_slice::<CircuitBuildListJson>(&b).unwrap();
+
         let launched_at = Utc::now().to_rfc3339();
         let s = ServerState {
             launched_at,
+            circuit_built_at_launch: list_json.timestamp,
             circuits_build_path,
         };
 
