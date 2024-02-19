b152acb413052f4d38377f0506e19d8c14da578d
Elden Park <eldeniyenden@gmail.com>
2024-02-19 17:28:45 +0900

    o

--- source/shy_webapp/src/components/sign_up_modal/SignUpModal.tsx
+++ source/shy_webapp/src/components/sign_up_modal/SignUpModal.tsx
@@ -3,7 +3,7 @@ import cn from "classnames";
 import Button from "@taigalabs/prfs-react-lib/src/button/Button";
 import { useRouter } from "next/navigation";
 import { useMutation } from "@tanstack/react-query";
-import { prfsApi2, prfsApi3 } from "@taigalabs/prfs-api-js";
+import { prfsApi3 } from "@taigalabs/prfs-api-js";
 import { PrfsSignUpRequest } from "@taigalabs/prfs-entities/bindings/PrfsSignUpRequest";
 import Modal from "@taigalabs/prfs-react-lib/src/modal/Modal";
 
