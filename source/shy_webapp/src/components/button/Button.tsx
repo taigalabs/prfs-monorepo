fa7acf26488696bd48120ebb3916b2bbc2bc2990
Elden Park <eldeniyenden@gmail.com>
2024-02-23 15:02:47 +0900

    o

---
+++ source/shy_webapp/src/components/button/Button.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import cn from "classnames";
+
+import styles from "./BoardMenu.module.scss";
+
+const Button: React.FC<ButtonProps> = ({ children, variant }) => {
+  return (
+    <button
+      className={cn(styles.wrapper, {
+        [styles.green_1]: variant === "green_1",
+        [styles.white_1]: variant === "white_1",
+      })}
+      type="button"
+    >
+      {children}
+    </button>
+  );
+};
+
+export default Button;
+
+export interface ButtonProps {
+  children: React.ReactNode;
+  variant: "green_1" | "white_1";
+}
