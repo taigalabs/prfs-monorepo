e47dfb80a2264d4e0a126e02386fa2df449faa1c
Elden Park <eldeniyenden@gmail.com>
2024-02-24 22:54:29 +0900

    o

--- source/prfs_api_js/src/index.ts
+++ source/prfs_api_js/src/index.ts
@@ -2,5 +2,4 @@ export * from "./apis/prfs_api";
 export * from "./apis/prfs_api3";
 export * from "./apis/id_api";
 export * from "./apis/atst_api";
-export * from "./apis/shy_api";
 export * from "./apis/id_session_api";
