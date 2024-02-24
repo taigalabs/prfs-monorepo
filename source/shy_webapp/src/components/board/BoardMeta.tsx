b90aef525ffa28556b32a4ac283c5feffc657d4a
Elden Park <eldeniyenden@gmail.com>
2024-02-25 08:44:44 +0900

    o

--- source/shy_webapp/src/components/board/BoardMeta.tsx
+++ source/shy_webapp/src/components/board/BoardMeta.tsx
@@ -7,10 +7,12 @@ import { shyApi2 } from "@taigalabs/shy-api-js";
 import styles from "./BoardMeta.module.scss";
 import { useI18N } from "@/i18n/hook";
 import Button from "@/components/button/Button";
+import Loading from "../loading/Loading";
+import { Spinner } from "@phosphor-icons/react";
 
 const BoardMeta: React.FC<BoardMetaProps> = ({ channelId }) => {
   const i18n = useI18N();
-  const { data, error } = useQuery({
+  const { data, error, isFetching } = useQuery({
     queryKey: ["get_shy_channel"],
     queryFn: async () => {
       return shyApi2({ type: "get_shy_channel", channel_id: channelId });
@@ -19,14 +21,19 @@ const BoardMeta: React.FC<BoardMetaProps> = ({ channelId }) => {
 
   const channel = data?.payload?.shy_channel;
 
-  return error ? (
-    <div>Error has occurred</div>
-  ) : channel ? (
+  if (error) {
+    return <div>Error has occurred</div>;
+  }
+
+  return channel ? (
     <div className={styles.wrapper}>
-      <div>{channel.label}</div>
+      <div className={styles.inner}>
+        <div className={styles.label}>{channel.label}</div>
+        <div className={styles.desc}>{channel.desc}</div>
+      </div>
     </div>
   ) : (
-    <div>Loading...</div>
+    <Loading />
   );
 };
 
