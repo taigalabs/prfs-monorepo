83693d43933822c6af6a86dc35277242d775834a
Elden Park <eldeniyenden@gmail.com>
2024-02-26 03:43:00 +0900

    o

--- source/shy_webapp/src/components/create_post_form/CreatePostForm.tsx
+++ source/shy_webapp/src/components/create_post_form/CreatePostForm.tsx
@@ -1,12 +1,11 @@
 import React from "react";
 import { useRouter } from "next/navigation";
+import { ShyChannel } from "@taigalabs/shy-entities/bindings/ShyChannel";
 
 import styles from "./CreatePostForm.module.scss";
 import { i18nContext } from "@/i18n/context";
 import { paths } from "@/paths";
-import RightBar from "@/components/right_bar/RightBar";
 import TextEditor from "@/components/text_editor/TextEditor";
-import { ShyChannel } from "@taigalabs/shy-entities/bindings/ShyChannel";
 
 const CreatePostForm: React.FC<CreatePostFormProps> = ({ channel }) => {
   const i18n = React.useContext(i18nContext);
@@ -14,25 +13,13 @@ const CreatePostForm: React.FC<CreatePostFormProps> = ({ channel }) => {
 
   return (
     <div className={styles.wrapper}>
-      33
-      {/* <ContentMainInfiniteScroll> */}
-      {/*   <ContentMainCenter> */}
-      {/*     <ContentMainHeader style={{ height: 65 }}> */}
-      {/*       <ContentMainTitle> */}
-      {/*         {i18n.code} for {channelId} */}
-      {/*       </ContentMainTitle> */}
-      {/*     </ContentMainHeader> */}
-      {/*     <ContentMainBody style={{ paddingTop: 65 }}> */}
-      {/*       <div className={styles.editorContainer}> */}
-      {/*         <TextEditor /> */}
-      {/*       </div> */}
-      {/*       <div className={styles.btnRow}>btn</div> */}
-      {/*     </ContentMainBody> */}
-      {/*   </ContentMainCenter> */}
-      {/*   <ContentMainRight> */}
-      {/*     <RightBar /> */}
-      {/*   </ContentMainRight> */}
-      {/* </ContentMainInfiniteScroll> */}
+      <div>
+        {i18n.code} for {channel.channel_id}
+      </div>
+      <div className={styles.editorContainer}>
+        <TextEditor />
+      </div>
+      <div className={styles.btnRow}>btn</div>
     </div>
   );
 };
