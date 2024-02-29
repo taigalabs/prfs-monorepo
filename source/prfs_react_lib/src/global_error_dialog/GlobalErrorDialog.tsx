294058bf1627ef5a893dfbd62e880cf04d22048f
Elden Park <eldeniyenden@gmail.com>
2024-02-29 15:15:58 +0900

    o

--- source/prfs_react_lib/src/global_error_dialog/GlobalErrorDialog.tsx
+++ source/prfs_react_lib/src/global_error_dialog/GlobalErrorDialog.tsx
@@ -2,13 +2,8 @@
 
 import React from "react";
 import cn from "classnames";
-// import { IoMdWarning } from "@react-icons/all-files/io/IoMdWarning";
-// import { IoClose } from "@react-icons/all-files/io5/IoClose";
 
 import styles from "./GlobalErrorDialog.module.scss";
-// import { useAppDispatch, useAppSelector } from "@/state/hooks";
-// import Overlay from "@taigalabs/prfs-react-lib/src/overlay/Overlay";
-// import { removeError } from "@/state/errorReducer";
 
 export const GlobalErrorDialogWrapper: React.FC<GlobalErrorDialogProps> = ({ children }) => {
   return <div className={styles.wrapper}>{children}</div>;
@@ -21,33 +16,6 @@ export const GlobalErrorDialogContent: React.FC<GlobalErrorDialogProps> = ({ chi
 export const GlobalErrorDialogBtnGroup: React.FC<GlobalErrorDialogProps> = ({ children }) => {
   return <div className={styles.btnGroup}>{children}</div>;
 };
-
-// const GlobalErrorDialog: React.FC<GlobalErrorDialogProps> = ({}) => {
-//   const error = useAppSelector(state => state.error.error);
-//   const dispatch = useAppDispatch();
-//   const handleClickClose = React.useCallback(() => {
-//     dispatch(removeError());
-//   }, [dispatch]);
-
-//   return (
-//       <Overlay className={styles.wrapper}>
-//         <div className={styles.dialog}>
-//           <div className={styles.content}>
-//             <IoMdWarning />
-//             <p>{error.message}</p>
-//           </div>
-//           <div className={styles.btnGroup}>
-//             <button type="button" onClick={handleClickClose}>
-//               <IoClose />
-//             </button>
-//           </div>
-//         </div>
-//       </Overlay>
-//     )
-//   );
-// };
-
-// export default GlobalErrorDialog;
 
 export interface GlobalErrorDialogProps {
   children: React.ReactNode;
