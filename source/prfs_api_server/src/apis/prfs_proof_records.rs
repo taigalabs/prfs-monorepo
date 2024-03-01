163312de0267a5f36b24fbb4ff3e03f148537bb6
Elden Park <eldeniyenden@gmail.com>
2024-03-01 17:58:49 +0900

    o

--- source/prfs_api_server/src/apis/prfs_proof_records.rs
+++ source/prfs_api_server/src/apis/prfs_proof_records.rs
@@ -13,7 +13,8 @@ use prfs_db_interface::prfs;
 use prfs_entities::{
     entities::PrfsProofType,
     prfs_api::{
-        CreatePrfsProofTypeRequest, CreatePrfsProofTypeResponse, GetPrfsProofRecordRequest,
+        CreatePrfsProofRecordRequest, CreatePrfsProofRecordResponse, CreatePrfsProofTypeRequest,
+        CreatePrfsProofTypeResponse, GetPrfsProofRecordRequest, GetPrfsProofRecordResponse,
         GetPrfsProofTypeByProofTypeIdRequest, GetPrfsProofTypeByProofTypeIdResponse,
         GetPrfsProofTypesRequest, GetPrfsProofTypesResponse,
     },
@@ -25,7 +26,7 @@ use std::sync::Arc;
 pub async fn get_prfs_proof_record(
     State(state): State<Arc<ServerState>>,
     Json(input): Json<GetPrfsProofRecordRequest>,
-) -> (StatusCode, Json<ApiResponse<GetPrfsProofTypesResponse>>) {
+) -> (StatusCode, Json<ApiResponse<GetPrfsProofRecordResponse>>) {
     let pool = &state.db2.pool;
     // let rows = prfs::get_prfs_proof_record(pool, input.offset, LIMIT).await;
 
@@ -36,32 +37,14 @@ pub async fn get_prfs_proof_record(
 
 pub async fn create_prfs_proof_record(
     State(state): State<Arc<ServerState>>,
-    Json(input): Json<CreatePrfsProofTypeRequest>,
-) -> (StatusCode, Json<ApiResponse<CreatePrfsProofTypeResponse>>) {
+    Json(input): Json<CreatePrfsProofRecordRequest>,
+) -> (StatusCode, Json<ApiResponse<CreatePrfsProofRecordResponse>>) {
     let pool = &state.db2.pool;
     let mut tx = pool.begin().await.unwrap();
 
-    unreachable!();
+    let public_key = prfs::insert_prfs_proof_record(&mut tx, &input.proof_record).await;
+    tx.commit().await.unwrap();
 
-    // let prfs_proof_type = PrfsProofType {
-    //     proof_type_id: input.proof_type_id,
-    //     label: input.label.to_string(),
-    //     author: input.author.to_string(),
-    //     desc: input.desc.to_string(),
-    //     expression: input.expression.to_string(),
-    //     img_url: input.img_url,
-    //     img_caption: input.img_caption,
-    //     circuit_id: input.circuit_id,
-    //     circuit_type_id: input.circuit_type_id,
-    //     circuit_type_data: input.circuit_type_data,
-    //     circuit_driver_id: input.circuit_driver_id,
-    //     created_at: chrono::offset::Utc::now(),
-    // };
-
-    // let id = prfs::insert_prfs_proof_type(&mut tx, &prfs_proof_type).await;
-
-    // tx.commit().await.unwrap();
-
-    // let resp = ApiResponse::new_success(CreatePrfsProofTypeResponse { id });
-    // return (StatusCode::OK, Json(resp));
+    let resp = ApiResponse::new_success(CreatePrfsProofRecordResponse { public_key });
+    return (StatusCode::OK, Json(resp));
 }
