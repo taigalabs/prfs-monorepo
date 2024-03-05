436e070af033ea6acf01b3a83524c75e1dfc708d
Elden Park <eldeniyenden@gmail.com>
2024-03-05 22:01:29 +0900

    o

--- source/shy_webapp/src/components/post/PostMenu.tsx
+++ source/shy_webapp/src/components/post/PostMenu.tsx
@@ -11,26 +11,7 @@ import { toShortDate } from "@/utils/time";
 import { PostInner } from "./PostComponent";
 
 const PostMenu: React.FC<PostContentProps> = ({ postId }) => {
-  // const { data: postData, isFetching: postDataIsFetching } = useQuery({
-  //   queryKey: ["get_shy_post", postId],
-  //   queryFn: async () => {
-  //     return shyApi2({ type: "get_shy_post", post_id: postId });
-  //   },
-  // });
-  // const post = postData?.payload?.shy_post;
-
-  // const publicKey = React.useMemo(() => {
-  //   return post?.inner.public_key.substring(0, 10) || "";
-  // }, [post?.inner.public_key]);
-
-  // const date = React.useMemo(() => {
-  //   if (post?.updated_at) {
-  //     const now = dayjs();
-  //     return toShortDate(post?.updated_at, now);
-  //   } else return "";
-  // }, [post?.updated_at]);
-
-  return <div className={styles.wrapper}>power</div>;
+  return <div className={styles.wrapper}>wip</div>;
 };
 
 export default PostMenu;
