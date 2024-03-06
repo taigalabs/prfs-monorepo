2c8240881db5e6cab31d70b3c90fb9a87f965c85
Elden Park <eldeniyenden@gmail.com>
2024-03-06 22:40:45 +0900

    o

--- source/shy_webapp/src/components/topic/TopicContent.tsx
+++ source/shy_webapp/src/components/topic/TopicContent.tsx
@@ -7,12 +7,8 @@ import dayjs from "dayjs";
 import { MdGroup } from "@react-icons/all-files/md/MdGroup";
 
 import styles from "./TopicContent.module.scss";
-import { paths } from "@/paths";
-import { toShortDate } from "@/utils/time";
-// import { PostInner } from "./PostComponent";
-// import PostMenu from "./TopicMenu";
 import { useI18N } from "@/i18n/hook";
-import Post from "../post/Post";
+import Post from "@/components/post/Post";
 
 const TopicContent: React.FC<PostContentProps> = ({ topicId }) => {
   const i18n = useI18N();
@@ -23,17 +19,6 @@ const TopicContent: React.FC<PostContentProps> = ({ topicId }) => {
     },
   });
   const topic = postData?.payload?.shy_topic_syn1;
-
-  // const publicKey = React.useMemo(() => {
-  //   return topic?.inner.shy_topic.author_public_key.substring(0, 10) || "";
-  // }, [topic?.inner.shy_topic.author_public_key]);
-
-  // const date = React.useMemo(() => {
-  //   if (topic?.updated_at) {
-  //     const now = dayjs();
-  //     return toShortDate(topic?.updated_at, now);
-  //   } else return "";
-  // }, [topic?.updated_at]);
 
   return (
     <div className={styles.wrapper}>
@@ -54,28 +39,6 @@ const TopicContent: React.FC<PostContentProps> = ({ topicId }) => {
             proof_identity_input={topic.inner.proof_identity_input}
             updated_at={topic.updated_at}
           />
-          {/* <PostInner> */}
-          {/*   <div className={styles.meta}> */}
-          {/*     <div className={styles.left}> */}
-          {/*       <div className={styles.item}> */}
-          {/*         <p className={styles.publicKey}>{publicKey}</p> */}
-          {/*       </div> */}
-          {/*       <div className={styles.item}> */}
-          {/*         <p className={styles.proofIdentityInput}>{topic.inner.proof_identity_input}</p> */}
-          {/*       </div> */}
-          {/*     </div> */}
-          {/*     <div className={styles.right}> */}
-          {/*       <p className={styles.date}>{date}</p> */}
-          {/*     </div> */}
-          {/*   </div> */}
-          {/*   <div */}
-          {/*     className={styles.content} */}
-          {/*     dangerouslySetInnerHTML={{ */}
-          {/*       __html: topic.inner.shy_topic.content, */}
-          {/*     }} */}
-          {/*   /> */}
-          {/*   <TopicMenu topicId={topicId} /> */}
-          {/* </PostInner> */}
         </>
       ) : (
         <Spinner />
