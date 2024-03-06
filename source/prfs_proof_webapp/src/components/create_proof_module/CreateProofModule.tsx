96adfdad33e9602b84f2342a0e2245c6d3b655e8
Elden Park <eldeniyenden@gmail.com>
2024-03-06 16:40:02 +0900

    o

--- source/prfs_proof_webapp/src/components/create_proof_module/CreateProofModule.tsx
+++ source/prfs_proof_webapp/src/components/create_proof_module/CreateProofModule.tsx
@@ -51,6 +51,7 @@ const CreateProofModule: React.FC<CreateProofModuleProps> = ({
           name: PROOF,
           proofTypeId: proofType.proof_type_id,
           queryType: QueryType.CREATE_PROOF,
+          proofAction: "PROOF_ACTION",
         },
       ],
       public_key: pkHex,
