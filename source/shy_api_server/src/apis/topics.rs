cfcfdac19aa4a2a1c32c79365597a9c8cfa015fd
Elden Park <eldeniyenden@gmail.com>
2024-03-06 19:06:14 +0900

    o

--- source/shy_api_server/src/apis/topics.rs
+++ source/shy_api_server/src/apis/topics.rs
@@ -1,4 +1,4 @@
-use prfs_axum_lib::axum::{body::Body, extract::State, http::StatusCode, Json};
+use prfs_axum_lib::axum::{extract::State, http::StatusCode, Json};
 use prfs_axum_lib::resp::ApiResponse;
 use prfs_common_server_state::ServerState;
 use prfs_entities::entities::PrfsProofRecord;
