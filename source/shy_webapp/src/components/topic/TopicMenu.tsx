bbe1a22e8bd13891c0b6b85aed6aa82c8897b26e
Elden Park <eldeniyenden@gmail.com>
2024-03-06 22:25:29 +0900

    o

--- source/shy_webapp/src/components/topic/TopicMenu.tsx
+++ source/shy_webapp/src/components/topic/TopicMenu.tsx
@@ -4,19 +4,26 @@ import { shyApi2 } from "@taigalabs/shy-api-js";
 import Spinner from "@taigalabs/prfs-react-lib/src/spinner/Spinner";
 import Link from "next/link";
 import dayjs from "dayjs";
+import { FaReply } from "@react-icons/all-files/fa/FaReply";
 
 import styles from "./TopicMenu.module.scss";
 import { paths } from "@/paths";
 import { toShortDate } from "@/utils/time";
 import { PostInner } from "./PostComponent";
 import { useI18N } from "@/i18n/hook";
+import Button from "@/components/button/Button";
 
 const TopicMenu: React.FC<PostContentProps> = ({ topicId }) => {
   const i18n = useI18N();
   return (
     <div className={styles.wrapper}>
       <ul>
-        <li>{i18n.reply}</li>
+        <li>
+          <Button variant="transparent_1" className={styles.btn}>
+            <FaReply />
+            <span>{i18n.reply}</span>
+          </Button>
+        </li>
       </ul>
     </div>
   );
