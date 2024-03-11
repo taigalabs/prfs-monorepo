637b4f6c3b56aff7987e6a185b2e09cab92d0fc0
Elden Park <eldeniyenden@gmail.com>
2024-03-11 23:53:12 +0900

    o

--- source/shy_webapp/src/components/post_list/Row.tsx
+++ source/shy_webapp/src/components/post_list/Row.tsx
@@ -7,21 +7,19 @@ import Link from "next/link";
 
 import styles from "./Row.module.scss";
 import { paths } from "@/paths";
+import Post from "@/components/post/Post";
 
 const Row: React.FC<RowProps> = ({ post }) => {
   return (
     <div className={styles.wrapper}>
-      <div className={styles.labelRow}>
-        33
-        {/* <span className={styles.label}>{post.inner.shy_post.}</span> */}
-        {/* <span className={styles.locale}>{channel.locale}</span> */}
-      </div>
-      <div
-        className={styles.content}
-        dangerouslySetInnerHTML={{
-          __html: post.inner.shy_post.content,
-        }}
-      />
+      {/* <Post */}
+      {/*   topicId={post.inner.shy_post.topic_id} */}
+      {/*   channel={channel} */}
+      {/*   author_public_key={topic.inner.shy_topic.author_public_key} */}
+      {/*   content={topic.inner.shy_topic.content} */}
+      {/*   proof_identity_input={topic.inner.proof_identity_input} */}
+      {/*   updated_at={topic.updated_at} */}
+      {/* /> */}
     </div>
   );
 };
