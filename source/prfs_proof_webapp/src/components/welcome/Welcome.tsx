217c06b3d847e02389af6c5427b23c8d99f7bee9
Elden Park <eldeniyenden@gmail.com>
2024-03-15 17:39:14 +0900

    o

--- source/prfs_proof_webapp/src/components/welcome/Welcome.tsx
+++ source/prfs_proof_webapp/src/components/welcome/Welcome.tsx
@@ -4,11 +4,8 @@ import React from "react";
 import cn from "classnames";
 
 import styles from "./Welcome.module.scss";
-import { i18nContext } from "@/i18n/context";
 
 const Welcome: React.FC<WelcomeProps> = ({}) => {
-  const i18n = React.useContext(i18nContext);
-
   return <div className={styles.wrapper}>welcome</div>;
 };
 
