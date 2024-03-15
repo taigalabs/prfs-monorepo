4682ac1b4aaa147f684ba9ba47c51458a3e376ec
Elden Park <eldeniyenden@gmail.com>
2024-03-15 18:00:17 +0900

    o

--- source/prfs_proof_webapp/src/components/welcome/Welcome.tsx
+++ source/prfs_proof_webapp/src/components/welcome/Welcome.tsx
@@ -1,21 +1,35 @@
 "use client";
 
 import React from "react";
+import { useRouter } from "next/navigation";
 import cn from "classnames";
 
 import styles from "./Welcome.module.scss";
 import Content from "./Welcome.mdx";
 import { useSignedInProofUser } from "@/hooks/user";
+import { paths } from "@/paths";
 
 const Welcome: React.FC<WelcomeProps> = ({}) => {
+  const router = useRouter();
   const { isInitialized, prfsProofCredential } = useSignedInProofUser();
 
+  React.useEffect(() => {
+    if (isInitialized && !prfsProofCredential) {
+      router.push(paths.__);
+    }
+  }, [isInitialized, prfsProofCredential, router]);
+
   return (
-    <div className={styles.wrapper}>
-      <div className={styles.inner}>
-        <Content />
+    prfsProofCredential && (
+      <div className={styles.wrapper}>
+        <div className={styles.inner}>
+          <Content
+            accountId={prfsProofCredential?.account_id}
+            attestationLink={paths.attestations__create__crypto_asset_size}
+          />
+        </div>
       </div>
-    </div>
+    )
   );
 };
 
