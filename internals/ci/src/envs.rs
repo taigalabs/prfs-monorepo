c879fe214afe8fa75fb49c0821f9537bb186b6ca
Elden Park <eldeniyenden@gmail.com>
2024-03-19 00:43:00 +0900

    o

--- internals/ci/src/envs.rs
+++ internals/ci/src/envs.rs
@@ -1,11 +1,24 @@
 use std::collections::HashMap;
+use std::process::Command;
 
 use crate::paths::PATHS;
 
-pub fn get_envs() -> HashMap<&'static str, &'static str> {
+pub fn get_envs() -> HashMap<&'static str, String> {
+    let git_hash = {
+        let out = Command::new("git")
+            .args(["rev-parse", "HEAD"])
+            .output()
+            .expect("git should exist");
+
+        String::from_utf8(out.stdout).unwrap()
+    };
+
     let envs = HashMap::from([
-        ("GIT_COMMIT_HASH", "AAA"),
-        ("PRFS_WORKSPACE_ROOT", PATHS.ws_root.to_str().unwrap()),
+        ("GIT_COMMIT_HASH", git_hash),
+        (
+            "PRFS_WORKSPACE_ROOT",
+            PATHS.ws_root.to_str().unwrap().to_string(),
+        ),
     ]);
 
     envs
