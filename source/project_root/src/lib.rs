87599ba4ccde7feca54d45d387f83312fa6da093
Elden Park <eldeniyenden@gmail.com>
2024-03-19 21:58:50 +0900

    o

--- source/project_root/src/lib.rs
+++ source/project_root/src/lib.rs
@@ -3,7 +3,8 @@ use std::path::PathBuf;
 pub const PROJECT_ROOT: &'static str = "PROJECT_ROOT";
 
 pub fn get_project_root() -> PathBuf {
-    let ws_root = std::env::var(PROJECT_ROOT).unwrap();
+    let ws_root =
+        std::env::var(PROJECT_ROOT).expect("'PROJECT_ROOT' should be provided as env variable");
     let ws_path = PathBuf::from(ws_root);
 
     ws_path
