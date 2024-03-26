9aae1d8bd9693ee0d11f3fb4f5a37295f2eb3b4a
Elden Park <eldeniyenden@gmail.com>
2024-03-27 02:58:20 +0900

    o

--- source/prfs_proof_webapp/src/components/search_proof_type_form/LogoContainer.tsx
+++ source/prfs_proof_webapp/src/components/search_proof_type_form/LogoContainer.tsx
@@ -3,11 +3,20 @@ import ImageLogo from "@taigalabs/prfs-react-lib/src/image_logo/ImageLogo";
 import cn from "classnames";
 
 import styles from "./LogoContainer.module.scss";
+import { useI18N } from "@/i18n/use_i18n";
+import { chivo } from "@/fonts";
 
 const LogoContainer: React.FC<LogoContainerProps> = ({ proofTypeChosen }) => {
+  const i18n = useI18N();
+
   return (
-    <div className={cn({ [styles.wrapper]: true, [styles.proofTypeChosen]: proofTypeChosen })}>
-      <ImageLogo width={proofTypeChosen ? 55 : "auto"} />
+    <div
+      className={cn(styles.wrapper, chivo.className, { [styles.proofTypeChosen]: proofTypeChosen })}
+    >
+      <div className={styles.text}>
+        <span>Create anonymous</span>
+        {/* <span className={styles.proofs}>proofs</span> */}
+      </div>
     </div>
   );
 };
