ca8325c252e168a98f050c8be2ebf5e9ecbfd996
Elden Park <eldeniyenden@gmail.com>
2024-04-04 02:58:21 +0900

    o

--- source/prfs_circuits_circom/src/builder/circuits_toml.rs
+++ source/prfs_circuits_circom/src/builder/circuits_toml.rs
@@ -1,6 +1,7 @@
 use colored::Colorize;
 use prfs_circuit_interface::circuit_types::CircuitTypeId;
 use serde::{Deserialize, Serialize};
+use std::collections::HashSet;
 
 use crate::{paths::PATHS, CircuitsError};
 
@@ -9,7 +10,7 @@ pub struct CircuitsToml {
     circuit_type_ids: Vec<CircuitTypeId>,
 }
 
-pub fn read_circuits_toml() -> Result<CircuitsToml, CircuitsError> {
+pub fn read_circuits_toml() -> Result<HashSet<CircuitTypeId>, CircuitsError> {
     let circuits_toml_path = PATHS.package_root.join("circuits.toml");
 
     match std::fs::read_to_string(&circuits_toml_path) {
@@ -21,8 +22,9 @@ pub fn read_circuits_toml() -> Result<CircuitsToml, CircuitsError> {
             );
 
             let t = toml::from_str::<CircuitsToml>(&str)?;
+            let set: HashSet<CircuitTypeId> = HashSet::from_iter(t.circuit_type_ids);
 
-            return Ok(t);
+            return Ok(set);
         }
         Err(err) => return Err(err.into()),
     }
