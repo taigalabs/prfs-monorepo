ee1e7fbf4ecfb11f541e357a1e4ec01d01baa95b
Elden Park <eldeniyenden@gmail.com>
2024-04-06 03:47:08 +0900

    o

--- source/prfs_proof_webapp/src/components/home/UsageScenario.tsx
+++ source/prfs_proof_webapp/src/components/home/UsageScenario.tsx
@@ -11,14 +11,14 @@ const UsageScenario: React.FC<LogoContainerProps> = () => {
   const i18n = useI18N();
 
   return (
-    <Area className={styles.wrapper}>
+    <div className={styles.wrapper}>
       <div className={styles.container}>
         <div className={styles.text}>
           What if you can use a finance app, a dating app, or a social app without having to submit
           your data while still being able to say you are undeniably eligible to use their services?
         </div>
       </div>
-    </Area>
+    </div>
   );
 };
 
