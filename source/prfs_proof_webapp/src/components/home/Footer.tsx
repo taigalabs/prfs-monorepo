b209353ef00b798da7c245b8bce4c4a834aa9bc3
Elden Park <eldeniyenden@gmail.com>
2024-04-05 20:03:43 +0900

    o

--- source/prfs_proof_webapp/src/components/home/Footer.tsx
+++ source/prfs_proof_webapp/src/components/home/Footer.tsx
@@ -1,14 +1,8 @@
 import React from "react";
 import cn from "classnames";
-import { MdArrowForward } from "@react-icons/all-files/md/MdArrowForward";
-import HoverableText from "@taigalabs/prfs-react-lib/src/hoverable_text/HoverableText";
-import Link from "next/link";
 
 import styles from "./Footer.module.scss";
 import { useI18N } from "@/i18n/use_i18n";
-import { urls } from "@/urls";
-import { paths } from "@/paths";
-import { Area, Subtitle, Title } from "./IntroComponents";
 
 const Footer: React.FC<LogoContainerProps> = () => {
   const i18n = useI18N();
