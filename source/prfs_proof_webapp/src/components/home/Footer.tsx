14d6bb4a5e9913e0ccdccfa37bb4b6ca842de5b7
Elden Park <eldeniyenden@gmail.com>
2024-04-05 20:12:16 +0900

    o

--- source/prfs_proof_webapp/src/components/home/Footer.tsx
+++ source/prfs_proof_webapp/src/components/home/Footer.tsx
@@ -3,6 +3,7 @@ import cn from "classnames";
 
 import styles from "./Footer.module.scss";
 import { useI18N } from "@/i18n/use_i18n";
+import { envs } from "@/envs";
 
 const Footer: React.FC<LogoContainerProps> = () => {
   const i18n = useI18N();
@@ -10,41 +11,43 @@ const Footer: React.FC<LogoContainerProps> = () => {
   return (
     <div className={styles.wrapper}>
       <div className={styles.inner}>
-        <div className={styles.firstCol}>
+        <div className={styles.lang}>
           <p>{i18n.english}</p>
         </div>
-        <div className={styles.col}>
-          <ul className={styles.list}>
-            <li className={styles.item}>
-              <a href="https://twitter.com/shy_chat">
-                <span>Twitter (Shy)</span>
-              </a>
-            </li>
-            <li className={styles.item}>
-              <a href="https://t.me/shy_chat">
-                <span>Telegram (Shy)</span>
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div className={styles.col}>
-          <ul className={styles.list}>
-            <li className={styles.item}>
-              <a href={process.env.NEXT_PUBLIC_PRFS_DOCS_WEBSITE_ENDPOINT}>
-                <span>Documentation</span>
-              </a>
-            </li>
-            <li className={styles.item}>
-              <a href={process.env.NEXT_PUBLIC_CODE_REPOSITORY_URL}>
-                <span>Github</span>
-              </a>
-            </li>
-            <li className={styles.item}>
-              <a href="https://www.taigalabs.xyz">
-                <span>Taiga Labs</span>
-              </a>
-            </li>
-          </ul>
+        <div className={styles.listContainer}>
+          <div className={styles.col}>
+            <ul className={styles.list}>
+              <li className={styles.item}>
+                <a href="https://twitter.com/shy_chat">
+                  <span>Twitter (Shy)</span>
+                </a>
+              </li>
+              <li className={styles.item}>
+                <a href="https://t.me/shy_chat">
+                  <span>Telegram (Shy)</span>
+                </a>
+              </li>
+            </ul>
+          </div>
+          <div className={styles.col}>
+            <ul className={styles.list}>
+              <li className={styles.item}>
+                <a href={envs.NEXT_PUBLIC_PRFS_DOCS_WEBSITE_ENDPOINT}>
+                  <span>Documentation</span>
+                </a>
+              </li>
+              <li className={styles.item}>
+                <a href={envs.NEXT_PUBLIC_CODE_REPOSITORY_URL}>
+                  <span>Code</span>
+                </a>
+              </li>
+              <li className={styles.item}>
+                <a href={envs.NEXT_PUBLIC_TAIGALABS_ENDPOINT}>
+                  <span>Taiga Labs</span>
+                </a>
+              </li>
+            </ul>
+          </div>
         </div>
       </div>
     </div>
