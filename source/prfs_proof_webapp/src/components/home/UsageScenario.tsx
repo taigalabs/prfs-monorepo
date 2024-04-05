869a2b4a835dc497640d907ea037bc42b5013538
Elden Park <eldeniyenden@gmail.com>
2024-04-05 16:29:36 +0900

    o

--- source/prfs_proof_webapp/src/components/home/UsageScenario.tsx
+++ source/prfs_proof_webapp/src/components/home/UsageScenario.tsx
@@ -9,31 +9,14 @@ import { Area, Title } from "./IntroComponents";
 
 const UsageScenario: React.FC<LogoContainerProps> = () => {
   const i18n = useI18N();
-  // const [tutorialLabel, setTutorialLabel] = React.useState(TutorialLabel.create_identity);
-
-  // const handleClickItem = React.useCallback(
-  //   (e: React.MouseEvent) => {
-  //     const label = e.currentTarget.getAttribute("data-label");
-  //     if (label) {
-  //       setTutorialLabel(Number(label));
-  //     }
-  //   },
-  //   [setTutorialLabel],
-  // );
 
   return (
     <Area className={styles.wrapper}>
       <div className={styles.container}>
-        power
-        {/* {tutorialLabel === TutorialLabel.create_identity && ( */}
-        {/*   <Iframe src="https://drive.google.com/file/d/1hyVmoqCExvPLMU9ABa7Vt6LaTw_D9dCV/preview" /> */}
-        {/* )} */}
-        {/* {tutorialLabel === TutorialLabel.create_proof && ( */}
-        {/*   <Iframe src="https://drive.google.com/file/d/1if74zytTjTy-eShG9GzyCQzhfjN-cN4b/preview" /> */}
-        {/* )} */}
-        {/* {tutorialLabel === TutorialLabel.create_topic && ( */}
-        {/*   <Iframe src="https://drive.google.com/file/d/1Hsic--IUT5LfRFpnRjmwvTcNYl3PDrGp/preview" /> */}
-        {/* )} */}
+        <div className={styles.text}>
+          What if you can use a finance app, a dating app, or a social app without having to submit
+          your information that will let anyone track what you do?
+        </div>
       </div>
     </Area>
   );
