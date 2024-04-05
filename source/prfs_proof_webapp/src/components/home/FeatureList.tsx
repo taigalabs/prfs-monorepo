6bdfe900c24784a264adc8b5c64435c5e008a2c6
Elden Park <eldeniyenden@gmail.com>
2024-04-06 03:40:42 +0900

    o

--- source/prfs_proof_webapp/src/components/home/FeatureList.tsx
+++ source/prfs_proof_webapp/src/components/home/FeatureList.tsx
@@ -1,16 +1,8 @@
 import React from "react";
 import cn from "classnames";
-import { MdArrowForward } from "@react-icons/all-files/md/MdArrowForward";
-import { MdArrowUpward } from "@react-icons/all-files/md/MdArrowUpward";
-import { FaMobileAlt } from "@react-icons/all-files/fa/FaMobileAlt";
-import { TbCertificate } from "@taigalabs/prfs-react-lib/src/tabler_icons/TbCertificate";
-import { TbMathPi } from "@taigalabs/prfs-react-lib/src/tabler_icons/TbMathPi";
-import Link from "next/link";
 
 import styles from "./FeatureList.module.scss";
 import { useI18N } from "@/i18n/use_i18n";
-import { paths } from "@/paths";
-import { Title } from "./IntroComponents";
 
 const FeatureList: React.FC<LogoContainerProps> = ({ handleFocusSearchBar }) => {
   const i18n = useI18N();
