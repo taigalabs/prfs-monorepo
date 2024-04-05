c64c6a77f5f68c2cad157cbeef8137b2de0dc49c
Elden Park <eldeniyenden@gmail.com>
2024-04-05 17:40:31 +0900

    o

--- source/prfs_proof_webapp/src/components/home/FeatureList.tsx
+++ source/prfs_proof_webapp/src/components/home/FeatureList.tsx
@@ -3,17 +3,14 @@ import cn from "classnames";
 import { MdArrowForward } from "@react-icons/all-files/md/MdArrowForward";
 import { MdArrowUpward } from "@react-icons/all-files/md/MdArrowUpward";
 import { FaMobileAlt } from "@react-icons/all-files/fa/FaMobileAlt";
-import HoverableText from "@taigalabs/prfs-react-lib/src/hoverable_text/HoverableText";
 import { TbCertificate } from "@taigalabs/prfs-react-lib/src/tabler_icons/TbCertificate";
-import { TbMathFunction } from "@taigalabs/prfs-react-lib/src/tabler_icons/TbMathFunction";
 import { TbMathPi } from "@taigalabs/prfs-react-lib/src/tabler_icons/TbMathPi";
 import Link from "next/link";
 
 import styles from "./FeatureList.module.scss";
 import { useI18N } from "@/i18n/use_i18n";
-import { urls } from "@/urls";
 import { paths } from "@/paths";
-import { Area, Title } from "./IntroComponents";
+import { Title } from "./IntroComponents";
 
 const FeatureList: React.FC<LogoContainerProps> = ({ handleFocusSearchBar }) => {
   const i18n = useI18N();
@@ -22,20 +19,17 @@ const FeatureList: React.FC<LogoContainerProps> = ({ handleFocusSearchBar }) => 
     <div className={styles.wrapper}>
       <div className={cn(styles.item, styles.gray)}>
         <div className={styles.box}>
-          <img
-            className={styles.image}
-            src="https://d1w1533jipmvi2.cloudfront.net/prfs_proof_example_1.png"
-            crossOrigin="anonymous"
-            alt="Proof example"
-          />
+          <div className={styles.image}>
+            <img
+              src="https://d1w1533jipmvi2.cloudfront.net/prfs_proof_example_1.png"
+              crossOrigin="anonymous"
+              alt="Proof example"
+            />
+          </div>
           <div className={styles.itemContent}>
-            <div className={styles.title}>
-              <p className={styles.iconBox}>
-                <TbMathPi />
-              </p>
-              {i18n.proof}
-            </div>
-            <p className={styles.desc}>Create and verify zero-knowledge proofs on your browser.</p>
+            <Title className={styles.title}>
+              Create a zero-knowledge proof on your own machine
+            </Title>
             <div className={styles.callToAction}>
               <p onClick={handleFocusSearchBar}>
                 Find proof type
@@ -47,12 +41,13 @@ const FeatureList: React.FC<LogoContainerProps> = ({ handleFocusSearchBar }) => 
       </div>
       <div className={styles.item}>
         <div className={styles.box}>
-          <img
-            className={cn(styles.image, {})}
-            src="https://d1w1533jipmvi2.cloudfront.net/prfs_atst_example_1.png"
-            crossOrigin="anonymous"
-            alt="Prfs attestation example"
-          />
+          <div className={styles.image}>
+            <img
+              src="https://d1w1533jipmvi2.cloudfront.net/prfs_atst_example_1.png"
+              crossOrigin="anonymous"
+              alt="Prfs attestation example"
+            />
+          </div>
           <div className={styles.itemContent}>
             <div className={styles.title}>
               <p className={styles.iconBox}>
@@ -74,12 +69,13 @@ const FeatureList: React.FC<LogoContainerProps> = ({ handleFocusSearchBar }) => 
       </div>
       <div className={cn(styles.item, styles.gray)}>
         <div className={styles.box}>
-          <img
-            className={cn(styles.image, {})}
-            src="https://d1w1533jipmvi2.cloudfront.net/prfs_id_example_1.png"
-            crossOrigin="anonymous"
-            alt="Prfs Id example"
-          />
+          <div className={styles.image}>
+            <img
+              src="https://d1w1533jipmvi2.cloudfront.net/prfs_id_example_1.png"
+              crossOrigin="anonymous"
+              alt="Prfs Id example"
+            />
+          </div>
           <div className={styles.itemContent}>
             <div className={styles.title}>
               <p className={styles.iconBox}>
