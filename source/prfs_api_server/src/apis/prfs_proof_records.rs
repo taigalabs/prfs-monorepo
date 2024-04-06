6eeffa2ca0955900f526454a8de2c47139e7e8d5
Elden Park <eldeniyenden@gmail.com>
2024-04-06 20:35:29 +0900

    o

--- source/prfs_api_server/src/apis/prfs_proof_records.rs
+++ source/prfs_api_server/src/apis/prfs_proof_records.rs
@@ -1,6 +1,7 @@
 use prfs_api_error_codes::PRFS_API_ERROR_CODES;
 use prfs_axum_lib::axum::{extract::State, http::StatusCode, Json};
 use prfs_axum_lib::resp::ApiResponse;
+use prfs_axum_lib::{bail_out_tx, bail_out_tx_commit};
 use prfs_common_server_state::ServerState;
 use prfs_db_interface::prfs;
 use prfs_entities::prfs_api::{
@@ -36,19 +37,17 @@ pub async fn create_prfs_proof_record(
     Json(input): Json<CreatePrfsProofRecordRequest>,
 ) -> (StatusCode, Json<ApiResponse<CreatePrfsProofRecordResponse>>) {
     let pool = &state.db2.pool;
-    let mut tx = match pool.begin().await {
-        Ok(t) => t,
+    let mut tx = bail_out_tx!(pool, &PRFS_API_ERROR_CODES.UNKNOWN_ERROR);
+
+    let public_key = match prfs::insert_prfs_proof_record(&mut tx, &input.proof_record).await {
+        Ok(p) => p,
         Err(err) => {
-            let resp = ApiResponse::new_error(
-                &PRFS_API_ERROR_CODES.UNKNOWN_ERROR,
-                format!("error starting db transaction: {}", err),
-            );
+            let resp = ApiResponse::new_error(&PRFS_API_ERROR_CODES.UNKNOWN_ERROR, err.to_string());
             return (StatusCode::BAD_REQUEST, Json(resp));
         }
     };
 
-    let public_key = prfs::insert_prfs_proof_record(&mut tx, &input.proof_record).await;
-    tx.commit().await.unwrap();
+    bail_out_tx_commit!(tx, &PRFS_API_ERROR_CODES.UNKNOWN_ERROR);
 
     let resp = ApiResponse::new_success(CreatePrfsProofRecordResponse { public_key });
     return (StatusCode::OK, Json(resp));
