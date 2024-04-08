57ea7f68909086234b73a08efa85b6f9463f8437
Elden Park <eldeniyenden@gmail.com>
2024-04-08 15:43:50 +0900

    oo

--- source/prfs_id_webapp/src/components/raw_value_dialog/RawValueModal.tsx
+++ source/prfs_id_webapp/src/components/raw_value_dialog/RawValueModal.tsx
@@ -3,11 +3,11 @@ import cn from "classnames";
 import { abbrev7and5 } from "@taigalabs/prfs-ts-utils";
 import Button from "@taigalabs/prfs-react-lib/src/button/Button";
 import { PrfsSet } from "@taigalabs/prfs-entities/bindings/PrfsSet";
+import Input from "@taigalabs/prfs-react-lib/src/input/Input";
 
 import styles from "./RawValueModal.module.scss";
-import { i18nContext } from "@/i18n/context";
+import { useI18N } from "@/i18n/context";
 import { useAppSelector } from "@/state/hooks";
-// import { useCachedItems } from "./use_cached_items";
 
 const RawValueModal: React.FC<CachedItemModalProps> = ({
   prfsSet,
@@ -15,43 +15,14 @@ const RawValueModal: React.FC<CachedItemModalProps> = ({
   handleChangeValue,
 }) => {
   const prfsIdCredential = useAppSelector(state => state.user.prfsIdCredential);
-  // const { walletAddrs } = useCachedItems({ prfsIdCredential, prfsSet });
-  const i18n = React.useContext(i18nContext);
+  const i18n = useI18N();
+  const [value, setValue] = React.useState("");
 
   const extendedHandleChangeValue = React.useCallback(() => {}, [handleChangeValue]);
 
-  // const addrList = React.useMemo(() => {
-  //   if (walletAddrs) {
-  //     const elems = [];
-  //     for (const addr of walletAddrs) {
-  //       const address = abbrev7and5(addr);
-  //       const handleClick = () => {
-  //         handleChangeItem(addr);
-  //       };
-
-  //       const el = (
-  //         <li key={addr}>
-  //           <Button variant="white_black_2" className={styles.itemBtn} handleClick={handleClick}>
-  //             {address}
-  //           </Button>
-  //         </li>
-  //       );
-  //       elems.push(el);
-  //     }
-  //     return (
-  //       <div>
-  //         <p className={styles.title}>{i18n.choose_an_address}</p>
-  //         <ul className={styles.itemList}>{elems}</ul>
-  //       </div>
-  //     );
-  //   } else {
-  //     return <div>No cached addresses to select</div>;
-  //   }
-  // }, [walletAddrs]);
-
   return prfsIdCredential ? (
     <div className={styles.wrapper}>
-      <input />
+      <Input name={""} label={i18n.member_id} value={value} readOnly />
       <div className={styles.btnRow}>
         <Button variant="transparent_aqua_blue_1" handleClick={handleClickClose}>
           {i18n.close}
