fc12c2bcc7ed8f74fdfd4037e0acfad0ec0e92fc
Elden Park <eldeniyenden@gmail.com>
2024-04-08 16:59:08 +0900

    o

--- source/prfs_proof_webapp/src/app/proof/attestations/group_member/page.tsx
+++ source/prfs_proof_webapp/src/app/proof/attestations/group_member/page.tsx
@@ -1,4 +1,4 @@
-import React, { Suspense } from "react";
+import React from "react";
 import { redirect } from "next/navigation";
 
 import styles from "./page.module.scss";
@@ -7,22 +7,6 @@ import { NONCE_ATST_GROUP_ID } from "@/atst_group_id";
 
 const CryptoSizePage = () => {
   redirect(`${paths.attestations__group_member}/${NONCE_ATST_GROUP_ID}`);
-
-  // return (
-  //   <DefaultLayout>
-  //     <AttestationsDefaultBody>
-  //       <Suspense>
-  //         <Attestations>
-  //           <AttestationsMain>
-  //             <AttestationsMainInner>
-  //               <GroupMemberAtstList />
-  //             </AttestationsMainInner>
-  //           </AttestationsMain>
-  //         </Attestations>
-  //       </Suspense>
-  //     </AttestationsDefaultBody>
-  //   </DefaultLayout>
-  // );
 };
 
 export default CryptoSizePage;
