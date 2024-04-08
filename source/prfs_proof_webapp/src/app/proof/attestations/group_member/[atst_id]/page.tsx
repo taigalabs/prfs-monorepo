433ed5d64e91e2cbbb453f2ce8fa4423dea7094d
Elden Park <eldeniyenden@gmail.com>
2024-04-09 00:43:01 +0900

    o

--- source/prfs_proof_webapp/src/app/proof/attestations/group_member/[atst_id]/page.tsx
+++ source/prfs_proof_webapp/src/app/proof/attestations/group_member/[atst_id]/page.tsx
@@ -8,25 +8,9 @@ import {
   AttestationsMain,
   AttestationsMainInner,
 } from "@/components/attestations/AttestationComponents";
-import GroupMemberAtstList from "@/components/group_member_atst_list/GroupMemberAtstList";
 import GroupMemberAtstDetail from "@/components/group_member_atst_detail/GroupMemberAtstDetail";
 
 const AtstGroupPage: React.FC<AtstGroupPageProps> = ({ params }) => {
-  // return (
-  //   <DefaultLayout>
-  //     <AttestationsDefaultBody>
-  //       <Suspense>
-  //         <Attestations>
-  //           <AttestationsMain>
-  //             <AttestationsMainInner>
-  //               <GroupMemberAtstList atst_group_id={params.atst_group_id} />
-  //             </AttestationsMainInner>
-  //           </AttestationsMain>
-  //         </Attestations>
-  //       </Suspense>
-  //     </AttestationsDefaultBody>
-  //   </DefaultLayout>
-  // );
   return (
     <DefaultLayout>
       <AttestationsDefaultBody>
