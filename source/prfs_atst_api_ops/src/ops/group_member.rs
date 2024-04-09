62dca5f074c671a8894b0f24da6b09c58506df04
Elden Park <eldeniyenden@gmail.com>
2024-04-09 16:45:02 +0900

    o

--- source/prfs_atst_api_ops/src/ops/group_member.rs
+++ source/prfs_atst_api_ops/src/ops/group_member.rs
@@ -1,5 +1,7 @@
+use prfs_crypto::ff::PrimeField;
 use prfs_crypto::hex;
 use prfs_crypto::hexutils;
+use prfs_crypto::k256::Scalar;
 use prfs_db_driver::sqlx::{Pool, Postgres, Transaction};
 use prfs_db_interface::prfs;
 use prfs_entities::atst_api::ComputeCryptoAssetTotalValuesResponse;
@@ -8,6 +10,7 @@ use prfs_entities::{
 };
 use prfs_web3_rs::ethers_core::utils::keccak256;
 use prfs_web_fetcher::destinations::coinbase;
+use rust_decimal::Decimal;
 
 use crate::AtstApiOpsError;
 
@@ -21,19 +24,26 @@ pub async fn compute_group_member_atst_value(
         prfs::get_prfs_attestations_by_atst_group_id(&pool, &PrfsAtstGroupId::crypto_1, 0, 50000)
             .await?;
 
-    let mut count = 0;
+    // let mut count = 0;
     for atst in atsts.iter_mut() {
         if let PrfsAtstMeta::group_member(m) = &atst.meta.0 {
             let bytes = m.value_raw.as_bytes();
-            let h = keccak256(bytes);
+            let hx = hex::encode(bytes);
+            let num = Decimal::from_str_radix(&hx, 16)?;
 
+            // Scalar::from_str_vartime(s)
+            // Scalar::from_str_vartime(bytes);
+            // let h = keccak256(bytes);
+            // let hashed = hex::encode(h);
+
+            atst.value_num = num;
             atst.value_raw = m.value_raw.to_string();
         }
     }
 
-    let _rows_updated = prfs::insert_prfs_attestations(&mut tx, &atsts).await?;
+    let rows_updated = prfs::insert_prfs_attestations(&mut tx, &atsts).await?;
 
     return Ok(ComputeGroupMemberValueResponse {
-        updated_row_count: count,
+        updated_row_count: rows_updated.try_into()?,
     });
 }
