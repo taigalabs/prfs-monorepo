5051b89a57732e561394ad15ba74388778350bb1
Elden Park <eldeniyenden@gmail.com>
2024-04-09 14:19:05 +0900

    o

--- source/prfs_proof_webapp/src/components/group_member_atst_list/GroupMemberAtstTable.tsx
+++ source/prfs_proof_webapp/src/components/group_member_atst_list/GroupMemberAtstTable.tsx
@@ -23,6 +23,7 @@ import {
   AttestationLoading,
 } from "@/components/attestations_table/AttestationsTable";
 import { useI18N } from "@/i18n/use_i18n";
+import Link from "next/link";
 
 const AtstRow: React.FC<AtstRowProps> = ({ atst, style, router, setIsNavigating }) => {
   const i18n = useI18N();
@@ -35,10 +36,19 @@ const AtstRow: React.FC<AtstRowProps> = ({ atst, style, router, setIsNavigating 
     return `${atst.cm.substring(0, 12)}...`;
   }, [atst.cm]);
 
-  const handleClickRow = React.useCallback(() => {
-    setIsNavigating(true);
-    router.push(`${paths.attestations__group_member}/g/${atst.atst_group_id}/${atst.atst_id}`);
-  }, [atst.atst_id, router, setIsNavigating]);
+  const url = React.useMemo(() => {
+    return `${paths.attestations__group_member}/g/${atst.atst_group_id}/${atst.atst_id}`;
+  }, [atst]);
+
+  const handleClickRow = React.useCallback(
+    (e: React.MouseEvent) => {
+      e.preventDefault();
+
+      setIsNavigating(true);
+      router.push(url);
+    },
+    [url, router, setIsNavigating],
+  );
 
   const meta = React.useMemo(() => {
     if (typeof atst.meta === "object") {
@@ -49,29 +59,31 @@ const AtstRow: React.FC<AtstRowProps> = ({ atst, style, router, setIsNavigating 
   }, [atst.cm]);
 
   return (
-    <AttestationTableRow style={style} handleClick={handleClickRow}>
-      <AttestationTableCell className={cn(styles.label, styles.cell)}>
-        <span>{label}</span>
-      </AttestationTableCell>
-      <AttestationTableCell className={cn(styles.commitment, styles.w1024)}>
-        {cm}
-      </AttestationTableCell>
-      <AttestationTableCell className={cn(styles.valueNum, styles.w1024)}>
-        {Number(atst.value_num)}
-      </AttestationTableCell>
-      <AttestationTableCell className={cn(styles.valueRaw, styles.w1024)}>
-        {atst.value_raw}
-      </AttestationTableCell>
-      <AttestationTableCell className={cn(styles.meta, styles.w480, styles.cell)}>
-        <span>{meta}</span>
-      </AttestationTableCell>
-      <AttestationTableCell className={cn(styles.notarized, styles.w1320)}>
-        {i18n.not_available}
-      </AttestationTableCell>
-      <AttestationTableCell className={cn(styles.onChain, styles.w1320)}>
-        {i18n.not_available}
-      </AttestationTableCell>
-    </AttestationTableRow>
+    <Link href={url} onClick={handleClickRow}>
+      <AttestationTableRow style={style}>
+        <AttestationTableCell className={cn(styles.label, styles.cell)}>
+          <span>{label}</span>
+        </AttestationTableCell>
+        <AttestationTableCell className={cn(styles.commitment, styles.w1024)}>
+          {cm}
+        </AttestationTableCell>
+        <AttestationTableCell className={cn(styles.valueNum, styles.w1024)}>
+          {Number(atst.value_num)}
+        </AttestationTableCell>
+        <AttestationTableCell className={cn(styles.valueRaw, styles.w1024)}>
+          {atst.value_raw}
+        </AttestationTableCell>
+        <AttestationTableCell className={cn(styles.meta, styles.w480, styles.cell)}>
+          <span>{meta}</span>
+        </AttestationTableCell>
+        <AttestationTableCell className={cn(styles.notarized, styles.w1320)}>
+          {i18n.not_available}
+        </AttestationTableCell>
+        <AttestationTableCell className={cn(styles.onChain, styles.w1320)}>
+          {i18n.not_available}
+        </AttestationTableCell>
+      </AttestationTableRow>
+    </Link>
   );
 };
 
