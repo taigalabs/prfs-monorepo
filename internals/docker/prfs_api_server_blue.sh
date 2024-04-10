43c48b4d986e2aa251814a34434e6e58d8e8e7c5
Elden Park <eldeniyenden@gmail.com>
2024-04-10 21:51:45 +0900

    o

--- internals/docker/prfs_api_server_blue.sh
+++ internals/docker/prfs_api_server_blue.sh
@@ -4,7 +4,7 @@ compose_config_path=$PROJECT_ROOT/internals/docker/compose/docker-compose.yml
 
 printf "Docker copmose file at %s" $compose_config_path;
 
-export CI_CMD="start_prfs_api_server_blue"
+export CI_CMD="start_prfs_api_server_green"
 
 docker compose \
   -f $compose_config_path \
