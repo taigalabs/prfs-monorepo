018d88a80a5ad843589efaf1b4a4b7c5a4f21ad6
Elden Park <eldeniyenden@gmail.com>
2024-04-10 14:12:39 +0900

    o

--- internals/ci/src/cmds/docker.rs
+++ internals/ci/src/cmds/docker.rs
@@ -1,11 +1,13 @@
 use clap::ArgMatches;
 use std::process::Command;
 
-use crate::{deps, paths::PATHS};
+use crate::{create_envs::create_envs, deps, paths::PATHS};
 
 pub const CMD_NAME: &str = "docker";
 
 pub fn run(sub_matches: &ArgMatches) {
+    let envs = create_envs();
+
     let extra_args = match sub_matches.get_many::<String>("extra_args") {
         Some(value_ref) => value_ref.map(|v| v.as_str()).collect::<Vec<_>>(),
         None => vec![],
@@ -25,6 +27,7 @@ pub fn run(sub_matches: &ArgMatches) {
 
                 let status = Command::new(deps::SH)
                     .args([file_path.into_os_string()])
+                    .envs(envs)
                     .status()
                     .expect(&format!("{} command failed to start", deps::CARGO));
                 assert!(status.success());
