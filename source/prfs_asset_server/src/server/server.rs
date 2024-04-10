e60d8582a76c6691867df069763a9e6f984c79ff
Elden Park <eldeniyenden@gmail.com>
2024-04-11 00:23:51 +0900

    o

--- source/prfs_asset_server/src/server/server.rs
+++ source/prfs_asset_server/src/server/server.rs
@@ -1,3 +1,4 @@
+use colored::Colorize;
 use prfs_axum_lib::axum::{self, Router};
 use prfs_axum_lib::tower_http::trace::TraceLayer;
 use std::net::SocketAddr;
@@ -17,9 +18,9 @@ fn using_serve_dir_with_handler_as_service() -> Router {
 async fn serve(app: Router, port: u16) {
     let addr = SocketAddr::from(([127, 0, 0, 1], port));
     let listener = tokio::net::TcpListener::bind(addr).await.unwrap();
+    let local_addr = listener.local_addr().unwrap();
 
-    println!("Asset server running on http://{}/", addr);
-    tracing::info!("listening on {}", listener.local_addr().unwrap());
+    tracing::info!("Listening on {}", local_addr.to_string().green());
 
     axum::serve(listener, app.layer(TraceLayer::new_for_http()))
         .await
