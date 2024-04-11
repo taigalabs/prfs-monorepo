96314baf44e9310be5201f78cdb3fc69f20c32fc
Elden Park <eldeniyenden@gmail.com>
2024-04-11 12:58:29 +0900

    o

--- source/shy_api_server/src/seed/data/shy_channels.rs
+++ source/shy_api_server/src/seed/data/shy_channels.rs
@@ -7,13 +7,14 @@ use crate::paths::PATHS;
 pub fn get_shy_channels_seed() -> Vec<ShyChannel> {
     let crypto_holders_desc =
         read_md_file(&PATHS.data_seed.join("shy_channels/crypto_holders.md")).unwrap();
-
     let korean_crypto_holders_desc = read_md_file(
         &PATHS
             .data_seed
             .join("shy_channels/korean_crypto_holders.md"),
     )
     .unwrap();
+    let nonce_seoul_md =
+        read_md_file(&PATHS.data_seed.join("shy_channels/nonce_seoul.md")).unwrap();
 
     let channels = vec![
         ShyChannel {
@@ -36,10 +37,7 @@ pub fn get_shy_channels_seed() -> Vec<ShyChannel> {
             channel_id: "0xa8482bb".into(),
             label: "Nonce community Seoul (논스)".into(),
             locale: Locale::ko,
-            desc: "Crypto talk. Gossip in the industry. Thoughts on investment. Confessions. \
-Whatever you couldn't have said with your identity revealed. You are expected to speak in English \
-in this channel."
-                .into(),
+            desc: nonce_seoul_md,
             proof_type_ids: JsonType::from(vec![]),
             status: shy_entities::entities::ShyChannelStatus::Normal,
         },
