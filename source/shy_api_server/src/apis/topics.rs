e3885e772d1338be6f63d64f1e705bafcdb78bda
Elden Park <eldeniyenden@gmail.com>
2024-04-11 19:06:07 +0900

    o

--- source/shy_api_server/src/apis/topics.rs
+++ source/shy_api_server/src/apis/topics.rs
@@ -50,33 +50,10 @@ pub async fn create_shy_topic(
         return (StatusCode::BAD_REQUEST, Json(resp));
     }
 
-    // let proof_starts_with: [u8; 8] = match input.proof[0..8].try_into() {
-    //     Ok(p) => p,
-    //     Err(err) => {
-    //         let resp = ApiResponse::new_error(
-    //             &SHY_API_ERROR_CODES.UNKNOWN_ERROR,
-    //             format!(
-    //                 "Cannot slice proof, proof len: {}, err: {}",
-    //                 input.proof.len(),
-    //                 err
-    //             ),
-    //         );
-    //         return (StatusCode::BAD_REQUEST, Json(resp));
-    //     }
-    // };
-
-    // let create_prfs_proof_record_req = CreatePrfsProofRecordRequest {
-    //     proof_record: PrfsProofRecord {
-    //         public_key: input.author_public_key.to_string(),
-    //         proof_starts_with,
-    //     },
-    // };
-
     let _proof_record_resp = match create_prfs_proof_record(
         &ENVS.prfs_api_server_endpoint,
         &input.proof,
         &input.author_public_key,
-        // &create_prfs_proof_record_req,
     )
     .await
     {
