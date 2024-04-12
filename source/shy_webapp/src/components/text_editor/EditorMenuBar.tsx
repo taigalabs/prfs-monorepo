0ba05fe4c601971adf8066b652d3aec0ee1765d1
Elden Park <eldeniyenden@gmail.com>
2024-04-12 17:54:55 +0900

    o

--- source/shy_webapp/src/components/text_editor/EditorMenuBar.tsx
+++ source/shy_webapp/src/components/text_editor/EditorMenuBar.tsx
@@ -1,5 +1,12 @@
 import React from "react";
 import { useCurrentEditor } from "@tiptap/react";
+import { FaBold } from "@react-icons/all-files/fa/FaBold";
+import { FaItalic } from "@react-icons/all-files/fa/FaItalic";
+import { FaStrikethrough } from "@react-icons/all-files/fa/FaStrikethrough";
+import { MdFormatListBulleted } from "@react-icons/all-files/md/MdFormatListBulleted";
+import { FaQuoteLeft } from "@react-icons/all-files/fa/FaQuoteLeft";
+import { FaUndo } from "@react-icons/all-files/fa/FaUndo";
+import { FaRedo } from "@react-icons/all-files/fa/FaRedo";
 
 import styles from "./EditorMenuBar.module.scss";
 
@@ -11,20 +18,69 @@ const EditorMenuBar: React.FC<EditorMenuBarProps> = ({}) => {
   }
 
   return (
-    <div className={styles.menuBar}>
+    <div className={styles.wrapper}>
       <button
         onClick={() => editor.chain().focus().toggleBold().run()}
         disabled={!editor.can().chain().focus().toggleBold().run()}
         className={editor.isActive("bold") ? "is-active" : ""}
       >
-        bold
+        <FaBold />
       </button>
       <button
         onClick={() => editor.chain().focus().toggleItalic().run()}
         disabled={!editor.can().chain().focus().toggleItalic().run()}
         className={editor.isActive("italic") ? "is-active" : ""}
       >
-        italic
+        <FaItalic />
+      </button>
+      <button
+        onClick={() => editor.chain().focus().toggleStrike().run()}
+        disabled={!editor.can().chain().focus().toggleStrike().run()}
+        className={editor.isActive("strike") ? "is-active" : ""}
+      >
+        <FaStrikethrough />
+      </button>
+      <button
+        onClick={() => editor.chain().focus().toggleHeading({ level: 1 }).run()}
+        className={editor.isActive("heading", { level: 1 }) ? "is-active" : ""}
+      >
+        <span className={styles.highlightBtn}>H1</span>
+      </button>
+      <button
+        onClick={() => editor.chain().focus().toggleHeading({ level: 2 }).run()}
+        className={editor.isActive("heading", { level: 2 }) ? "is-active" : ""}
+      >
+        <span className={styles.highlightBtn}>H2</span>
+      </button>
+      <button
+        onClick={() => editor.chain().focus().toggleHeading({ level: 3 }).run()}
+        className={editor.isActive("heading", { level: 3 }) ? "is-active" : ""}
+      >
+        <span className={styles.highlightBtn}>H3</span>
+      </button>
+      <button
+        onClick={() => editor.chain().focus().toggleBulletList().run()}
+        className={editor.isActive("bulletList") ? "is-active" : ""}
+      >
+        <MdFormatListBulleted />
+      </button>
+      <button
+        onClick={() => editor.chain().focus().toggleBlockquote().run()}
+        className={editor.isActive("blockquote") ? "is-active" : ""}
+      >
+        <FaQuoteLeft />
+      </button>
+      <button
+        onClick={() => editor.chain().focus().undo().run()}
+        disabled={!editor.can().chain().focus().undo().run()}
+      >
+        <FaUndo />
+      </button>
+      <button
+        onClick={() => editor.chain().focus().redo().run()}
+        disabled={!editor.can().chain().focus().redo().run()}
+      >
+        <FaRedo />
       </button>
     </div>
   );
