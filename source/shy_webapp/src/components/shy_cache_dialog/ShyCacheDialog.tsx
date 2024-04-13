0a4e368f24aaab8bf2c730704003670b915ec122
Elden Park <eldeniyenden@gmail.com>
2024-04-13 23:41:32 +0900

    o

--- source/shy_webapp/src/components/shy_cache_dialog/ShyCacheDialog.tsx
+++ source/shy_webapp/src/components/shy_cache_dialog/ShyCacheDialog.tsx
@@ -1,62 +1,58 @@
 import React, { useId } from "react";
 import {
   FloatingFocusManager,
-  FloatingOverlay,
-  FloatingPortal,
+  autoUpdate,
+  flip,
+  offset,
+  shift,
   useClick,
   useDismiss,
   useFloating,
   useInteractions,
   useRole,
 } from "@floating-ui/react";
+import { usePrfsI18N } from "@taigalabs/prfs-i18n/react";
 
 import styles from "./ShyCacheDialog.module.scss";
 import { useShyCache } from "@/hooks/user";
 import ShyCacheModal from "./ShyCacheModal";
-import { usePrfsI18N } from "@taigalabs/prfs-i18n/react";
 
 const ShyCacheDialog: React.FC<ShyCacheDialogProps> = () => {
-  const { shyCache, isCacheInitialized } = useShyCache();
+  const { shyCache } = useShyCache();
   const [isOpen, setIsOpen] = React.useState(false);
-  const { refs, context } = useFloating({
+  const { refs, floatingStyles, context } = useFloating({
     open: isOpen,
     onOpenChange: setIsOpen,
+    placement: "bottom-end",
+    middleware: [offset(10), flip({ fallbackAxisSideDirection: "end" }), shift()],
+    whileElementsMounted: autoUpdate,
   });
   const click = useClick(context);
+  const dismiss = useDismiss(context);
   const role = useRole(context);
-  const dismiss = useDismiss(context, { outsidePress: false });
-  const { getReferenceProps, getFloatingProps } = useInteractions([click, role, dismiss]);
+  const { getReferenceProps, getFloatingProps } = useInteractions([click, dismiss, role]);
   const headingId = useId();
-  const descriptionId = useId();
-  const i18n = usePrfsI18N();
 
-  const hash = React.useMemo(() => {
-    if (shyCache) {
-    }
-  }, [shyCache]);
+  const i18n = usePrfsI18N();
 
   return (
     <>
       <div className={styles.base} ref={refs.setReference} {...getReferenceProps()}>
         {i18n.cache}
       </div>
-      <FloatingPortal>
-        {isOpen && (
-          <FloatingOverlay className={styles.overlay} lockScroll>
-            <FloatingFocusManager context={context}>
-              <div
-                className={styles.dialog}
-                ref={refs.setFloating}
-                aria-labelledby={headingId}
-                aria-describedby={descriptionId}
-                {...getFloatingProps()}
-              >
-                <ShyCacheModal shyCache={shyCache} />
-              </div>
-            </FloatingFocusManager>
-          </FloatingOverlay>
-        )}
-      </FloatingPortal>
+      {isOpen && (
+        <FloatingFocusManager context={context}>
+          <div
+            className={styles.dialog}
+            ref={refs.setFloating}
+            aria-labelledby={headingId}
+            style={floatingStyles}
+            {...getFloatingProps()}
+          >
+            <ShyCacheModal shyCache={shyCache} />
+          </div>
+        </FloatingFocusManager>
+      )}
     </>
   );
 };
--- source/shy_webapp/src/components/shy_cache_dialog/ShyCacheModal.tsx
+++ source/shy_webapp/src/components/shy_cache_dialog/ShyCacheModal.tsx
@@ -1,25 +1,27 @@
 import React, { useId } from "react";
-import {
-  FloatingFocusManager,
-  FloatingOverlay,
-  FloatingPortal,
-  useClick,
-  useDismiss,
-  useFloating,
-  useInteractions,
-  useRole,
-} from "@floating-ui/react";
 
 import styles from "./ShyCacheModal.module.scss";
 import { LocalShyCache } from "@/storage/shy_cache";
 
 const ShyCacheModal: React.FC<ShyCacheDialogProps> = ({ shyCache }) => {
-  const hash = React.useMemo(() => {
+  const elems = React.useMemo(() => {
     if (shyCache) {
+      const els = [];
+      for (const key in shyCache) {
+        const item = shyCache[key];
+        els.push(
+          <div key={key}>
+            <p>{key}</p>
+            <p>{item}</p>
+          </div>,
+        );
+      }
+    } else {
+      return <span>Cache is empty</span>;
     }
   }, [shyCache]);
 
-  return <div className={styles.wrapper}>base</div>;
+  return <div className={styles.wrapper}>{elems}333333</div>;
 };
 
 export default ShyCacheModal;
