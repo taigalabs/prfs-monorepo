31ea19224221b3e2f6d3d5a490ab8308ff8d520c
Elden Park <eldeniyenden@gmail.com>
2024-04-14 00:36:08 +0900

    o

--- source/shy_webapp/src/components/home/Home.tsx
+++ source/shy_webapp/src/components/home/Home.tsx
@@ -5,7 +5,7 @@ import React from "react";
 import styles from "./Home.module.scss";
 import Channels from "@/components/channels/Channels";
 import { useShyCache } from "@/hooks/user";
-import Loading from "../loading/Loading";
+import Loading from "@/components/loading/Loading";
 
 const Home: React.FC<HomeProps> = () => {
   const { shyCache, isCacheInitialized } = useShyCache();
