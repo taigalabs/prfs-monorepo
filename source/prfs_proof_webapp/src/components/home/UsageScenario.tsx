83c2448afc26f35bbeb9fdf5e9a98520caaaac16
Elden Park <eldeniyenden@gmail.com>
2024-04-17 18:05:28 +0900

    o

--- source/prfs_proof_webapp/src/components/home/UsageScenario.tsx
+++ source/prfs_proof_webapp/src/components/home/UsageScenario.tsx
@@ -11,6 +11,12 @@ const UsageScenario: React.FC<LogoContainerProps> = () => {
     <div className={styles.wrapper}>
       <div className={styles.container}>
         <div className={styles.text}>
+          Integrating zk-proof functionality into existing applications takes a great time and
+          effort - Prfs helps apps do it in days. Specifically, we help them specify which
+          qualification they want to collect, preprocess cryptographic data, and allow their users
+          to create anonymous proof.
+        </div>
+        <div className={cn(styles.text, styles.text2)}>
           What if you can use a finance app, a dating app, or a social app without having to submit
           your data while still being able to say you are undeniably eligible to use their services?
         </div>
