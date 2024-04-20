a0a9b849e43fd8ee9f82312a378ce8e073df0ec9
Elden Park <eldeniyenden@gmail.com>
2024-04-21 00:01:58 +0900

    o

--- source/prfs_id_webapp/src/app/page.tsx
+++ source/prfs_id_webapp/src/app/page.tsx
@@ -7,14 +7,7 @@ const HomePage = () => {
   return (
     <div className={styles.wrapper}>
       <div className={styles.title}>Prfs identity</div>
-      <p>
-        <span>Launch timestamp </span>
-        <span>{envs.NEXT_PUBLIC_LAUNCH_TIMESTAMP}</span>
-      </p>
-      <p>
-        <span>Commit hash </span>
-        <span>{envs.NEXT_PUBLIC_GIT_COMMIT_HASH}</span>
-      </p>
+      power
     </div>
   );
 };
