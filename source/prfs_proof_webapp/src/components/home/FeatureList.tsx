0107e09c103e9d970de77ead3e8aebaa55e541ce
Elden Park <eldeniyenden@gmail.com>
2024-04-21 22:08:59 +0900

    o

--- source/prfs_proof_webapp/src/components/home/FeatureList.tsx
+++ source/prfs_proof_webapp/src/components/home/FeatureList.tsx
@@ -9,31 +9,29 @@ const FeatureList: React.FC<LogoContainerProps> = ({ handleFocusSearchBar }) => 
 
   return (
     <div className={styles.wrapper}>
-      <div className={cn(styles.item, styles.gray)}>
-        <div className={styles.box}>
-          <div className={styles.image}>
-            <img
-              src="https://d1w1533jipmvi2.cloudfront.net/prfs_proof_example_1.png"
-              crossOrigin="anonymous"
-              alt="Proof example"
-            />
+      <div className={cn(styles.item)}>
+        <div className={styles.itemContent}>
+          <div className={styles.inner}>
+            <p className={styles.title}>Create a zero-knowledge proof on your own</p>
+            <p className={styles.desc}>
+              It is important that you are able to create a proof with minimal reliance on others.
+              Prfs is designed to allow you to make an anonymous claim by yourself.
+            </p>
           </div>
-          <div className={styles.itemContent}>
-            <div className={styles.inner}>
-              <p className={styles.title}>Create a zero-knowledge proof on your own</p>
-              <p className={styles.desc}>
-                It is important that you are able to create a proof with minimal reliance on others.
-                Prfs is designed to allow you to make an anonymous claim by yourself.
-              </p>
-            </div>
-          </div>
+        </div>
+        <div className={styles.image}>
+          <img
+            src="https://d1w1533jipmvi2.cloudfront.net/prfs_proof_1.png"
+            crossOrigin="anonymous"
+            alt="Proof example"
+          />
         </div>
       </div>
       <div className={styles.item}>
         <div className={styles.box}>
           <div className={styles.image}>
             <img
-              src="https://d1w1533jipmvi2.cloudfront.net/prfs_atst_example_1.png"
+              src="https://d1w1533jipmvi2.cloudfront.net/prfs_attestation_1.png"
               crossOrigin="anonymous"
               alt="Prfs attestation example"
             />
@@ -51,11 +49,11 @@ const FeatureList: React.FC<LogoContainerProps> = ({ handleFocusSearchBar }) => 
           </div>
         </div>
       </div>
-      <div className={cn(styles.item, styles.gray)}>
+      <div className={cn(styles.item)}>
         <div className={styles.box}>
           <div className={styles.image}>
             <img
-              src="https://d1w1533jipmvi2.cloudfront.net/prfs_id_example_1.png"
+              src="https://d1w1533jipmvi2.cloudfront.net/prfs_id_1.png"
               crossOrigin="anonymous"
               alt="Prfs Id example"
             />
