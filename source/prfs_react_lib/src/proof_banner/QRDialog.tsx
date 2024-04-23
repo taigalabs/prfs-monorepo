2546a051a1a9170e40bf3928d2c0bd6d7f57af20
Elden Park <eldeniyenden@gmail.com>
2024-04-23 15:26:15 +0900

    o

--- source/prfs_react_lib/src/proof_banner/QRDialog.tsx
+++ source/prfs_react_lib/src/proof_banner/QRDialog.tsx
@@ -48,16 +48,18 @@ const QRDialog: React.FC<QRDialogProps> = ({ data }) => {
               <div className={styles.backdrop}>
                 <FloatingFocusManager context={context}>
                   <div
-                    className={styles.dialog}
+                    className={styles.modal}
                     ref={refs.setFloating}
                     aria-labelledby={headingId}
                     aria-describedby={descriptionId}
                     {...getFloatingProps()}
                   >
                     <div className={styles.QRContainer}>
-                      <button className={styles.closeBtn} onClick={handleClickClose}>
-                        <AiOutlineClose />
-                      </button>
+                      <div className={styles.topBtnRow}>
+                        <button className={styles.closeBtn} onClick={handleClickClose}>
+                          <AiOutlineClose />
+                        </button>
+                      </div>
                       <QRCodeView data={data} size={210} />
                     </div>
                   </div>
