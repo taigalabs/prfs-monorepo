92e5402e86b5bc26d4ca56b61503a320a7f050aa
Elden Park <eldeniyenden@gmail.com>
2024-05-14 14:26:23 +0900

    o

--- source/shy_webapp/src/components/topic/TopicContent.tsx
+++ source/shy_webapp/src/components/topic/TopicContent.tsx
@@ -10,6 +10,8 @@ import { Proof } from "@taigalabs/prfs-driver-interface";
 import styles from "./TopicContent.module.scss";
 import Post from "@/components/post/Post";
 import Loading from "@/components/loading/Loading";
+import { PostMarkdown } from "../post/PostMarkdown";
+import AuthorLabel from "../author/AuthorLabel";
 
 const TopicContent: React.FC<PostContentProps> = ({ topicId, channel, rerender, subChannelId }) => {
   const i18n = usePrfsI18N();
@@ -44,6 +46,7 @@ const TopicContent: React.FC<PostContentProps> = ({ topicId, channel, rerender, 
             <div className={styles.inner}>
               <p className={styles.title}>{shy_topic_with_proofs.shy_topic.inner.title}</p>
               <div className={styles.postMeta}>
+                <AuthorLabel publicKey={shy_topic_with_proofs.shy_topic.inner.author_public_key} />
                 <button className={styles.participants} type="button">
                   {/* <MdGroup /> */}
                   <div>{proofIdentities ?? i18n.participants}</div>
@@ -51,23 +54,24 @@ const TopicContent: React.FC<PostContentProps> = ({ topicId, channel, rerender, 
               </div>
             </div>
           </div>
-          <Post
-            topicId={topicId}
-            channel={channel}
-            author_public_key={shy_topic_with_proofs.shy_topic.inner.author_public_key}
-            author_proof_ids={shy_topic_with_proofs.shy_topic.inner.author_proof_ids}
-            // author_proof_identity_inputs={author_proof_identity_inputs!}
-            content={shy_topic_with_proofs.shy_topic.inner.content}
-            updated_at={shy_topic_with_proofs.shy_topic.updated_at}
-            handleSucceedPost={rerender}
-            subChannelId={subChannelId}
-            // imgUrl={shy_topic}
-            // proofs={shy_proofs}
-            // imgUrl={topic.inner.img_url}
-            // expression={topic.inner.expression}
-            // proof={proof}
-            // proof_type_id={topic.inner.proof_type_id}
-          />
+          <PostMarkdown html={shy_topic_with_proofs.shy_topic.inner.content} />
+          {/* <Post */}
+          {/*   topicId={topicId} */}
+          {/*   channel={channel} */}
+          {/*   author_public_key={shy_topic_with_proofs.shy_topic.inner.author_public_key} */}
+          {/*   author_proof_ids={shy_topic_with_proofs.shy_topic.inner.author_proof_ids} */}
+          {/*   // author_proof_identity_inputs={author_proof_identity_inputs!} */}
+          {/*   content={shy_topic_with_proofs.shy_topic.inner.content} */}
+          {/*   updated_at={shy_topic_with_proofs.shy_topic.updated_at} */}
+          {/*   handleSucceedPost={rerender} */}
+          {/*   subChannelId={subChannelId} */}
+          {/*   // imgUrl={shy_topic} */}
+          {/*   // proofs={shy_proofs} */}
+          {/*   // imgUrl={topic.inner.img_url} */}
+          {/*   // expression={topic.inner.expression} */}
+          {/*   // proof={proof} */}
+          {/*   // proof_type_id={topic.inner.proof_type_id} */}
+          {/* /> */}
         </>
       ) : (
         <Loading centerAlign>
