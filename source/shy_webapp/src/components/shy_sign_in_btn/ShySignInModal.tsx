dc7579ac82c61c5abda562f24079f6a7aca269bf
Elden Park <eldeniyenden@gmail.com>
2024-05-14 13:52:47 +0900

    o

--- source/shy_webapp/src/components/shy_sign_in_btn/ShySignInModal.tsx
+++ source/shy_webapp/src/components/shy_sign_in_btn/ShySignInModal.tsx
@@ -4,12 +4,22 @@ import { BsPersonFill } from "@react-icons/all-files/bs/BsPersonFill";
 import { FaAddressCard } from "@react-icons/all-files/fa/FaAddressCard";
 
 import styles from "./ShySignInModal.module.scss";
+import { useShyI18N } from "@/i18n";
 
 const ShySignInModal: React.FC<ShySignInModalProps> = ({}) => {
+  const i18n = useShyI18N();
+
   return (
-    <div className={styles.wrapper}>
-      <FaAddressCard />
-    </div>
+    <ul className={styles.wrapper}>
+      <li className={styles.item}>
+        <img
+          className={styles.prfsLogo}
+          src="https://d1w1533jipmvi2.cloudfront.net/logo_p_lato_192.png"
+          alt="Prfs"
+        />
+        <span>{i18n.sign_in_via_prfs}</span>
+      </li>
+    </ul>
   );
 };
 
--- source/shy_webapp/src/i18n/en.ts
+++ source/shy_webapp/src/i18n/en.ts
@@ -1,4 +1,5 @@
 export default {
   // there_are_other_posts: 'There are other posts '
   sign_in: "Sign in",
+  sign_in_via_prfs: "Sign in via Prfs",
 };
